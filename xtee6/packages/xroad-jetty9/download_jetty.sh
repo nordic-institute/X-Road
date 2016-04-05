0845ebb025e12cb142236105f05dd3b0328c9236
Vitali Stupin <vitali.stupin@ria.ee>
2016-04-05 14:28:57 +0300

    Fixing Jetty download link

--- xtee6/packages/xroad-jetty9/download_jetty.sh
+++ xtee6/packages/xroad-jetty9/download_jetty.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Helper script for downloading Jetty if it is not found locally.
 
-REMOTE_JETTY_SHA1SUM="$(curl -ss http://download.eclipse.org/jetty/9.3.6.v20151106/dist/jetty-distribution-9.3.6.v20151106.tar.gz.sha1)  $HOME/jetty/jetty.tgz"
+REMOTE_JETTY_SHA1SUM="$(curl -ss http://archive.eclipse.org/jetty/9.3.6.v20151106/dist/jetty-distribution-9.3.6.v20151106.tar.gz.sha1)  $HOME/jetty/jetty.tgz"
 LOCAL_JETTY_SHA1SUM=$(sha1sum ~/jetty/jetty.tgz || echo "")
 test -d ~/jetty || mkdir ~/jetty
 echo "Remote hash: $REMOTE_JETTY_SHA1SUM"
@@ -9,7 +9,7 @@ echo "Local hash:  $LOCAL_JETTY_SHA1SUM"
 if [[ ${REMOTE_JETTY_SHA1SUM} != ${LOCAL_JETTY_SHA1SUM} ]] ; then
   echo "Downloading Jetty"
   rm -rf ~/jetty/jetty.tgz jetty9
-  wget -O ~/jetty/jetty.tgz "http://download.eclipse.org/jetty/9.3.6.v20151106/dist/jetty-distribution-9.3.6.v20151106.tar.gz"
+  wget -O ~/jetty/jetty.tgz "http://archive.eclipse.org/jetty/9.3.6.v20151106/dist/jetty-distribution-9.3.6.v20151106.tar.gz"
 else
   echo "Found jetty.tgz with the expected checksum"
 fi
