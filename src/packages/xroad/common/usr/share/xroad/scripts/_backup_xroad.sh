f1c3170abe5aa31b54bbfff789e873243b9cdb9e
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2017-04-05 12:56:33 +0300

    Fix backup fails due to file permissions

--- src/packages/xroad/common/usr/share/xroad/scripts/_backup_xroad.sh
+++ src/packages/xroad/common/usr/share/xroad/scripts/_backup_xroad.sh
@@ -35,7 +35,7 @@ create_database_backup () {
 
 create_backup_tarball () {
   echo "CREATING TAR ARCHIVE TO ${BACKUP_FILENAME}"
-  tar --create -v --label "${TARBALL_LABEL}" --file ${BACKUP_FILENAME} --exclude="postgresql/*" ${BACKED_UP_PATHS}
+  tar --create -v --label "${TARBALL_LABEL}" --file ${BACKUP_FILENAME} --exclude="/etc/xroad/postgresql" ${BACKED_UP_PATHS}
   if [ $? != 0 ] ; then
     die "Creating a backup file to ${BACKUP_FILENAME} failed!" \
         "Please check the error messages and fix them before trying again!"
