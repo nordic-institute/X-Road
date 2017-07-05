fda45f69145f5975bcb4c675ed26081bbea86b6b
Ilkka Seppälä <ilkka.seppala@gofore.com>
2017-07-05 15:18:45 +0300

    PVAYLADEV-772: Restore script modifications

--- src/packages/xroad/common/usr/share/xroad/scripts/_restore_xroad.sh
+++ src/packages/xroad/common/usr/share/xroad/scripts/_restore_xroad.sh
@@ -64,12 +64,12 @@ clear_shared_memory () {
 select_commands () {
   if has_command initctl
   then
-    LIST_CMD="initctl list | grep -E  '^xroad-|^xtee55-' | cut -f 1 -d ' '"
+    # LIST_CMD="initctl list | grep -E  '^xroad-|^xtee55-' | cut -f 1 -d ' '"
     STOP_CMD="initctl stop"
     START_CMD="initctl start"
   elif has_command systemctl
   then
-    LIST_CMD="systemctl --plain -qt service list-units | grep -E 'xroad-.*.service\s' | sed 's/^\s*//' | cut -d' ' -f1"
+    # LIST_CMD="systemctl --plain -qt service list-units | grep -E 'xroad-.*.service\s' | sed 's/^\s*//' | cut -d' ' -f1"
     STOP_CMD="systemctl stop"
     START_CMD="systemctl start"
   else
@@ -78,13 +78,16 @@ select_commands () {
 }
 
 stop_services () {
-  echo "STOPPING ALL SERVICES EXCEPT JETTY"
+  echo "STOPPING REGISTERED SERVICES"
   select_commands
   # XROAD_SERVICES=$(eval ${LIST_CMD} | grep -v -- -jetty)
-  XROAD_SERVICES="xroad-monitor xroad-confclient xroad-proxy xroad-signer xroad-opmonitor" # xroad-jetty omitted on purpose
-  for service in ${XROAD_SERVICES} ; do
-    echo ${STOP_CMD} ${service}
-    ${STOP_CMD} ${service}
+  # XROAD_SERVICES="xroad-monitor xroad-confclient xroad-proxy xroad-signer xroad-opmonitor" # xroad-jetty omitted on purpose
+  for entry in "/etc/xroad/backup.d/"* ; do
+    if  [[ -f ${entry} ]] ; then
+      servicename=`basename $entry | sed 's/.*_//'`
+      echo ${STOP_CMD} ${service}
+      ${STOP_CMD} ${service}
+    fi
   done
 }
 
@@ -178,9 +181,13 @@ remove_tmp_restore_dir() {
 }
 
 restart_services () {
-  echo "RESTARTING SERVICES"
-  for service in ${XROAD_SERVICES} ; do
-    ${START_CMD} ${service}
+  echo "RESTARTING REGISTERED SERVICES"
+  for entry in "/etc/xroad/backup.d/"* ; do
+    if  [[ -f ${entry} ]] ; then
+      servicename=`basename $entry | sed 's/.*_//'`
+      echo ${START_CMD} ${servicename}
+      ${START_CMD} ${servicename}
+    fi
   done
 }
 
