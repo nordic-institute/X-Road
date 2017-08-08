dfd230981f38b181fd353f9888124f2b6a07ddb9
pekkaan <anssi.pekkarinen@q-factory.fi>
2017-08-08 15:50:37 +0300

    Webserver, mockserver and improved ansible scripts

--- ansible/roles/xroad-mockserver/files/root/install-scripts/install_mock.sh
+++ ansible/roles/xroad-mockserver/files/root/install-scripts/install_mock.sh
@@ -10,10 +10,10 @@ if [ $? -ne 0 ]; then
 fi
 
 # Create run_mock.sh script from template
-sed -e "s~^SOAPUI_HOME\=.*$~SOAPUI_HOME=${SOAPUI_DIR}~" -e "s~^XROAD_MOCK_PATH\=.*$~XROAD_MOCK_PATH=${XROAD_MOCK_PATH}~" template_run_mock.sh > run_mock.sh
-chmod +x run_mock.sh
+cp run_mock.sh run_mock.backup.sh
+sed -e "s~^SOAPUI_HOME\=.*$~SOAPUI_HOME=${SOAPUI_DIR}~" -e "s~^XROAD_MOCK_PATH\=.*$~XROAD_MOCK_PATH=${XROAD_MOCK_PATH}~" run_mock.sh.template > /etc/init.d/mock-service
+chmod +x /etc/init.d/mock-service
 
-sudo mv run_mock.sh /etc/init.d/mock-service
 sudo update-rc.d mock-service defaults
 sudo update-rc.d mock-service enable
 sudo /etc/init.d/mock-service start
