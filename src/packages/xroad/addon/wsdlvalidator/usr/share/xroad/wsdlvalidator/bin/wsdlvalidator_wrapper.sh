9b73108a4076c2ac2492353e1008bb62a07bf2b5
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2017-08-18 11:39:55 +0300

    Fix wrapper script (PVAYLADEV-766)

--- src/packages/xroad/addon/wsdlvalidator/usr/share/xroad/wsdlvalidator/bin/wsdlvalidator_wrapper.sh
+++ src/packages/xroad/addon/wsdlvalidator/usr/share/xroad/wsdlvalidator/bin/wsdlvalidator_wrapper.sh
@@ -9,5 +9,4 @@ if [ $# -lt 1 ]; then
     ST="-st"
 fi
 
-exec $JAVA_HOME/bin/java -D"ee.ria.xroad.internalKeyStorePassword=internal" -D"ee.ria.xroad.internalKeyStore=/etc/xroad/ssl/internal.p12" -cp "${cxf_classpath}" ee.ria.xroad.wsdlvalidator.WSDLValidatorLoader -r $WSDLVALIDATOR_HOME/etc/xroad6.properties "$ST" "$@"
-
+exec $JAVA_HOME/bin/java -D"ee.ria.xroad.internalKeyStorePassword=internal" -D"ee.ria.xroad.internalKeyStore=/etc/xroad/ssl/internal.p12" -cp "${cxf_classpath}" ee.ria.xroad.wsdlvalidator.WSDLValidatorLoader -r $WSDLVALIDATOR_HOME/etc/xroad6.properties $ST "$@"
