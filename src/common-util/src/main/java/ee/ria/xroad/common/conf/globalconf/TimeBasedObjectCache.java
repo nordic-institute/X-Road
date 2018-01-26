213e42778b3c443af8d3aedd24eedab5d7d56fbb
Ilkka Seppälä <ilkka.seppala@gofore.com>
2018-01-26 12:53:22 +0200

    PVAYLADEV-1033 Checkstyle fix

--- src/common-util/src/main/java/ee/ria/xroad/common/conf/globalconf/TimeBasedObjectCache.java
+++ src/common-util/src/main/java/ee/ria/xroad/common/conf/globalconf/TimeBasedObjectCache.java
@@ -26,7 +26,6 @@ import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
