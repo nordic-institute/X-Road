bf4d8d838b8f1eea412cdb77b7cf4a18b5ececbc
Henri Haapakanni <henri.haapakanni@gofore.com>
2018-10-17 15:07:23 +0300

    XRDDEV-165 fix client proxy to enforce whitelisted cipher suite

--- src/proxy/src/main/java/ee/ria/xroad/proxy/clientproxy/FastestConnectionSelectingSSLSocketFactory.java
+++ src/proxy/src/main/java/ee/ria/xroad/proxy/clientproxy/FastestConnectionSelectingSSLSocketFactory.java
@@ -85,9 +85,8 @@ class FastestConnectionSelectingSSLSocketFactory
     private final Cache<CacheKey, URI> selectedHosts;
     private final boolean cachingEnabled;
 
-    FastestConnectionSelectingSSLSocketFactory(SSLContext sslContext,
-                                               String[] supportedCipherSuites) {
-        super(sslContext, null, supportedCipherSuites, (HostnameVerifier) null);
+    FastestConnectionSelectingSSLSocketFactory(SSLContext sslContext) {
+        super(sslContext, null, SystemProperties.getXroadTLSCipherSuites(), (HostnameVerifier) null);
         this.socketfactory = sslContext.getSocketFactory();
         this.selectedHosts = CacheBuilder.newBuilder()
                 .expireAfterWrite(SystemProperties.getClientProxyFastestConnectingSslUriCachePeriod(), TimeUnit.SECONDS)
@@ -164,6 +163,11 @@ class FastestConnectionSelectingSSLSocketFactory
         }
 
         return sslSocket;
+    }
+
+    @Override
+    protected void prepareSocket(final SSLSocket socket) throws IOException {
+        socket.setEnabledCipherSuites(SystemProperties.getXroadTLSCipherSuites());
     }
 
     private static void updateOpMonitoringData(HttpContext context,
