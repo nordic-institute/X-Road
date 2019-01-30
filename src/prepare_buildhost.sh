0435947001aad358c10887460cfab0b99568168b
Ilkka Seppälä <ilkka.seppala@gofore.com>
2019-01-30 11:41:03 +0200

    XRDDEV-258 update jruby

--- src/prepare_buildhost.sh
+++ src/prepare_buildhost.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get install -y curl software-properties-common
 
 REL=$(lsb_release -sr | cut -d'.' -f1)
-JRUBY_VERSION=$(cat .jruby-version || echo "9.1.13.0")
+JRUBY_VERSION=$(cat .jruby-version || echo "9.2.5.0")
 
 if [ $REL -lt 16 ]; then
     sudo apt-add-repository -y ppa:openjdk-r/ppa
@@ -25,7 +25,7 @@ if ! command -v rvm &>/dev/null; then
 fi
 
 rvm install jruby-$JRUBY_VERSION --binary --skip-gemsets
-rvm jruby-$JRUBY_VERSION do jgem install jruby-openssl jruby-launcher gem-wrappers rubygems-bundler rake:12.0.0 rvm jruby-jars:$JRUBY_VERSION bundler:1.14.6 warbler:2.0.4 bundler-audit
+rvm jruby-$JRUBY_VERSION do jgem install jruby-openssl jruby-launcher gem-wrappers rubygems-bundler rake:12.3.2 rvm jruby-jars:$JRUBY_VERSION bundler:1.14.6 warbler:2.0.4 bundler-audit
 
 mkdir -p /var/tmp/xroad
 
@@ -37,4 +37,3 @@ if [[ $REL -ge 16 && ! -e /.dockerenv ]]; then
         newgrp docker
     fi
 fi
-
