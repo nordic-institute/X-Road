8933906d93361ab1325a5cf75c7fcfeb1185a562
Mikko Riippi <mikko.riippi@gofore.com>
2019-02-05 14:22:36 +0200

    Removed localisation setup from vue.config

--- src/proxy-ui-api/frontend/vue.config.js
+++ src/proxy-ui-api/frontend/vue.config.js
@@ -2,13 +2,4 @@ module.exports = {
   devServer: {
     proxy: 'http://xroad2-docker-ss6:8020' // this needs to be parametrized (address where backend runs)
   },
-
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: false
-    }
-  }
 }
