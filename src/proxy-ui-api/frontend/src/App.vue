45f4270654725b53280ecce242b55bf8bfbd06e0
Mikko Riippi <mikko.riippi@gofore.com>
2019-02-05 11:52:29 +0200

    Installed Vuetify via Vue CLI

--- src/proxy-ui-api/frontend/src/App.vue
+++ src/proxy-ui-api/frontend/src/App.vue
@@ -1,29 +1,39 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>fas fa-external-link-alt</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      //
     }
   }
 }
-</style>
+</script>
--- src/proxy-ui-api/frontend/src/main.ts
+++ src/proxy-ui-api/frontend/src/main.ts
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import './plugins/vuetify'
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 Vue.config.productionTip = false;
 
