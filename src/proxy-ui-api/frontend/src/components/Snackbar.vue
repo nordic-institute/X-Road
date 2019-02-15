2eff66ce3f0b6d143e0b3adcf4c1b236a8a2f0cc
Mikko Riippi <mikko.riippi@gofore.com>
2019-02-15 12:56:23 +0200

    Changed snackbar from javascript to typescript.

--- src/proxy-ui-api/frontend/src/components/Snackbar.vue
+++ src/proxy-ui-api/frontend/src/components/Snackbar.vue
@@ -1,54 +1,39 @@
 <template>
   <div>
-      <v-snackbar
-        v-model="error"
-        color="error"
-        :timeout="timeout"
-      >
-        {{ message }}
-        <v-btn
-           flat
-          @click="error = false"
-        >
-          Close
-        </v-btn>
-      </v-snackbar>
+    <v-snackbar v-model="error" color="error" :timeout="timeout">
+      {{ message }}
+      <v-btn flat @click="error = false">Close</v-btn>
+    </v-snackbar>
 
-      <v-snackbar
-        v-model="success"
-        color="success"
-        :timeout="timeout"
-      >
-        {{ message }}
-        <v-btn
-          flat
-          @click="success = false"
-        >
-          Close
-        </v-btn>
-      </v-snackbar>
+    <v-snackbar v-model="success" color="success" :timeout="timeout">
+      {{ message }}
+      <v-btn flat @click="success = false">Close</v-btn>
+    </v-snackbar>
   </div>
 </template>
-<script>
-export default {
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
   // Wrapper for vuetify snackbar component
   data() {
     return {
-      message: null,
+      message: '',
       success: false,
       error: false,
-      timeout: 2000
-    }
+      timeout: 2000,
+    };
   },
   methods: {
-    showError(message) {
+    showError(message: string): void {
       this.message = message;
       this.error = true;
     },
-    showSuccess(message) {
+    showSuccess(message: string): void {
       this.message = message;
       this.success = true;
-    }
-  }
-}
+    },
+  },
+});
 </script>
