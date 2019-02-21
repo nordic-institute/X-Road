081b6f3f927a2e86a7fde184dbfd68cb611fb68d
Janne Mattila <janne.mattila@gofore.com>
2019-02-21 18:32:26 +0200

    XRDDEV-205 fix 403 errors

--- src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/exceptions/ApplicationExceptionHandler.java
+++ src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/exceptions/ApplicationExceptionHandler.java
@@ -28,6 +28,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -63,4 +64,14 @@ public class ApplicationExceptionHandler {
         return exceptionTranslator.toResponseEntity(e, HttpStatus.UNAUTHORIZED);
     }
 
+    /**
+     * handle access denied exceptions
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorInfo> exception(AccessDeniedException e) {
+        log.error("exception caught", e);
+        return exceptionTranslator.toResponseEntity(e, HttpStatus.FORBIDDEN);
+    }
 }
