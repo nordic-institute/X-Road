cec3998cfa66efd21548979bf2c55d64aca6a07e
Mikko Riippi <mikko.riippi@gofore.com>
2019-02-26 16:46:32 +0200

    Keys page shows clients with fetch and reset buttons until it has proper content

--- src/proxy-ui-api/frontend/src/views/Keys.vue
+++ src/proxy-ui-api/frontend/src/views/Keys.vue
@@ -1,8 +1,8 @@
 <template>
   <v-layout align-center justify-center column>
     <v-layout align-center justify-center row elevation-0 buttons>
-      <v-btn outline round color="primary" @click="fetchCities">FETCH DATA</v-btn>
-      <v-btn outline round color="primary" @click="clearCities">CLEAR DATA</v-btn>
+      <v-btn outline round color="primary" @click="fetchClients">FETCH DATA</v-btn>
+      <v-btn outline round color="primary" @click="clearClients">CLEAR DATA</v-btn>
     </v-layout>
 
     <dataTable/>
@@ -20,31 +20,25 @@ export default Vue.extend({
   data: () => ({}),
 
   methods: {
-    fetchCities() {
-      this.$store.dispatch('fetchData').then(
+    fetchClients() {
+      this.$store.dispatch('fetchClients').then(
         (response) => {
-          this.$bus.$emit('show-success', 'Great success!');
+          this.$bus.$emit('show-success', 'Clients fetched');
         },
         (error) => {
           this.$bus.$emit('show-error', error.message);
         },
       );
     },
-    clearCities(): void {
+    clearClients(): void {
       this.$store.dispatch('clearData');
-    },
-    demoLogout(): void {
-      this.$store.dispatch('demoLogout');
     },
   },
   computed: {
     ...mapGetters(['isAuthenticated']),
-    cookies() {
-      return document.cookie.split(';');
-    },
   },
   created() {
-    this.fetchCities();
+    this.fetchClients();
   },
 });
 </script>
