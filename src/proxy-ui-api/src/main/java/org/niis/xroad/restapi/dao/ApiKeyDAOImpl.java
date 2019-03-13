b8c43355d12a5a7dfdc2d0f6fa74dba7f2f3766f
Janne Mattila <janne.mattila@gofore.com>
2019-03-13 20:12:19 +0200

    XRDDEV-237 api key persistence missing files

---
+++ src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/dao/ApiKeyDAOImpl.java
@@ -0,0 +1,51 @@
+/**
+ * The MIT License
+ * Copyright (c) 2018 Estonian Information System Authority (RIA),
+ * Nordic Institute for Interoperability Solutions (NIIS), Population Register Centre (VRK)
+ * Copyright (c) 2015-2017 Estonian Information System Authority (RIA), Population Register Centre (VRK)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.niis.xroad.restapi.dao;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.niis.xroad.restapi.domain.ApiKeyType;
+
+import java.util.List;
+
+/**
+ * API key data access object implementation.
+ */
+public class ApiKeyDAOImpl {
+
+    public ApiKeyType findById(Session session, Long id) {
+        return (ApiKeyType) session.get(ApiKeyType.class, id);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<ApiKeyType> findAll(Session session) {
+        Query query = session.createQuery("from " + ApiKeyType.class.getName());
+        return query.list();
+    }
+
+    public long insert(Session session, ApiKeyType apiKeyType) {
+        return (Long) session.save(apiKeyType);
+    }
+}
