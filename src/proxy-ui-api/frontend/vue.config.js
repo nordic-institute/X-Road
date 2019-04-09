65d4801fed374e52a5321f6bc04161b016f8ee56
Mikko Riippi <mikko.riippi@gofore.com>
2019-04-09 14:47:11 +0300

    XRDDEV-389 Add i18n

--- src/proxy-ui-api/frontend/vue.config.js
+++ src/proxy-ui-api/frontend/vue.config.js
@@ -3,4 +3,13 @@ module.exports = {
     proxy: 'https://docker-rest-ui-ss.local:4000', // this needs to be parametrized (address where backend runs)
     https: true
   },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
 }
