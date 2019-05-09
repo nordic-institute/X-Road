d0bbb5e79c47a8ca014e54bb5b2432294228eed1
Mikko Riippi <mikko.riippi@gofore.com>
2019-05-09 14:43:58 +0300

    XRDDEV-300 Add session timeout polling

--- src/proxy-ui-api/frontend/src/views/Base.vue
+++ src/proxy-ui-api/frontend/src/views/Base.vue
@@ -8,28 +8,92 @@
         <router-view/>
       </transition>
     </v-layout>
+
+    <v-dialog v-model="logoutDialog" width="500" lazy>
+      <v-card class="xroad-card">
+        <v-card-title>
+          <span class="headline">Session expired</span>
+        </v-card-title>
+        <v-card-text>You have been idle for 30 minutes and your session has expired. For security reasons, you will be logged out.</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            round
+            outline
+            class="mb-2 rounded-button"
+            @click="closeLogoutDialog()"
+          >Ok</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import { mapGetters } from 'vuex';
-
+import axios from 'axios';
+import { RouteName } from '@/global';
 import Toolbar from '../components/Toolbar.vue';
 
 export default Vue.extend({
   components: {
     Toolbar,
   },
+  data() {
+    return {
+      interval: 0,
+      logoutDialog: false,
+    };
+  },
+  watch: {
+    logoutDialog(val) {
+      // Watch for changes to logout dialog model.
+      // Do logout if the user closes the dialog by ok button or clicking outside it.
+      if (!val) {
+        this.logout();
+      }
+    },
+  },
+  created() {
+    // Set interval to poll backend for session
+    this.interval = setInterval(() => this.pollSessionStatus(), 30000);
+  },
+  methods: {
+    closeLogoutDialog() {
+      this.logoutDialog = false;
+    },
+    pollSessionStatus() {
+      return axios.get('/notifications/session-status').catch((error) => {
+        if (error.response && error.response.status === 401) {
+          this.logoutDialog = true;
+          clearInterval(this.interval);
+        }
+      });
+    },
+    logout(): void {
+      this.$store.dispatch('logout');
+      this.$router.replace({ name: RouteName.Login });
+    },
+  },
 });
 </script>
 
+<style lang="scss">
+// Override vuetify styling in alert dialog card
+.xroad-card {
+  .v-card__title {
+    border-bottom: solid #663cdc 2px;
+  }
+}
+</style>
+
 <style lang="scss" scoped>
-.full-width {
+.base-full-width {
   width: 100%;
   max-width: 1280px;
-  display: flex;
-  justify-content: center;
+  margin: 10px;
 }
 
 .fade-enter-active,
