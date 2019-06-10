9825ab6bec29538524947945e0524edd2bab3319
Janne Mattila <janne.mattila@gofore.com>
2019-06-10 17:42:51 +0300

    XRDDEV-485 implement single service description enable and disable

--- src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/openapi/ServiceDescriptionsApiController.java
+++ src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/openapi/ServiceDescriptionsApiController.java
@@ -26,7 +26,9 @@ package org.niis.xroad.restapi.openapi;
 
 import lombok.extern.slf4j.Slf4j;
 import org.niis.xroad.restapi.converter.ServiceDescriptionConverter;
+import org.niis.xroad.restapi.exceptions.InvalidParametersException;
 import org.niis.xroad.restapi.openapi.model.InlineObject9;
+import org.niis.xroad.restapi.service.ServiceDescriptionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -34,6 +36,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.NativeWebRequest;
+
+import java.util.Collections;
 
 /**
  * clients api
@@ -46,27 +50,51 @@ public class ServiceDescriptionsApiController implements ServiceDescriptionsApi 
 
     private final NativeWebRequest request;
     private final ServiceDescriptionConverter serviceDescriptionConverter;
+    private final ServiceDescriptionService serviceDescriptionService;
 
     /**
      * ServiceDescriptionsApiController constructor
      * @param serviceDescriptionConverter
+     * @param serviceDescriptionService
      */
 
     @Autowired
     public ServiceDescriptionsApiController(NativeWebRequest request,
-                                            ServiceDescriptionConverter serviceDescriptionConverter) {
+                                            ServiceDescriptionConverter serviceDescriptionConverter,
+                                            ServiceDescriptionService serviceDescriptionService) {
         this.request = request;
         this.serviceDescriptionConverter = serviceDescriptionConverter;
+        this.serviceDescriptionService = serviceDescriptionService;
+    }
+
+    @Override
+    @PreAuthorize("hasAuthority('ENABLE_DISABLE_WSDL')")
+    public ResponseEntity<Void> enableServiceDescription(String id) {
+        Long serviceDescriptionId = parseServiceDescriptionId(id);
+        serviceDescriptionService.enableServices(Collections.singletonList(serviceDescriptionId));
+        return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+
+    private Long parseServiceDescriptionId(String id) {
+        Long serviceDescriptionId = null;
+        try {
+            serviceDescriptionId = Long.valueOf(id);
+        } catch (NumberFormatException nfe) {
+            throw new InvalidParametersException(nfe);
+        }
+        return serviceDescriptionId;
     }
 
     @Override
     @PreAuthorize("hasAuthority('ENABLE_DISABLE_WSDL')")
     public ResponseEntity<Void> disableServiceDescription(String id, InlineObject9 inlineObject9) {
-        log.info("id: {}", id);
-        log.info("inlineobject: {}", inlineObject9);
+        String disabledNotice = null;
         if (inlineObject9 != null) {
-            log.info("disabled notice: {}", inlineObject9.getDisabledNotice());
+            disabledNotice = inlineObject9.getDisabledNotice();
         }
+        Long serviceDescriptionId = parseServiceDescriptionId(id);
+        serviceDescriptionService.disableServices(Collections.singletonList(serviceDescriptionId),
+                disabledNotice);
         return new ResponseEntity<Void>(HttpStatus.OK);
     }
 
