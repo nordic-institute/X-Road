6b891f0fa4359bfdc15e66cf6f5a6064bab80df8
Mikko Riippi <mikko.riippi@gofore.com>
2019-06-26 15:10:44 +0300

    XRDDEV-458 Add expandable component

---
+++ src/proxy-ui-api/frontend/src/components/Expandable.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="header">
+      <div>
+        <v-btn flat fab small @click="clicked" class="no-hover">
+          <v-icon v-if="isOpen">expand_more</v-icon>
+          <v-icon v-else>chevron_right</v-icon>
+        </v-btn>
+      </div>
+      <div class="header-link">
+        <slot name="link"></slot>
+      </div>
+
+      <v-spacer/>
+      <div>
+        <slot name="action">
+          <v-switch class="switch"></v-switch>
+        </slot>
+      </div>
+    </div>
+    <div v-if="isOpen">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'expandable',
+  components: {},
+  props: {
+    isOpen: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  methods: {
+    clicked(): void {
+      if (this.isOpen) {
+        this.$emit('close');
+      } else {
+        this.$emit('open');
+      }
+    },
+  },
+});
+</script>
+
+<style lang="scss" >
+.no-hover:hover:before,
+.no-hover:focus:before {
+  background-color: transparent;
+}
+
+.header {
+  display: flex;
+  align-items: center;
+  height: 48px;
+  width: 850px;
+  border-radius: 4px;
+  background-color: #ededed;
+  box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.2);
+
+  .header-link {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+}
+
+.switch {
+  width: 60px;
+}
+</style>
+
+
