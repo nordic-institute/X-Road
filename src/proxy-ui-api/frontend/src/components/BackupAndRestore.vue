38f72ed921ae49b8cdb754889a862770e2500254
Mikko Riippi <mikko.riippi@gofore.com>
2019-10-01 14:57:09 +0300

    XRDDEV-631 Add components for new views

---
+++ src/proxy-ui-api/frontend/src/components/BackupAndRestore.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper">Backup and restore (placeholder)</div>
+</template>
+
+<script lang="ts">
+/**
+ * View for 'backup and restore' tab. Not implemented yet.
+ */
+import Vue from 'vue';
+
+export default Vue.extend({});
+</script>
+
+<style lang="scss" scoped>
+</style>
