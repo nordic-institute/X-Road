14a8239034cfdb4ae5e4717482659c78aba21950
Caro Hautamäki <caro.hautamaki@gofore.com>
2019-10-03 14:34:27 +0300

    XRDDEV-667 REST auth refactoring // initial commit
    * initial createEndpoint method

--- src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/service/ServiceDescriptionService.java
+++ src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/service/ServiceDescriptionService.java
@@ -26,6 +26,7 @@ package org.niis.xroad.restapi.service;
 
 import ee.ria.xroad.common.conf.serverconf.model.ClientType;
 import ee.ria.xroad.common.conf.serverconf.model.DescriptionType;
+import ee.ria.xroad.common.conf.serverconf.model.EndpointType;
 import ee.ria.xroad.common.conf.serverconf.model.ServiceDescriptionType;
 import ee.ria.xroad.common.conf.serverconf.model.ServiceType;
 import ee.ria.xroad.common.identifier.ClientId;
@@ -190,12 +191,16 @@ public class ServiceDescriptionService {
      */
     @PreAuthorize("hasAuthority('ADD_WSDL')")
     public ServiceDescriptionType addWsdlServiceDescription(ClientId clientId,
-                                          String url,
-                                          boolean ignoreWarnings) {
+            String url,
+            boolean ignoreWarnings) {
         ClientType client = clientService.getClient(clientId);
         if (client == null) {
             throw new NotFoundException("Client with id " + clientId.toShortString() + " not found");
         }
+
+        // TODO: Create EndpointTypes for each Service in ServiceDescription
+
+        client.getEndpoint()
 
         WsdlProcessingResult wsdlProcessingResult = processWsdl(client, url, null);
 
@@ -211,6 +216,12 @@ public class ServiceDescriptionService {
         client.getServiceDescription().add(serviceDescriptionType);
         clientRepository.saveOrUpdateAndFlush(client);
         return serviceDescriptionType;
+    }
+
+    private EndpointType createEndpoint(List<EndpointType> endpoints, String serviceCode, String method, String path,
+            boolean isGenerated) {
+        EndpointType endpointType = new EndpointType(serviceCode, method, path, true);
+        return endpointType;
     }
 
     /**
@@ -269,7 +280,7 @@ public class ServiceDescriptionService {
      * @return ServiceDescriptionType
      */
     private ServiceDescriptionType updateWsdlUrl(ServiceDescriptionType serviceDescriptionType, String url,
-                                                boolean ignoreWarnings) {
+            boolean ignoreWarnings) {
         // Shouldn't be able to edit e.g. REST service descriptions with a WSDL URL
         if (serviceDescriptionType.getType() != DescriptionType.WSDL) {
             throw new BadRequestException("Existing service description (id: "
@@ -335,7 +346,7 @@ public class ServiceDescriptionService {
      * one we are updating now.
      */
     private void checkForExistingWsdl(ClientType client, String url,
-                                      Long updatedServiceDescriptionId) throws ConflictException {
+            Long updatedServiceDescriptionId) throws ConflictException {
         client.getServiceDescription().forEach(serviceDescription -> {
             if (!serviceDescription.getId().equals(updatedServiceDescriptionId)) {
                 if (serviceDescription.getUrl().equalsIgnoreCase(url)) {
@@ -359,7 +370,6 @@ public class ServiceDescriptionService {
 
         return serviceDescriptionType;
     }
-
 
     private ServiceDescriptionType getServiceDescriptionOfType(ClientType client, String url,
             DescriptionType descriptionType) {
@@ -402,7 +412,6 @@ public class ServiceDescriptionService {
      * If non-fatal warnings, return those.
      * @param url
      * @return list of validation warnings that can be ignored by choice
-     *
      * @throws BadRequestException if fatal validation errors occurred
      */
     private List<String> validateWsdl(String url) throws BadRequestException {
@@ -442,7 +451,7 @@ public class ServiceDescriptionService {
         // throw error with service metadata if conflicted
         if (!conflictedServices.isEmpty()) {
             List<String> errorMetadata = new ArrayList();
-            for (ServiceType conflictedService: conflictedServices) {
+            for (ServiceType conflictedService : conflictedServices) {
                 // error metadata contains service name and service description url
                 errorMetadata.add(FormatUtils.getServiceFullName(conflictedService));
                 errorMetadata.add(conflictedService.getServiceDescription().getUrl());
@@ -450,7 +459,6 @@ public class ServiceDescriptionService {
             throw new ConflictException(new Error(ERROR_SERVICE_EXISTS, errorMetadata));
         }
     }
-
 
     @Data
     private class WsdlProcessingResult {
@@ -465,12 +473,12 @@ public class ServiceDescriptionService {
      * @param client client who is associated with the wsdl
      * @param url url of the wsdl
      * @param updatedServiceDescriptionId id of the service description we
-     *                                    will update with this wsdl, or null
-     *                                    if we're adding a new one
+     * will update with this wsdl, or null
+     * if we're adding a new one
      * @return parsed and validated wsdl and possible warnings
      */
     private WsdlProcessingResult processWsdl(ClientType client, String url,
-                                             Long updatedServiceDescriptionId) {
+            Long updatedServiceDescriptionId) {
 
         WsdlProcessingResult result = new WsdlProcessingResult();
         // check for valid url (is this not enough??)
