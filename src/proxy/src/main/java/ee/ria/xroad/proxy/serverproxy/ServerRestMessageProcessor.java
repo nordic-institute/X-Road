abfad661f763cc5fe1a440449283bfb94cf57528
Tapio Jaakkola <tapio.jaakkola@github.mail.kapsi.fi>
2019-10-09 16:05:26 +0300

    XRDDEV-696: Rename DescriptionTypes

--- src/proxy/src/main/java/ee/ria/xroad/proxy/serverproxy/ServerRestMessageProcessor.java
+++ src/proxy/src/main/java/ee/ria/xroad/proxy/serverproxy/ServerRestMessageProcessor.java
@@ -343,8 +343,8 @@ class ServerRestMessageProcessor extends MessageProcessorBase {
         }
 
         DescriptionType descriptionType = ServerConf.getDescriptionType(requestServiceId);
-        if (descriptionType != null && descriptionType != DescriptionType.OPENAPI3
-            && descriptionType != DescriptionType.OPENAPI3_DESCRIPTION) {
+        if (descriptionType != null && descriptionType != DescriptionType.REST
+            && descriptionType != DescriptionType.OPENAPI3) {
             throw new CodedException(X_INVALID_SERVICE_TYPE,
                     "Service is a SOAP service and cannot be called using REST interface");
         }
