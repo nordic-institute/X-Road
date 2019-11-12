765face99485df904fda5e282bf119a2e66880bc
Mikko Riippi <mikko.riippi@gofore.com>
2019-11-12 16:56:48 +0200

    XRDDEV-656 Add localised string

--- src/proxy-ui-api/frontend/src/components/TokenLoginDialog.vue
+++ src/proxy-ui-api/frontend/src/components/TokenLoginDialog.vue
@@ -10,7 +10,7 @@
   >
     <div slot="content">
       <div class="dlg-edit-row">
-        <div class="dlg-row-title">Token PIN</div>
+        <div class="dlg-row-title">{{$t('fields.tokenPin')}}</div>
         <ValidationProvider
           rules="required"
           ref="tokenPin"
