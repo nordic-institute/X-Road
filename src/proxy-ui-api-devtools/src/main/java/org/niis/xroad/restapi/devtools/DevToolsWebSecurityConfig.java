ae5b24e1d8750c21ef8beba8c5aa1ac374014635
Janne Mattila <janne.mattila@gofore.com>
2019-11-19 20:26:28 +0200

    XRDDEV-758 add http and https devtools profiles since both are needed

--- src/proxy-ui-api-devtools/src/main/java/org/niis/xroad/restapi/devtools/DevToolsWebSecurityConfig.java
+++ src/proxy-ui-api-devtools/src/main/java/org/niis/xroad/restapi/devtools/DevToolsWebSecurityConfig.java
@@ -36,7 +36,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
  * and actuator endpoint
  */
 @Configuration
-@Profile("devtools")
+@Profile({"devtools-http", "devtools-https"})
 @Order(DevToolsWebSecurityConfig.AFTER_STATIC_ASSETS)
 public class DevToolsWebSecurityConfig extends WebSecurityConfigurerAdapter {
     public static final int AFTER_STATIC_ASSETS = 10;
