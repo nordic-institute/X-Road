c1b5e90f190a88c993187144de6a604ba3b89591
Tapio Jaakkola <tapio.jaakkola@github.mail.kapsi.fi>
2019-11-20 14:11:27 +0200

    XRDDEV-661: Fix checkstyle

--- src/proxy-ui-api/src/test/java/org/niis/xroad/restapi/service/KeyServiceTest.java
+++ src/proxy-ui-api/src/test/java/org/niis/xroad/restapi/service/KeyServiceTest.java
@@ -52,7 +52,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 /**
