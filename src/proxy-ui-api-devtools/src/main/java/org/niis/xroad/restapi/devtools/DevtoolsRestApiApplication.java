9beec88da92818d816a59f9af74af30e3da66d65
Caro Hautamäki <caro.hautamaki@gofore.com>
2019-12-05 11:06:35 +0200

    XRDDEV-707 Import existing cert // Unrelated devtools fix
    * remove old property source

--- src/proxy-ui-api-devtools/src/main/java/org/niis/xroad/restapi/devtools/DevtoolsRestApiApplication.java
+++ src/proxy-ui-api-devtools/src/main/java/org/niis/xroad/restapi/devtools/DevtoolsRestApiApplication.java
@@ -28,14 +28,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * devtools -enabled main spring boot application.
  */
 @ServletComponentScan(basePackages = {"org.niis.xroad.restapi"})
 @SpringBootApplication(scanBasePackages = {"org.niis.xroad.restapi"})
-@PropertySource("classpath:/common-application.properties")
 @EnableCaching
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 public class DevtoolsRestApiApplication {
