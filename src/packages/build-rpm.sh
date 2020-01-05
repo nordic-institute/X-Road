28f7e505ba3bbcfad24657c99494e8d009e76ea3
Samel Jon Gunnarsson <samuel.jon.gunnarsson@gmail.com>
2020-01-05 22:44:42 +0000

    Adding parameter in build_packages and updating build-rpm.sh accordingly for rhel-8 support

--- src/packages/build-rpm.sh
+++ src/packages/build-rpm.sh
@@ -1,31 +1,59 @@
 #!/bin/bash
+##########################################################################################
+# This build script takes 3 parameters.
+# The first parameter which is the release version 7 or 8 is mandatory.
+# The second parameter (optional) is -release or the name of the spec file to build.
+# The third parameter (optional) is the command to execute in the rpmbuild, default is bb
+##########################################################################################
 set -e
 
 VERSION=6.24.0
 
-if [[ $1 == "-release" ]] ; then
-  RELEASE=1
-  FILES="xroad-*.spec"
-  CMD="bb"
+# First parameter
+case "$1" in
+    7)
+        REDHAT_VERSION=7
+    ;;
+    8)
+        REDHAT_VERSION=8
+    ;;
+    *)
+        echo "Unknown distribution $1"
+    ;;
+esac
+
+# Second Parameter
+if [[ $2 == "-release" ]] ; then
+    RELEASE=1
+    FILES="xroad-*.spec"
+    CMD="bb"
 else
-  RELEASE=0
-  DATE="$(date --utc --date @$(git show -s --format=%ct || date +%s) +'%Y%m%d%H%M%S')"
-  HASH="$(git show -s --format=git%h --abbrev=7 || echo 'local')"
-  SNAPSHOT=.$DATE$HASH
-  FILES=${1-'xroad-*.spec'}
-  CMD=${2-bb}
+    RELEASE=0
+    DATE="$(date --utc --date @$(git show -s --format=%ct || date +%s) +'%Y%m%d%H%M%S')"
+    HASH="$(git show -s --format=git%h --abbrev=7 || echo 'local')"
+    SNAPSHOT=.$DATE$HASH
+    FILES=${2-'xroad-*.spec'}
+    # Third parameter
+    CMD=${3-bb}
 fi
 
+########################################
+# build process begins
+########################################
 DIR=$(cd "$(dirname $0)" && pwd)
 cd "$DIR"
-
-mkdir -p build/xroad/redhat
+# Copy the SPEC and SOURCES to the build folder
+mkdir -p build/xroad/redhat-${REDHAT_VERSION}
 cp -a src/xroad/redhat build/xroad
 mkdir -p build/xroad-jetty9
 cp -a src/xroad-jetty9/redhat build/xroad-jetty9/
-find build/ -name "*.rpm" | xargs rm -f
+# Clean out rpm packages before building
+find build/ -name "*.el${REDHAT_VERSION}*.rpm" | xargs rm -f
 
-ROOT=${DIR}/build/xroad/redhat
+#############
+# build xroad rpms
+#############
+ROOT=${DIR}/build/xroad/redhat/${REDHAT_VERSION}
 rpmbuild \
     --define "xroad_version $VERSION" \
     --define "rel $RELEASE" \
@@ -34,8 +62,10 @@ rpmbuild \
     --define "srcdir $DIR/src/xroad" \
     -${CMD} "${ROOT}/SPECS/"${FILES}
 
-# build jetty rpms
-ROOT="${DIR}/build/xroad-jetty9/redhat"
+#############
+# build xroad-jetty rpms
+#############
+ROOT="${DIR}/build/xroad-jetty9/redhat/${REDHAT_VERSION}"
 ./download_jetty.sh
 rpmbuild \
     --define "xroad_version $VERSION" \
