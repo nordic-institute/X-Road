77b3fff83afee606681b469e1952a819af0473fc
Raul Martinez <raul.martinez@leadin.co.uk>
2020-01-05 19:08:33 +0100

    XRDDEV-788 Make autologin pin code readable by xroad user.

--- sidecar/files/ss-entrypoint.sh
+++ sidecar/files/ss-entrypoint.sh
@@ -4,7 +4,7 @@
 if [  -n "$XROAD_TOKEN_PIN" ]
 then
     echo "XROAD_TOKEN_PIN variable set, writing to /etc/xroad/autologin"
-    echo "$XROAD_TOKEN_PIN" > /etc/xroad/autologin
+    su xroad -c 'echo $XROAD_TOKEN_PIN >/etc/xroad/autologin'
     unset XROAD_TOKEN_PIN
 fi
 
