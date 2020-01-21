362ee69022dce0fb5edb510fdcffd81f61b4401b
Raul Martinez <raul.martinez@leadin.co.uk>
2020-01-21 17:05:57 +0100

    XRDDEV-678 Added generation of internal ssl certificates on the initial docker configuration script.

--- sidecar/files/ss-entrypoint.sh
+++ sidecar/files/ss-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+NAME=internal
+DIR=/etc/xroad/ssl
+XROAD_SCRIPT_LOCATION=/usr/share/xroad/scripts
+ARGS="-n "$NAME" -f -S -p"
+
 # Set xroad-autologin software token PIN code
 if [  -n "$XROAD_TOKEN_PIN" ]
 then
@@ -7,6 +12,10 @@ then
     su xroad -c 'echo $XROAD_TOKEN_PIN >/etc/xroad/autologin'
     unset XROAD_TOKEN_PIN
 fi
+
+# Generate internal TLS certificate
+set -- $ARGS
+$XROAD_SCRIPT_LOCATION/generate_certificate.sh $@
 
 # Start services
 exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
