215bc731914c23b705873e3008702a58207c1f93
Raul Martinez <raul.martinez@leadin.co.uk>
2020-01-28 23:34:51 +0100

    XRDDEV-678 Fixed typos. Removed unnecessary variables.

--- sidecar/files/ss-entrypoint.sh
+++ sidecar/files/ss-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+XROAD_SCRIPT_LOCATION=/usr/share/xroad/scripts
+
 # Set xroad-autologin software token PIN code
 if [  -n "$XROAD_TOKEN_PIN" ]
 then
@@ -8,19 +10,18 @@ then
     unset XROAD_TOKEN_PIN
 fi
 
-# Generate internal TLS certificates on the first run
-if [ -f $FILE ];
+# Generate internal and admin UI TLS certificates on the first run
+if [ ! -f /etc/xroad/ssl/internal.crt ];
 then
-    echo "Generating new webUI TLS key/certificate with $subj and $altn"
-    XROAD_SCRIPT_LOCATION=/usr/share/xroad/scripts
-    NAME=internal
-    ARGS="-n "$NAME" -f -S -p"
-    set -- $ARGS
+    echo "Generating new internal TLS key and certificate"
+    ARGS="-n internal -f -S -p"
     $XROAD_SCRIPT_LOCATION/generate_certificate.sh $ARGS
+fi
 
-    NAME=nginx
-    ARGS="-n "$NAME" -f -S -p"
-    set -- $ARGS
+if [ ! -f /etc/xroad/ssl/nginx.crt ];
+then
+    echo "Generating new SSL key and certificate for the admin UI"
+    ARGS="-n nginx -f -S -p"
     $XROAD_SCRIPT_LOCATION/generate_certificate.sh $ARGS
 fi
 
