fbc2606e3dff5788ecf28eb1f419cb9914332dc5
Raul Martinez <raul.martinez@leadin.co.uk>
2020-01-28 08:57:12 +0100

    XRDDEV-678 Addressed review comments. Added generation of nginx ssl certificates on the initial docker configuration script. Added check if certificates and keys exists. Added removal of certs and keys on first build.

--- sidecar/files/ss-entrypoint.sh
+++ sidecar/files/ss-entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-NAME=internal
-DIR=/etc/xroad/ssl
-XROAD_SCRIPT_LOCATION=/usr/share/xroad/scripts
-ARGS="-n "$NAME" -f -S -p"
-
 # Set xroad-autologin software token PIN code
 if [  -n "$XROAD_TOKEN_PIN" ]
 then
@@ -13,9 +8,21 @@ then
     unset XROAD_TOKEN_PIN
 fi
 
-# Generate internal TLS certificate
-set -- $ARGS
-$XROAD_SCRIPT_LOCATION/generate_certificate.sh $@
+# Generate internal TLS certificates on the first run
+if [ -f $FILE ];
+then
+    echo "Generating new webUI TLS key/certificate with $subj and $altn"
+    XROAD_SCRIPT_LOCATION=/usr/share/xroad/scripts
+    NAME=internal
+    ARGS="-n "$NAME" -f -S -p"
+    set -- $ARGS
+    $XROAD_SCRIPT_LOCATION/generate_certificate.sh $ARGS
+
+    NAME=nginx
+    ARGS="-n "$NAME" -f -S -p"
+    set -- $ARGS
+    $XROAD_SCRIPT_LOCATION/generate_certificate.sh $ARGS
+fi
 
 # Start services
 exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
