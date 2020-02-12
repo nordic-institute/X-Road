33be59d6a044086ff748556a02b43e7bf6fdd144
Janne Mattila <janne.mattila@gofore.com>
2020-02-12 12:19:10 +0200

    XRDDEV-812 remove unnecessary annotation

--- src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/cache/SecurityServerOwner.java
+++ src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/cache/SecurityServerOwner.java
@@ -28,11 +28,9 @@ import ee.ria.xroad.common.identifier.ClientId;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
 
 @Data
 @AllArgsConstructor
-@Slf4j
 public class SecurityServerOwner {
     private ClientId id;
 }
