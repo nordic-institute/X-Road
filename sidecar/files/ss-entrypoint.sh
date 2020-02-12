3fe99fa3aa3b375f04ca18c9b259b2f7bc192738
Raul Martinez <raul.martinez@leadin.co.uk>
2020-02-12 08:50:57 +0100

    XRDDEV-789 Added upgrade logic to initial docker configuration script. Added version output on image docker build file.

--- sidecar/files/ss-entrypoint.sh
+++ sidecar/files/ss-entrypoint.sh
@@ -2,6 +2,27 @@
 
 XROAD_SCRIPT_LOCATION=/usr/share/xroad/scripts
 
+INSTALLED_VERSION=$(dpkg-query --showformat='${Version}' --show xroad-proxy)
+
+# Update X-Road configuration on startup, if necessary
+if [ -f /etc/xroad/VERSION ]; then
+    IMAGE_VERSION="$(cat /etc/xroad/VERSION)"
+else
+    echo "WARN: Current configuration version not known" >&2
+    IMAGE_VERSION=
+fi
+if [ -n "$IMAGE_VERSION" ] && dpkg --compare-versions "$INSTALLED_VERSION" gt "$IMAGE_VERSION";
+then
+    echo "Updating configuration from $IMAGE_VERSION to $INSTALLED_VERSION"
+    pg_ctlcluster 10 main start
+    pg_isready -t 10
+    dpkg-reconfigure xroad-proxy
+    pg_ctlcluster 10 main stop
+    nginx -s stop
+    sleep 1
+    echo "$INSTALLED_VERSION" >/etc/xroad/version
+fi
+
 # Set xroad-autologin software token PIN code
 if [  -n "$XROAD_TOKEN_PIN" ]
 then
