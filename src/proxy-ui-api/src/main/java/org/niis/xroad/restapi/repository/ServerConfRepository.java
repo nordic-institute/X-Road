d3ba274782b13c467aa085704b67017602266287
Janne Mattila <janne.mattila@gofore.com>
2020-02-19 17:41:06 +0200

    XRDDEV-860 remove unused serverConfRepository.saveOrUpdate

--- src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/repository/ServerConfRepository.java
+++ src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/repository/ServerConfRepository.java
@@ -56,13 +56,4 @@ public class ServerConfRepository {
         ServerConfDAOImpl serverConfDAO = new ServerConfDAOImpl();
         return serverConfDAO.getConf(persistenceUtils.getCurrentSession());
     }
-
-    /**
-     * Executes a Hibernate saveOrUpdate(serverConfType)
-     * @param serverConfType
-     */
-    public void saveOrUpdate(ServerConfType serverConfType) {
-        persistenceUtils.getCurrentSession().saveOrUpdate(serverConfType);
-    }
-
 }
