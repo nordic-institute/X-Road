2c0d384b6615980eb8491c69be1abb7b83881f52
Raul Martinez <raul.martinez@leadin.co.uk>
2020-02-25 22:53:36 +0100

    XRDDEV-786 Addressed review comments. Remove the serverconf database, username and properties file in the image creation. Added logic to recreate serverconf database with user-supplied credentials.

--- sidecar/files/ss-entrypoint.sh
+++ sidecar/files/ss-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 XROAD_SCRIPT_LOCATION=/usr/share/xroad/scripts
+DB_PROPERTIES=/etc/xroad/db.properties
+DB_URL=jdbc:postgresql://127.0.0.1:5432/serverconf
+DB_NAME=serverconf
 
 INSTALLED_VERSION=$(dpkg-query --showformat='${Version}' --show xroad-proxy)
 PACKAGED_VERSION="$(cat /root/VERSION)"
@@ -27,6 +30,34 @@ if [ "$INSTALLED_VERSION" == "$PACKAGED_VERSION" ]; then
 else
     echo "WARN: Installed version ($INSTALLED_VERSION) does not match packaged version ($PACKAGED_VERSION)" >&2
 fi
+
+# Set xroad-autologin software token PIN code
+if [  -n "$XROAD_TOKEN_PIN" ]
+then
+    echo "XROAD_TOKEN_PIN variable set, writing to /etc/xroad/autologin"
+    su xroad -c 'echo $XROAD_TOKEN_PIN >/etc/xroad/autologin'
+    unset XROAD_TOKEN_PIN
+fi
+
+# Recreate serverconf database and properties file with user-supplied username and password
+echo "Creating serverconf database with user-supplied credentials"
+pg_ctlcluster 10 main start
+su - postgres -c "psql postgres -tAc \"CREATE ROLE ${XROAD_DB_USER} LOGIN PASSWORD '${XROAD_DB_PASSWD}';\""
+su - postgres -c "createdb ${DB_NAME} -O ${XROAD_DB_USER} -E UTF-8"
+su - postgres -c "psql ${DB_NAME} -tAc \"CREATE EXTENSION IF NOT EXISTS hstore\""
+cd /usr/share/xroad/db/
+/usr/share/xroad/db/liquibase.sh --classpath=/usr/share/xroad/jlib/proxy.jar --url="${DB_URL}?dialect=ee.ria.xroad.common.db.CustomPostgreSQLDialect" --changeLogFile=/usr/share/xroad/db/${DB_NAME}-changelog.xml --password=${XROAD_DB_PASSWD} --username=${XROAD_DB_USER}  update || die "Connection to database has failed, please check database availability and configuration in ${DB_PROPERTIES} file"
+pg_ctlcluster 10 main stop
+
+touch $DB_PROPERTIES
+crudini --set $DB_PROPERTIES '' serverconf.hibernate.jdbc.use_streams_for_binary true
+crudini --set $DB_PROPERTIES '' serverconf.hibernate.dialect ee.ria.xroad.common.db.CustomPostgreSQLDialect
+crudini --set $DB_PROPERTIES '' serverconf.hibernate.connection.driver_class org.postgresql.Driver
+crudini --set $DB_PROPERTIES '' serverconf.hibernate.connection.url $DB_URL
+crudini --set $DB_PROPERTIES '' serverconf.hibernate.connection.username  $XROAD_DB_USER
+crudini --set $DB_PROPERTIES '' serverconf.hibernate.connection.password $XROAD_DB_PASSWD
+chown xroad:xroad $DB_PROPERTIES
+chmod 640 $DB_PROPERTIES
 
 # Generate internal and admin UI TLS keys and certificates on the first run
 if [ ! -f /etc/xroad/ssl/internal.crt ];
