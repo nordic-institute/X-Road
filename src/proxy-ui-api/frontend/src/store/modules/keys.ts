5cfcee7d114442af6854613bfbae05c471c9a796
Mikko Riippi <mikko.riippi@gofore.com>
2020-02-26 16:58:20 +0200

    XRDDEV-857 Add tokens to store

--- src/proxy-ui-api/frontend/src/store/modules/keys.ts
+++ src/proxy-ui-api/frontend/src/store/modules/keys.ts
@@ -2,6 +2,7 @@ import { ActionTree, GetterTree, Module, MutationTree } from 'vuex';
 import { RootState } from '../types';
 import { Key, Token, TokenType, TokenCertificate } from '@/types';
 import axios from 'axios';
+import _ from 'lodash';
 import * as api from '@/util/api';
 
 
@@ -17,16 +18,105 @@ export const tokensState: KeysState = {
   selectedToken: undefined,
 };
 
-export const getters: GetterTree<KeysState, RootState> = {
+export const tokensGetters: GetterTree<KeysState, RootState> = {
   tokenExpanded: (state) => (id: string) => {
     return state.expandedTokens.includes(id);
   },
   tokens(state): Token[] {
     return state.tokens;
   },
+  sortedTokens(state): Token[] {
+
+    if (!state.tokens || state.tokens.length === 0) {
+      return [];
+    }
+
+    // Sort array by id:s so it doesn't jump around. Order of items in the backend reply changes between requests.
+    const arr = _.cloneDeep(state.tokens).sort((a: Token, b: Token) => {
+      if (a.id < b.id) {
+        return -1;
+      }
+      if (a.id > b.id) {
+        return 1;
+      }
+
+      // equal id:s. (should not happen)
+      return 0;
+    });
+
+    return arr;
+
+  },
   selectedToken(state): Token | undefined {
     return state.selectedToken;
   },
+  filteredTokens: (state, getters) => (search: string) => {
+    // Filter term is applied to token namem key name and certificate owner id
+    let arr = _.cloneDeep(getters.sortedTokens);
+
+    if (!search) {
+      return arr;
+    }
+
+    const mysearch = search.toLowerCase();
+
+    if (mysearch.length < 1) {
+      return state.tokens;
+    }
+
+    arr.forEach((token: Token) => {
+      token.keys.forEach((key: Key) => {
+        const certs = key.certificates.filter((cert: TokenCertificate) => {
+          if (cert.owner_id) {
+            return cert.owner_id.toLowerCase().includes(mysearch);
+          }
+          return false;
+        });
+        key.certificates = certs;
+      });
+    });
+
+    arr.forEach((token: Token) => {
+      const keys = token.keys.filter((key: Key) => {
+        if (key.certificates && key.certificates.length > 0) {
+          return true;
+        }
+
+        if (key.name) {
+          return key.name.toLowerCase().includes(mysearch);
+        }
+        return false;
+      });
+      token.keys = keys;
+    });
+
+    arr = arr.filter((token: Token) => {
+      if (token.keys && token.keys.length > 0) {
+        return true;
+      }
+
+      return token.name.toLowerCase().includes(mysearch);
+    });
+
+    return arr;
+  },
+
+  tokensFilteredByName: (state, getters) => (search: string) => {
+    // Filter term is applied to token name
+    const arr = getters.sortedTokens;
+
+    if (!search || search.length < 1) {
+      return arr;
+    }
+
+    const mysearch = search.toLowerCase();
+
+    return arr.filter((token: Token) => {
+      return token.name.toLowerCase().includes(mysearch);
+    });
+
+  },
+
 };
 
 export const mutations: MutationTree<KeysState> = {
@@ -107,7 +197,7 @@ export const actions: ActionTree<KeysState, RootState> = {
 export const keysModule: Module<KeysState, RootState> = {
   namespaced: false,
   state: tokensState,
-  getters,
+  getters: tokensGetters,
   actions,
   mutations,
 };
