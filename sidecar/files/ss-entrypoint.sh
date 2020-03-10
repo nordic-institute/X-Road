02f52699bfb5fd2d0f3e13296b9ccd15e5c55f04
Raul Martinez <raul.martinez@leadin.co.uk>
2020-03-10 09:36:20 +0100

    XRDDEV-789 Addressed review comments. Adding back up of read-only configuration on image build and logic to restore it in the entrypoint script if the image version is newer than the existing version.

--- sidecar/files/ss-entrypoint.sh
+++ sidecar/files/ss-entrypoint.sh
@@ -3,24 +3,29 @@
 XROAD_SCRIPT_LOCATION=/usr/share/xroad/scripts
 
 INSTALLED_VERSION=$(dpkg-query --showformat='${Version}' --show xroad-proxy)
+PACKAGED_VERSION="$(cat /root/VERSION)"
 
 # Update X-Road configuration on startup, if necessary
-if [ -f /etc/xroad/VERSION ]; then
-    IMAGE_VERSION="$(cat /etc/xroad/VERSION)"
+if [ "$INSTALLED_VERSION" == "$PACKAGED_VERSION" ]; then
+    if [ -f /etc/xroad/VERSION ]; then
+        CONFIG_VERSION="$(cat /etc/xroad/VERSION)"
+    else
+        echo "WARN: Current configuration version not known" >&2
+        CONFIG_VERSION=
+    fi
+    if [ -n "$CONFIG_VERSION" ] && dpkg --compare-versions "$PACKAGED_VERSION" gt "$CONFIG_VERSION"; then
+        echo "Updating configuration from $CONFIG_VERSION to $PACKAGED_VERSION"
+        cp -a /root/etc/xroad/* /etc/xroad/
+        pg_ctlcluster 10 main start
+        pg_isready -t 10
+        dpkg-reconfigure xroad-proxy
+        pg_ctlcluster 10 main stop
+        nginx -s stop
+        sleep 1
+        echo "$PACKAGED_VERSION" >/etc/xroad/version
+    fi
 else
-    echo "WARN: Current configuration version not known" >&2
-    IMAGE_VERSION=
-fi
-if [ -n "$IMAGE_VERSION" ] && dpkg --compare-versions "$INSTALLED_VERSION" gt "$IMAGE_VERSION";
-then
-    echo "Updating configuration from $IMAGE_VERSION to $INSTALLED_VERSION"
-    pg_ctlcluster 10 main start
-    pg_isready -t 10
-    dpkg-reconfigure xroad-proxy
-    pg_ctlcluster 10 main stop
-    nginx -s stop
-    sleep 1
-    echo "$INSTALLED_VERSION" >/etc/xroad/version
+    echo "WARN: Installed version ($INSTALLED_VERSION) does not match packaged version ($PACKAGED_VERSION)" >&2
 fi
 
 # Set xroad-autologin software token PIN code
