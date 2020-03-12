833eb4b6e38adb4dbd2f07fbe2e83208c84aa4c6
Caro Hautamäki <caro.hautamaki@gofore.com>
2020-03-12 16:43:53 +0200

    XRDDEV-930 List backups
    * backups list and filtering
    * non-functional buttons in place
    * moved some scoped styles to global-style and tables
    * fixed console errors on client list

---
+++ src/proxy-ui-api/frontend/src/views/Settings/BackupAndRestore/BackupAndRestore.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="wrapper xrd-view-common">
+    <div class="table-toolbar">
+      <v-text-field
+        v-model="search"
+        :label="$t('action.search')"
+        single-line
+        hide-details
+        class="search-input"
+        data-test="backup-search"
+      >
+        <v-icon slot="append">mdi-magnify</v-icon>
+      </v-text-field>
+      <div>
+        <v-btn
+          v-if="canBackup"
+          color="primary"
+          rounded
+          dark
+          class="button-spacing rounded-button elevation-0"
+          data-test="backup-create-configuration"
+          >{{ $t('backup.backupConfiguration') }}
+        </v-btn>
+        <v-btn
+          v-if="canBackup"
+          color="primary"
+          rounded
+          dark
+          class="button-spacing rounded-button elevation-0"
+          data-test="backup-upload"
+          >{{ $t('backup.uploadBackup') }}
+        </v-btn>
+      </div>
+    </div>
+    <BackupsDataTable :canBackup="canBackup" :filter="search" />
+  </div>
+</template>
+
+<script lang="ts">
+/**
+ * View for 'backup and restore' tab. Not implemented yet.
+ */
+import Vue from 'vue';
+import BackupsDataTable from '@/views/Settings/BackupAndRestore/BackupsDataTable.vue';
+import { Permissions } from '@/global';
+
+export default Vue.extend({
+  components: {
+    BackupsDataTable,
+  },
+  data() {
+    return {
+      search: '',
+    };
+  },
+  computed: {
+    canBackup(): boolean {
+      return this.$store.getters.hasPermission(
+        Permissions.BACKUP_CONFIGURATION,
+      );
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '../../../assets/tables';
+.search-input {
+  max-width: 300px;
+}
+
+.button-spacing {
+  margin-left: 20px;
+}
+</style>
--- src/proxy-ui-api/frontend/src/views/Settings/Settings.vue
+++ src/proxy-ui-api/frontend/src/views/Settings/Settings.vue
@@ -1,11 +1,21 @@
 <template>
-  <v-container>
-    <v-tabs v-model="tab" class="xrd-tabs" color="secondary" grow slider-size="4">
+  <div class="wrapper xrd-view-common">
+    <v-tabs
+      v-model="tab"
+      class="xrd-tabs"
+      color="secondary"
+      grow
+      slider-size="4"
+    >
       <v-tabs-slider color="secondary"></v-tabs-slider>
-      <v-tab v-for="tab in tabs" v-bind:key="tab.key" :to="tab.to">{{ $t(tab.name) }}</v-tab>
+      <v-tab v-for="tab in tabs" v-bind:key="tab.key" :to="tab.to">
+        {{ $t(tab.name) }}</v-tab
+      >
     </v-tabs>
-    <router-view />
-  </v-container>
+    <div class="content">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -36,4 +46,8 @@ export default Vue.extend({
   },
 });
 </script>
-
+<style lang="scss">
+.content {
+  width: 1000px;
+}
+</style>
