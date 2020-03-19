b408f382bce659762097b64ac87666993627e101
Raido Kaju <raido.kaju@niis.org>
2020-03-19 10:24:09 +0200

    XRDDEV-889: Add timestamping service

--- src/proxy-ui-api/frontend/src/views/Settings/SystemParameters/SystemParameters.vue
+++ src/proxy-ui-api/frontend/src/views/Settings/SystemParameters/SystemParameters.vue
@@ -34,9 +34,7 @@
         <v-row no-gutters class="mt-10" v-if="hasPermission(permissions.VIEW_TSPS)">
           <v-col><h3>{{$t('systemParameters.timestampingServices.title')}}</h3></v-col>
           <v-col class="text-right">
-            <large-button data-test="system-parameters-timestamping-services-add-button" outlined :requires-permission="permissions.ADD_TSP">
-              {{$t('systemParameters.timestampingServices.action.add')}}
-            </large-button>
+            <add-timestamping-service-dialog :configured-timestamping-services="configuredTimestampingServices" @added="fetchConfiguredTimestampingServiced"/>
           </v-col>
         </v-row>
         <v-row no-gutters v-if="hasPermission(permissions.VIEW_TSPS)">
@@ -99,11 +97,13 @@ import SmallButton from '@/components/ui/SmallButton.vue';
 import { Anchor, CertificateAuthority, TimestampingService } from '@/types';
 import * as api from '@/util/api';
 import { Permissions } from '@/global';
+import AddTimestampingServiceDialog from '@/views/Settings/SystemParameters/AddTimestampingServiceDialog.vue';
 
 export default Vue.extend({
   components: {
     LargeButton,
     SmallButton,
+    AddTimestampingServiceDialog,
   },
   data() {
     return {
