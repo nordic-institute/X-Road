52b18c668ee3bf23d69463cb01ea7e68dde221df
Olli Ruotsalainen <ollijr@gmail.com>
2020-03-24 01:15:03 -0700

    Xrddev 875 localgroups members e2etests (#391)
    * XRDDEV-459 clientslist e2e tests
    
    Add e2e test for security server clients list.
    
    * XRDDEV-459 clientslist e2e tests
    
    Change to chrome style sorting, add more test data
    
    * XRDDEV-459 clientslist e2e tests
    
    Change test data
    
    * XRDDEV-460 client details e2e tests
    
    Add test for client details and update page object
    
    * XRDDEV-327 logout e2e tests
    
    Add test for logout and timed logout, update page object and global settings
    
    * XRDDEV-841 client local groups filtering e2e tests
    
    Add e2e test for filtering client local groups
    Change default selector to xpath
    Move clearValue2 to common library
    
    * XRDDEV-841 client local groups filtering e2e tests
    
    Fixed json error
    
    * XRDDEV-842 client local groups add new group e2e tests
    
    Add new e2e tests for add  local group dialog
    Change to use data-test attributes
    Fix to client details certificate UI change
    
    * XRDDEV-842 local group add new group e2e tests
    
    Fix chromedriver version and settings.
    
    * XRDDEV-842 local group add new group e2e tests
    
    Fix logout test.
    
    * XRDDEV-875 clients localgroup members e2e tests
    
    Add localgroup member e2e tests
    
    * XRDDEV-875 localgroup members e2etests
    
    Implement review changes

--- src/proxy-ui-api/frontend/tests/e2e/pages/ssMainPage.js
+++ src/proxy-ui-api/frontend/tests/e2e/pages/ssMainPage.js
@@ -15,6 +15,20 @@ var navigateCommands = {
   openSettingsTab: function() {
     this.click('@settingsTab');
     return this;
+  },
+  logout: function() {
+    this.click('@userMenuButton');
+    this.pause(1000);
+    this.click('@userMenuitemLogout');
+    return this;
+  },
+  acceptLogout: function() {
+    this.click('@logoutOKButton');
+    return this;
+  },
+  closeSnackbar: function() {
+    this.click('@snackBarCloseButton');
+    return this;
   }
 };
 
@@ -62,21 +76,137 @@ var clientInfoCommands = {
     this.click('@localGroupsTab');
     return this;
   }
-}
+};
 
 var clientDetailsCommands = {
   openSignCertificateInfo: function() {
     this.click('@clientSignCertificate');
     return this;
   }
-}
+};
 
 var certificatePopupCommands = {
   close: function() {
-    this.click('@certificateInfoOKButton');
+    this.click('@certificateInfoCloseButton');
     return this;
   }
-}
+};
+
+var localGroupPopupCommands = {
+  changeCode: function(code) {
+    this.clearValue2('@localGroupCode');
+    this.setValue('@localGroupCode', code);
+    return this;
+  },
+  changeDescription: function(description) {
+    this.clearValue2('@localGroupDescription');
+    this.setValue('@localGroupDescription', description);
+    return this;
+  },
+  deleteThisGroup: function() {
+    this.click('@localGroupDeleteButton');
+    return this;
+  },
+  openAddMembers: function() {
+    this.click('@localGroupAddMembersButton');
+    return this;
+  },
+  searchMembers: function() {
+    this.click('@localGroupSearchWrap');
+    this.click('@localGroupSearchButton');
+    return this;
+  },
+  addSelectedMembers: function() {
+    this.click('@localGroupAddSelectedButton');
+    return this;
+  },
+  cancelAddMembersDialog: function() {
+    this.click('@localGroupCancelAddButton');
+    return this;
+  },
+  selectNewTestComMember: function() {
+    this.click('@localGroupTestComCheckbox');
+    return this;
+  },
+  clickRemoveAll: function() {
+    this.click('@localGroupRemoveAllButton');
+    return this;
+  },
+  clickRemoveTestComMember: function() {
+    this.click('@localGroupTestComRemoveButton');
+    return this;
+  },
+  confirmMemberRemove: function() {
+    this.click('@localGroupRemoveYesButton');
+    return this;
+  },
+  cancelMemberRemove: function() {
+    this.click('@localGroupRemoveCancelButton');
+    return this;
+  },
+  confirmDelete: function() {
+    this.click('@localGroupRemoveYesButton');
+    return this;
+  },
+  cancelDelete: function() {
+    this.click('@localGroupRemoveCancelButton');
+    return this;
+  },
+  clickHeader: function() {
+    this.click('@localGroupCode');
+    return this;
+  },
+  close: function() {
+    this.click('@localGroupPopupCloseButton');
+    return this;
+  }
+};
+
+var clientLocalGroupsCommands = {
+  openAddLocalGroupDialog: function() {
+    this.click('@addGroupButton');
+    return this;
+  },
+  filterBy: function(filter) {
+    this.clearValue2('@filterInput');
+    this.setValue('@filterInput', filter);
+    return this;
+  },
+  openAddDialog: function() {
+    this.click('@addGroupButton');
+    return this;
+  },
+  confirmAddDialog: function() {
+    this.click('@confirmAddButton');
+    return this;
+  },
+  cancelAddDialog: function() {
+    this.click('@cancelAddButton');
+    return this;
+  },
+  enterCode: function(code) {
+    this.clearValue2('@groupCode');
+    this.setValue('@groupCode', code);
+    return this;
+  },
+  enterDescription: function(description) {
+    this.clearValue2('@groupDescription');
+    this.setValue('@groupDescription', description);
+    return this;
+  },
+  openAbbDetails: function() {
+    this.click('@groupCodeCellAbb');
+    return this;
+  },
+  openBacDetails: function() {
+    this.click('@groupCodeCellBac');
+    return this;
+  },
+  openCbbDetails: function() {
+    this.click('@groupCodeCellCbb');
+    return this;
+  }
+};
 
 module.exports = {
   url: process.env.VUE_DEV_SERVER_URL,
@@ -96,7 +226,16 @@ module.exports = {
       locateStrategy: 'xpath' },
     userMenuButton: { 
       selector: 'div.v-toolbar__content button .mdi-account-circle', 
-      locateStrategy: 'css selector' }
+      locateStrategy: 'css selector' },
+    userMenuitemLogout: { 
+      selector: '#logout-list-tile',
+      locateStrategy: 'css selector' },
+    logoutOKButton: { 
+      selector: '//div[contains(@class, "v-dialog")]//button[.//*[contains(text(), "Ok")]]', 
+      locateStrategy: 'xpath' },
+    snackBarCloseButton: { 
+      selector: '//div[contains(@class, "v-snack__content")]//button[.//*[contains(text(), "Close")]]', 
+      locateStrategy: 'xpath' }
   },
   sections: {
     clientsTab: {
@@ -155,16 +294,117 @@ module.exports = {
               selector: 'span.cert-name',
               locateStrategy: 'css selector' }
           }      
+        },
+        localGroups: {
+          selector: '//div[contains(@class, "xrd-view-common") and .//*[contains(@class, "v-tab--active") and contains(text(), "local groups")]]',
+          locateStrategy: 'xpath',
+          commands: [clientLocalGroupsCommands],
+          elements: {
+            filterInput: { 
+              selector: '//input',
+              locateStrategy: 'xpath' },
+            addGroupButton: { 
+              selector: '//button[.//*[contains(text(), "Add group")]]',
+              locateStrategy: 'xpath' },
+            confirmAddButton: { 
+              selector: '//button[@data-test="dialog-save-button"]',
+              locateStrategy: 'xpath' },
+            cancelAddButton: {
+              selector: '//button[@data-test="dialog-cancel-button"]',
+              locateStrategy: 'xpath' },
+            groupCode: { 
+              selector: '//div[contains(@class, "dlg-edit-row") and .//*[contains(text(), "Code")]]//input',
+              locateStrategy: 'xpath' },
+            groupDescription: { 
+              selector: '//div[contains(@class, "dlg-edit-row") and .//*[contains(text(), "Description")]]//input',
+              locateStrategy: 'xpath' },
+            groupCodeCellAbb: { 
+              selector: '//table[contains(@class, "details-certificates")]//span[contains(text(),"abb")]',
+              locateStrategy: 'xpath' },
+            groupCodeCellBac: { 
+              selector: '//table[contains(@class, "details-certificates")]//span[contains(text(),"bac")]',
+              locateStrategy: 'xpath' },
+            groupCodeCellCbb: { 
+              selector: '//table[contains(@class, "details-certificates")]//span[contains(text(),"cbb")]',
+              locateStrategy: 'xpath' },
+            abbDetails: { 
+              selector: '//span[contains(text(),"abb")]',
+              locateStrategy: 'xpath' },
+            bacDetails: { 
+              selector: '//span[contains(text(),"bac")]',
+              locateStrategy: 'xpath' },
+            bacDetails: { 
+              selector: '//span[contains(text(),"cbb")]',
+              locateStrategy: 'xpath' }
+          }      
         }
       }     
     },
     certificatePopup: {
-      selector: '//*[contains(@class,"v-dialog--active") and .//*[contains(@class, "headline") and contains(text(),"Certificate")]]',
+      selector: '//div[contains(@class, "xrd-view-common") and .//span[contains(@class, "cert-headline") and contains(text(),"Certificate")]]',
       locateStrategy: 'xpath',
       commands: [certificatePopupCommands],
       elements: {
-        certificateInfoOKButton: { 
-          selector: '.v-dialog--active button',
+        certificateInfoCloseButton: { 
+          selector: 'div.cert-dialog-header #close-x',
+          locateStrategy: 'css selector' }
+      }
+    },
+    localGroupPopup: {
+      selector: '//div[contains(@class, "xrd-tab-max-width") and .//div[contains(@class, "cert-hash") and contains(text(),"Local group")]]',
+      locateStrategy: 'xpath',
+      commands: [localGroupPopupCommands],
+      elements: {
+        localGroupAddMembersButton: { 
+          selector: '//button[.//*[contains(text(), "Add Members")]]',
+          locateStrategy: 'xpath' },
+        localGroupRemoveAllButton: { 
+          selector: '//button[.//*[contains(text(), "Remove All")]]',
+          locateStrategy: 'xpath' },
+        localGroupDeleteButton: { 
+          selector: '//button[.//*[contains(text(), "Delete")]]',
+          locateStrategy: 'xpath' },
+        localGroupAddSelectedButton: { 
+          selector: '//button[.//*[contains(text(), "Add selected")]]',
+          locateStrategy: 'xpath' },
+        localGroupSearchButton: {
+          selector: '//button[.//*[contains(text(), "Search")]]',
+          locateStrategy: 'xpath' },
+        localGroupCancelAddButton: {
+          selector: '//button[.//*[contains(text(), "Cancel")]]',
+          locateStrategy: 'xpath' },
+        localGroupTestComCheckbox: {
+          selector: '//tr[.//*[contains(text(), "TestCom")]]//*[contains(@class, "v-input--selection-controls__ripple")]',
+          locateStrategy: 'xpath' },
+        localGroupRemoveMemberButton: { 
+          selector: '//button[.//*[contains(text(), "Add group")]]',
+          locateStrategy: 'xpath' },
+        localGroupSearchWrap: {
+          selector: '//div[contains(@class, "search-wrap")]',
+          locateStrategy: 'xpath' },
+        localGroupRemoveYesButton: {
+          selector: '//button[contains(@data-test, "dialog-save-button")]',
+          locateStrategy: 'xpath' },
+        localGroupRemoveCancelButton: {
+          selector: '//button[contains(@data-test, "dialog-cancel-button")]',
+          locateStrategy: 'xpath' },
+        localGroupTestComRemoveButton: {
+          selector: '//tr[.//*[contains(text(), "TestCom")]]//button[.//*[contains(text(), "Remove")]]',
+          locateStrategy: 'xpath' },
+        localGroupTestGovRemoveButton: {
+          selector: '//tr[.//*[contains(text(), "TestGov")]]//button[.//*[contains(text(), "Remove")]]',
+          locateStrategy: 'xpath' },
+        localGroupTestOrgRemoveButton: {
+          selector: '//tr[.//*[contains(text(), "TestOrg")]]//button[.//*[contains(text(), "Remove")]]',
+          locateStrategy: 'xpath' },
+        localGroupCode: { 
+          selector: '//div[contains(@class, "cert-dialog-header")]//*[contains(@class, "cert-headline")]',
+          locateStrategy: 'xpath' },
+        localGroupDescription: { 
+          selector: '//div[contains(@class, "description-input")]//input',
+          locateStrategy: 'xpath' },
+        localGroupPopupCloseButton: { 
+          selector: 'div.cert-dialog-header #close-x',
           locateStrategy: 'css selector' }
       }
     }
