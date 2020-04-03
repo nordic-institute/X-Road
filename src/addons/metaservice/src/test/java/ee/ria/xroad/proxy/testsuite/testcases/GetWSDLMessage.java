5cc4c894f0d7b03292c183f20f7997f007c30295
Petteri Kivimäki <petteri.kivimaki@niis.org>
2020-04-03 16:38:42 +0300

    XRDDEV-825 Fix failing getWsdl test
    - Add getWsdl.query file.

--- src/addons/metaservice/src/test/java/ee/ria/xroad/proxy/testsuite/testcases/GetWSDLMessage.java
+++ src/addons/metaservice/src/test/java/ee/ria/xroad/proxy/testsuite/testcases/GetWSDLMessage.java
@@ -33,14 +33,12 @@ import ee.ria.xroad.common.conf.serverconf.model.ServiceDescriptionType;
 import ee.ria.xroad.common.conf.serverconf.model.ServiceType;
 import ee.ria.xroad.common.identifier.ClientId;
 import ee.ria.xroad.common.util.AbstractHttpSender;
-import ee.ria.xroad.common.util.MimeTypes;
 import ee.ria.xroad.proxy.testsuite.Message;
 import ee.ria.xroad.proxy.testsuite.MessageTestCase;
 import ee.ria.xroad.proxy.testsuite.TestSuiteServerConf;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
-import org.apache.james.mime4j.MimeException;
 import org.apache.james.mime4j.stream.BodyDescriptor;
 import org.xml.sax.InputSource;
 
@@ -48,6 +46,7 @@ import javax.wsdl.Definition;
 import javax.wsdl.WSDLException;
 import javax.wsdl.factory.WSDLFactory;
 import javax.wsdl.xml.WSDLReader;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
@@ -57,11 +56,10 @@ import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static ee.ria.xroad.common.conf.serverconf.ServerConfDatabaseCtx.doInTransaction;
-import static ee.ria.xroad.proxy.util.MetaserviceTestUtil.DUMMY_QUERY_FILE;
 import static ee.ria.xroad.proxy.util.MetaserviceTestUtil.cleanDB;
 import static ee.ria.xroad.proxy.util.MetaserviceTestUtil.parseOperationNamesFromWSDLDefinition;
 import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.startsWith;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -94,8 +92,7 @@ public class GetWSDLMessage extends MessageTestCase {
      * Constructs the test case.
      */
     public GetWSDLMessage() {
-        // the contents of this file are not used for testing, MessageTestCase just needs it to be valid SOAP
-        this.requestFileName = DUMMY_QUERY_FILE;
+        this.requestFileName = "getWsdl.query";
         this.mockServer = new WireMockServer(options().port(WSDL_SERVER_PORT));
     }
 
@@ -113,7 +110,7 @@ public class GetWSDLMessage extends MessageTestCase {
 
         List<String> operationNames = parseOperationNamesFromWSDLDefinition(definition);
 
-        assertThat(wsdl.getContentType(), is(MimeTypes.TEXT_XML));
+        assertThat(wsdl.getContentType(), startsWith("multipart/related; type=\"text/xml\"; charset=UTF-8;"));
 
         assertThat("Expected to find certain operations",
                 operationNames,
@@ -200,7 +197,7 @@ public class GetWSDLMessage extends MessageTestCase {
         }
 
         protected class WSDLMessageHandler extends MessageContentHandler {
-
+            private int nextPart = 0;
             private WSDLReader wsdlReader;
 
             WSDLMessageHandler() throws WSDLException {
@@ -208,13 +205,20 @@ public class GetWSDLMessage extends MessageTestCase {
             }
 
             @Override
-            public void body(BodyDescriptor bd, InputStream is) throws MimeException, IOException {
+            public void body(BodyDescriptor bd, InputStream is) throws IOException {
 
-                try {
-                    definition = wsdlReader.readWSDL(null, new InputSource(is));
-                } catch (WSDLException e) {
-                    throw new IOException(e);
+                switch (nextPart) {
+                    case 0: // SOAP
+                        nextPart = 1;
+                        break;
+                    default: // Attachment => WSDL
+                        try {
+                            definition = wsdlReader.readWSDL(null, new InputSource(is));
+                        } catch (WSDLException e) {
+                            throw new IOException(e);
 
+                        }
+                        break;
                 }
             }
         }
