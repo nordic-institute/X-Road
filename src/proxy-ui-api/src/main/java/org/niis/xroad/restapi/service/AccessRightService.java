94da0cc968c568b6a76c782cdf4e021ddba9a29b
Tapio Jaakkola <tapio.jaakkola@github.mail.kapsi.fi>
2020-04-09 18:10:20 +0300

    XRDDEV-960: Add comments, Javadocs and brief definion for concept of base endpoint

--- src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/service/AccessRightService.java
+++ src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/service/AccessRightService.java
@@ -96,12 +96,25 @@ public class AccessRightService {
         this.endpointService = endpointService;
     }
 
+    /**
+     * Get AccessRightHolderDtos for given client
+     *
+     * The concept of base endpoint is used to find service level access rights in this method.
+     * Base endpoint is in other words service (code) level endpoint.
+     * Each service has one base endpoint.
+     * Base endpoint has method '*' and path '**'.
+     *
+     * @param clientId
+     * @return
+     * @throws ClientNotFoundException
+     */
     public List<AccessRightHolderDto> getAccessRightHoldersByClient(ClientId clientId)
             throws ClientNotFoundException {
         ClientType clientType = clientRepository.getClient(clientId);
         if (clientType == null) {
             throw new ClientNotFoundException("Client " + clientId.toShortString() + " not found");
         }
+
         // Filter just acls that are set to base endpoints so they are on service code level
         List<AccessRightType> serviceCodeLevelAcls = clientType.getAcl().stream()
                 .filter(acl -> acl.getEndpoint().isBaseEndpoint())
@@ -110,6 +123,7 @@ public class AccessRightService {
         return mapAccessRightsToAccessRightHolders(clientType, distinctAccessRightTypes);
     }
 
+    // Get unique AccessRightTypes from the given list
     private List<AccessRightType> distinctAccessRightTypeByXroadId(List<AccessRightType> acls) {
         HashMap<XRoadId, AccessRightType> uniqueServiceClientMap = new HashMap<>();
         for (AccessRightType acl : acls) {
