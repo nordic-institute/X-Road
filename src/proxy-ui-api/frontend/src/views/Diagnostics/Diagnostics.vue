2d914e2469af44b08dec2db067047669fceaea7a
Mikko Riippi <mikko.riippi@gofore.com>
2020-04-14 16:55:28 +0300

    XRDDEV-936 Add real backend data

--- src/proxy-ui-api/frontend/src/views/Diagnostics/Diagnostics.vue
+++ src/proxy-ui-api/frontend/src/views/Diagnostics/Diagnostics.vue
@@ -16,10 +16,10 @@
           </thead>
           <tbody>
             <tr>
-              <td>x</td>
-              <td class="service-url" data-test="service-url">Message, hello</td>
-              <td>12:00</td>
-              <td>12:00</td>
+              <td>{{globalconf.status_class}}</td>
+              <td class="service-url" data-test="service-url">{{globalconf.status_code}}</td>
+              <td>{{globalconf.prev_update_at}}</td>
+              <td>{{globalconf.next_update_at}}</td>
             </tr>
           </tbody>
         </table>
@@ -41,13 +41,17 @@
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>x</td>
+            <tr
+              v-for="timestampingService in timestapmingServices"
+              v-bind:key="timestampingService.url"
+            >
+              <td>{{timestampingService.status_class }}</td>
               <td class="service-url" data-test="service-url">
-                <serviceIcon :service="service" />https://x-road.tee
+                <serviceIcon :service="service" />
+                {{timestampingService.url}}
               </td>
-              <td>message, hello!</td>
-              <td>12:00</td>
+              <td>{{$t('diagnostics.timestamping.timestampingStatus.'+timestampingService.status_code)}}</td>
+              <td>{{timestampingService.prev_update_at}}</td>
             </tr>
           </tbody>
         </table>
@@ -59,27 +63,38 @@
         <span class="headline">{{$t('diagnostics.ocspResponders.title')}}</span>
       </v-card-title>
       <v-card-text class="pt-4">
-        {{$t('diagnostics.ocspResponders.certificationService')}}
-        <table class="xrd-table">
-          <thead>
-            <tr>
-              <th>{{$t('diagnostics.status')}}</th>
-              <th>{{$t('diagnostics.serviceUrl')}}</th>
-              <th>{{$t('diagnostics.message')}}</th>
-              <th>{{$t('diagnostics.previousUpdate')}}</th>
-              <th>{{$t('diagnostics.nextUpdate')}}</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>o</td>
-              <td class="service-url" data-test="service-url">https://vauva.fi</td>
-              <td>hello?</td>
-              <td>12:00</td>
-              <td>12:00</td>
-            </tr>
-          </tbody>
-        </table>
+        <div
+          v-for="ocspDiags in ocspResponderDiagnostics"
+          v-bind:key="ocspDiags.distinguished_name"
+        >
+          <div class="cert-service-name">
+            <span>{{$t('diagnostics.ocspResponders.certificationService')}}</span>
+
+            {{ocspDiags.distinguished_name}}
+          </div>
+          <table class="xrd-table">
+            <thead>
+              <tr>
+                <th>{{$t('diagnostics.status')}}</th>
+                <th>{{$t('diagnostics.serviceUrl')}}</th>
+                <th>{{$t('diagnostics.message')}}</th>
+                <th>{{$t('diagnostics.previousUpdate')}}</th>
+                <th>{{$t('diagnostics.nextUpdate')}}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="ocsp in ocspDiags.ocsp_responders" v-bind:key="ocsp.url">
+                <td>{{ocsp.status_class}}</td>
+                <td class="service-url" data-test="service-url">{{ocsp.url}}</td>
+                <td>{{$t('diagnostics.ocspResponders.ocspStatus.'+ocsp.status_code)}}</td>
+                <td>{{ocsp.prev_update_at}}</td>
+
+                <td>{{ocsp.next_update_at}}</td>
+              </tr>
+            </tbody>
+          </table>
+          {{ocsp}}
+        </div>
       </v-card-text>
     </v-card>
   </v-layout>
@@ -87,9 +102,61 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import * as api from '@/util/api';
 
 export default Vue.extend({
-  data: () => ({}),
+  data: () => ({
+    timestapmingServices: [],
+    globalconf: undefined,
+    ocspResponderDiagnostics: [],
+  }),
+
+  computed: {
+    ocsps() {
+      const map1 = this.ocspResponderDiagnostics
+        .map((obj) => {
+          return obj.ocsp_responders;
+        })
+        .flat();
+      return map1;
+    },
+  },
+
+  methods: {
+    fetchData(): void {
+      api
+        .get(`/diagnostics/timestamping-services`)
+        .then((res) => {
+          this.timestapmingServices = res.data;
+        })
+        .catch((error) => {
+          throw error;
+        });
+
+      api
+        .get(`/diagnostics/globalconf`)
+        .then((res) => {
+          console.log(res);
+          this.globalconf = res.data;
+        })
+        .catch((error) => {
+          throw error;
+        });
+
+      api
+        .get(`/diagnostics/ocsp-responders`)
+        .then((res) => {
+          console.log(res);
+          this.ocspResponderDiagnostics = res.data;
+        })
+        .catch((error) => {
+          throw error;
+        });
+    },
+  },
+  created() {
+    this.fetchData();
+  },
 });
 </script>
 
@@ -113,5 +180,13 @@ export default Vue.extend({
   }
 
   margin-bottom: 30px;
+}
+
+.cert-service-name {
+  margin-top: 30px;
+
+  span {
+    font-weight: bold;
+  }
 }
 </style>
