cda85b3a90266c7b38dd4fefc13142fb822b93ba
bertofl90 <alberto.fernandez@gofore.com>
2020-04-22 14:24:17 +0200

    XRDDEV-985 Add dockerfile for provider

--- setup_security_server_sidecar.sh
+++ setup_security_server_sidecar.sh
@@ -43,9 +43,9 @@ postgresqlport=${7:-5432}
 docker network inspect xroad-network >/dev/null 2>&1 || docker network create -d bridge xroad-network
 
 echo "=====> Build sidecar image"
-docker build -f sidecar/Dockerfile -t xroad-sidecar-security-server-image sidecar/
+docker build -f sidecar/provider/Dockerfile -t xroad-sidecar-security-server-image-provider sidecar/
 echo "=====> Run container"
-docker run --detach -p $2:4000 -p $httpport:80 -p 5588:5588 --network xroad-network -e XROAD_TOKEN_PIN=$3 -e XROAD_ADMIN_USER=$4 -e XROAD_ADMIN_PASSWORD=$5 -e XROAD_DB_HOST=$postgresqlhost -e XROAD_DB_PORT=$postgresqlport -e XROAD_DB_PWD=$XROAD_DB_PASSWORD --name $1 xroad-sidecar-security-server-image
+docker run --detach -p $2:4000 -p $httpport:80 -p 5588:5588 --network xroad-network -e XROAD_TOKEN_PIN=$3 -e XROAD_ADMIN_USER=$4 -e XROAD_ADMIN_PASSWORD=$5 -e XROAD_DB_HOST=$postgresqlhost -e XROAD_DB_PORT=$postgresqlport -e XROAD_DB_PWD=$XROAD_DB_PASSWORD --name $1 xroad-sidecar-security-server-image-provider
 
 printf "\n
 Sidecar security server software token PIN is set to $3
