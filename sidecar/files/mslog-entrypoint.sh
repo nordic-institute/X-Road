dc67d27b1aaf4af7b3b6543e0712365ff3c43346
bertofl90 <alberto.fernandez@gofore.com>
2020-05-06 17:33:43 +0200

    XRDDEV-985 Install messagelog in vanilla image and disable it, enable message log in sidecar provider image

---
+++ sidecar/files/mslog-entrypoint.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+XROAD_SCRIPT_LOCATION=/usr/share/xroad/scripts
+DB_PROPERTIES=/etc/xroad/db.properties
+ROOT_PROPERTIES=/etc/xroad.properties
+GROUPNAMES="xroad-security-officer xroad-registration-officer xroad-service-administrator xroad-system-administrator xroad-securityserver-observer"
+
+INSTALLED_VERSION=$(dpkg-query --showformat='${Version}' --show xroad-proxy)
+PACKAGED_VERSION="$(cat /root/VERSION)"
+
+# Update X-Road configuration on startup, if necessary
+if [ "$INSTALLED_VERSION" == "$PACKAGED_VERSION" ]; then
+    if [ -f /etc/xroad/VERSION ]; then
+        CONFIG_VERSION="$(cat /etc/xroad/VERSION)"
+    else
+        echo "WARN: Current configuration version not known" >&2
+        CONFIG_VERSION=
+    fi
+    if [ -n "$CONFIG_VERSION" ] && dpkg --compare-versions "$PACKAGED_VERSION" gt "$CONFIG_VERSION"; then
+        echo "Updating configuration from $CONFIG_VERSION to $PACKAGED_VERSION"
+        cp -a /root/etc/xroad/* /etc/xroad/
+        pg_ctlcluster 10 main start
+        pg_isready -t 10
+        dpkg-reconfigure xroad-proxy
+        pg_ctlcluster 10 main stop
+        nginx -s stop
+        sleep 1
+        echo "$PACKAGED_VERSION" >/etc/xroad/VERSION
+    fi
+else
+    echo "WARN: Installed version ($INSTALLED_VERSION) does not match packaged version ($PACKAGED_VERSION)" >&2
+fi
+
+# Configure admin user with user-supplied username and password
+user_exists=$(id -u ${XROAD_ADMIN_USER} > /dev/null 2>&1)
+if [ $? != 0 ]
+then
+    echo "Creating admin user with user-supplied credentials"
+    useradd -m ${XROAD_ADMIN_USER} -s /usr/sbin/nologin
+    echo "${XROAD_ADMIN_USER}:${XROAD_ADMIN_PASSWORD}" | chpasswd
+    echo "xroad-proxy xroad-common/username string ${XROAD_ADMIN_USER}" | debconf-set-selections
+
+    echo "Configuring groups"
+    usergroups=" $(id -Gn "${XROAD_ADMIN_USER}") "
+
+    for groupname in ${GROUPNAMES}; do
+        if [[ $usergroups != *" $groupname "* ]]; then
+            echo "$groupname"
+            usermod -a -G "$groupname" "${XROAD_ADMIN_USER}" || true
+        fi
+    done
+fi
+
+# Generate internal and admin UI TLS keys and certificates on the first run
+if [ ! -f /etc/xroad/ssl/internal.crt ];
+then
+    echo "Generating new internal TLS key and certificate"
+    ARGS="-n internal -f -S -p"
+    $XROAD_SCRIPT_LOCATION/generate_certificate.sh $ARGS
+fi
+
+if [ ! -f /etc/xroad/ssl/nginx.crt ];
+then
+    echo "Generating new SSL key and certificate for the admin UI"
+    ARGS="-n nginx -f -S -p"
+    $XROAD_SCRIPT_LOCATION/generate_certificate.sh $ARGS
+fi
+
+# Recreate serverconf database and properties file with serverconf username and random password on the first run
+if [ ! -f ${DB_PROPERTIES} ]
+then
+    echo "Creating serverconf database and properties file"
+    if [[ ! -z "${XROAD_DB_PWD}" && "${XROAD_DB_HOST}" != "127.0.0.1" ]];
+    then
+        echo "xroad-proxy xroad-common/database-host string ${XROAD_DB_HOST}:${XROAD_DB_PORT}" | debconf-set-selections
+        touch /etc/xroad.properties
+        chown root:root /etc/xroad.properties
+        chmod 600 /etc/xroad.properties
+        echo "postgres.connection.password = ${XROAD_DB_PWD}" >> ${ROOT_PROPERTIES}
+        crudini --del /etc/supervisor/conf.d/xroad.conf program:postgres
+        dpkg-reconfigure -fnoninteractive xroad-proxy
+        dpkg-reconfigure -fnoninteractive xroad-addon-messagelog
+        nginx -s stop
+    else
+        pg_ctlcluster 10 main start
+        dpkg-reconfigure -fnoninteractive xroad-proxy
+        dpkg-reconfigure -fnoninteractive xroad-addon-messagelog
+        pg_ctlcluster 10 main stop
+        nginx -s stop
+    fi
+fi
+
+#cp -rp /etc/xroad/db.properties /etc/xroad/db.properties.back
+
+# Start services
+exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
--- sidecar/files/provider-ss-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sed -i 's;exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf; ;g' /root/entrypoint.sh
-source root/entrypoint.sh  
-apt-get install xroad-addon-messagelog
-exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
