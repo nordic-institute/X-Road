31fc59729a8e67363b0804ddb5d3e48b2e9349f8
yamatokataoka <34284486+yamatokataoka@users.noreply.github.com>
2020-05-12 21:42:31 +0900

    Fix date filter

--- src/proxy-ui-api/frontend/src/filters.ts
+++ src/proxy-ui-api/frontend/src/filters.ts
@@ -43,7 +43,7 @@ Vue.filter('formatDate', (value: string): string => {
   const date = new Date(value);
 
   return date.getFullYear() + '-'
-    + date.getMonth().toString().padStart(2, '0') + '-'
+    + (date.getMonth() + 1).toString().padStart(2, '0') + '-'
     + date.getDay().toString().padStart(2, '0');
 });
 
@@ -58,7 +58,7 @@ Vue.filter('formatDateTime', (value: string): string => {
   const date = new Date(value);
 
   return date.getFullYear() + '-'
-    + date.getMonth().toString().padStart(2, '0') + '-'
+    + (date.getMonth() + 1).toString().padStart(2, '0') + '-'
     + date.getDay().toString().padStart(2, '0') + ' '
     + date.getHours().toString().padStart(2, '0') + ':'
     + date.getMinutes().toString().padStart(2, '0');
