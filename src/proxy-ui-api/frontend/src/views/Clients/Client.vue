e52a62435ea53793f28f327b6ae353ba9db62c46
Mikko Riippi <mikko.riippi@gofore.com>
2020-05-13 21:42:38 +0300

    XRDDEV-862 Add delete client function for members

--- src/proxy-ui-api/frontend/src/views/Clients/Client.vue
+++ src/proxy-ui-api/frontend/src/views/Clients/Client.vue
@@ -4,6 +4,7 @@
       <h1 v-if="client" class="display-1 mb-3">{{client.member_name}} ({{ $t("client.owner") }})</h1>
 
       <div>
+        <LargeButton v-if="showDelete" @click="confirmDelete = true">{{$t('action.delete')}}</LargeButton>
         <LargeButton
           v-if="showUnregister"
           @click="confirmUnregisterClient = true"
@@ -16,6 +17,25 @@
     </v-tabs>
 
     <router-view />
+    <!-- Confirm dialog for delete client -->
+    <ConfirmDialog
+      :dialog="confirmDelete"
+      :loading="deleteLoading"
+      title="client.action.delete.confirmTitle"
+      text="client.action.delete.confirmText"
+      @cancel="confirmDelete = false"
+      @accept="deleteClient()"
+    />
+
+    <!-- Confirm dialog for deleting orphans -->
+    <ConfirmDialog
+      :dialog="confirmOrphans"
+      :loading="orphansLoading"
+      title="client.action.removeOrphans.confirmTitle"
+      text="client.action.removeOrphans.confirmText"
+      @cancel="confirmOrphans = false"
+      @accept="deleteOrphans()"
+    />
 
     <!-- Confirm dialog for unregister client -->
     <ConfirmDialog
@@ -50,16 +70,37 @@ export default Vue.extend({
   },
   data() {
     return {
-      tab: null,
-      confirmUnregisterClient: false,
-      unregisterLoading: false,
+      orphansLoading: false as boolean,
+      confirmDelete: false as boolean,
+      deleteLoading: false as boolean,
+      confirmOrphans: false as boolean,
+      tab: undefined as undefined | Tab,
+      confirmUnregisterClient: false as boolean,
+      unregisterLoading: false as boolean,
     };
   },
   computed: {
     ...mapGetters(['client']),
     showUnregister(): boolean {
+      return (
+        this.client &&
+        this.$store.getters.hasPermission(Permissions.SEND_CLIENT_DEL_REQ) &&
+        (this.client.status === 'REGISTERED' ||
+          this.client.status === 'REGISTRATION_IN_PROGRESS')
+      );
+    },
+    showDelete(): boolean {
+      if (
+        !this.client ||
+        this.client.status === 'REGISTERED' ||
+        this.client.status === 'REGISTRATION_IN_PROGRESS'
+      ) {
+        return false;
+      }
+
       return this.$store.getters.hasPermission(Permissions.SEND_CLIENT_DEL_REQ);
     },
+
     tabs(): Tab[] {
       const allTabs = [
         {
@@ -98,6 +139,66 @@ export default Vue.extend({
       this.$store.dispatch('fetchClient', id).catch((error) => {
         this.$store.dispatch('showError', error);
       });
+    },
+
+    deleteClient(): void {
+      this.deleteLoading = true;
+      this.$store.dispatch('deleteClient', this.client.id).then(
+        (response) => {
+          this.$store.dispatch('showSuccess', 'client.action.delete.success');
+          this.checkOrphans();
+        },
+        (error) => {
+          this.$store.dispatch('showError', error);
+          this.confirmDelete = false;
+          this.deleteLoading = false;
+        },
+      );
+    },
+
+    checkOrphans(): void {
+      this.$store.dispatch('getOrphans', this.client.id).then(
+        (response) => {
+          this.confirmDelete = false;
+          this.deleteLoading = false;
+          this.confirmOrphans = true;
+        },
+        (error) => {
+          this.confirmDelete = false;
+          this.deleteLoading = false;
+          if (error.response.status === 404) {
+            // No orphans found so exit the view
+            this.$router.go(-1);
+          } else {
+            // There was some other error, but the client is already deleted so exit the view
+            this.$store.dispatch('showError', error);
+            this.$router.go(-1);
+          }
+        },
+      );
+    },
+
+    deleteOrphans(): void {
+      this.orphansLoading = true;
+      this.$store
+        .dispatch('deleteOrphans', this.client.id)
+        .then(
+          (response) => {
+            this.$store.dispatch(
+              'showSuccess',
+              'client.action.removeOrphans.success',
+            );
+          },
+          (error) => {
+            // There was some other error, but the client is already deleted so exit the view
+            this.$store.dispatch('showError', error);
+          },
+        )
+        .finally(() => {
+          this.confirmOrphans = false;
+          this.orphansLoading = false;
+          this.$router.go(-1);
+        });
     },
 
     unregisterClient(): void {
