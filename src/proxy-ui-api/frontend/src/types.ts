777bb41a9b672d42df6e5f0d411d049f17b58468
Mikko Riippi <mikko.riippi@gofore.com>
2020-05-14 20:17:48 +0300

    XRDDEV-929 Update types

--- src/proxy-ui-api/frontend/src/types.ts
+++ src/proxy-ui-api/frontend/src/types.ts
@@ -283,6 +283,12 @@ export interface Client {
    * false
    */
   readonly owner?: boolean;
+  /**
+   * if this client is local and has a valid sign cert
+   * example:
+   * false
+   */
+  readonly has_valid_local_sign_cert?: boolean;
   connection_type?: ConnectionType; // enum
   readonly status?: ClientStatus; // enum
 }
@@ -594,6 +600,39 @@ export interface GroupMember {
    * 2018-12-15T00:00:00.001Z
    */
   created_at: string; // date-time
+}
+/**
+ * security server initial configuration
+ */
+export interface InitialServerConf {
+  /**
+   * member class
+   * example:
+   * GOV
+   */
+  owner_member_class?: string; // text
+  /**
+   * member code
+   * example:
+   * 12345678-9
+   */
+  owner_member_code?: string; // text
+  /**
+   * security server code
+   * example:
+   * SS1
+   */
+  security_server_code?: string; // text
+  /**
+   * pin code for the initial software token
+   * example:
+   * sup3rs3cr3t_p!n
+   */
+  software_token_pin?: string; // text
+  /**
+   * if true, any ignorable warnings are ignored. if false (or missing), any warnings cause request to fail
+   */
+  ignore_warnings?: boolean;
 }
 /**
  * key for the certificate
@@ -914,6 +953,12 @@ export interface Service {
    * example:
    * myService.v1
    */
+  full_service_code?: string; // text
+  /**
+   * encoded service code and version
+   * example:
+   * myService
+   */
   service_code: string; // text
   /**
    * service time out value
@@ -972,9 +1017,9 @@ export interface ServiceClient {
    * My own Local group code
    */
   readonly local_group_code?: string; // text
-  service_client_type?: ServiceClientType; // text
+  readonly service_client_type?: ServiceClientType; // text
   /**
-   * time when access right were given at. Contains meaningful value when listing service clients that have been granted access to a specific endpoint or service.
+   * time when access right were given at. When listing client's service clients without specifying the service, the time when first service access right was given to this service client for any service. When listing service clients for a specific service, time when service client was added permission to that service.
    * example:
    * 2018-12-15T00:00:00.001Z
    */
