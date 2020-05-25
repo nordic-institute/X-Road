36b426e61505102d9f9fb86d4e663c4f3e56c863
Ilkka Seppälä <ilkka.seppala@gofore.com>
2020-05-25 16:08:35 +0300

    XRDDEV-727 refactor xroad-ca, xroad-cp, xroad-cs and xroad-ss

--- ansible/roles/xroad-ca/common-files/home/ca/CA/ca.py
+++ ansible/roles/xroad-ca/common-files/home/ca/CA/ca.py
@@ -1,44 +1,41 @@
 #!/usr/bin/python
 
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from BaseHTTPServer import BaseHTTPRequestHandler
 import subprocess
 import tempfile
 import cgi
-import os
+import time
 
 class CAHandler(BaseHTTPRequestHandler):
 
-    FORM_HTML = '''\
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Test CA</title>
-  </head>
-  <body>
-    <form method="POST" enctype="multipart/form-data" action="sign">
-      <fieldset>
-        <legend>Test CA: CSR signing</legend>
+    form_html = \
+        '''
+        <html>
+        <body>
+        <form method="POST" enctype="multipart/form-data" action="sign">
+        <fieldset>
+        <legend>TestCA: CSR signing</legend>
         <div>
-          <label style="display:inline-block; width:4em" for="csr">CSR</label>
-          <input name="certreq" type="file" id="csr">
+        <label style="display:inline-block; width:4em" for="csr">CSR</label>
+        <input name="certreq" type="file" id="csr">
         </div>
         <div>
-          <label style="display:inline-block; width:4em">Type</label>
-          <input type="radio" name="type" id="sign" value="sign">
-          <label for="sign">Sign</label>
-          <input type="radio" name="type" id="auth" value="auth">
-          <label for="auth">Auth</label>
-          <input type="radio" name="type" id="auto" value="auto" checked>
-          <label for="auto">Autodetect from file name</label>
+        <label style="display:inline-block; width:4em">Type</label>
+        <input type="radio" name="type" id="sign" value="sign">
+        <label for="sign">Sign</label>
+        <input type="radio" name="type" id="auth" value="auth">
+        <label for="auth">Auth</label>
+        <input type="radio" name="type" id="auto" value="auto" checked>
+        <label for="auto">Autodetect from file name</label>
         </div>
         <div>
-          <input type="submit" value="Sign" style="margin-top:1em"/>
-         </div>
-      </fieldset>
-    </form>
-  </body>
-</html>
-'''.encode()
+        <input type="submit" value="Sign" style="margin-top:1em"/>
+        </div>
+        </fieldset>
+        </form>
+        </html>
+        </body>
+        '''
 
     def do_GET(self):
         if self.path == "/favicon.ico":
@@ -48,9 +45,11 @@ class CAHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-Type', 'text/html; charset=utf-8')
         self.end_headers()
-        self.wfile.write(self.FORM_HTML)
+        self.wfile.write(self.form_html.encode())
 
     def do_POST(self):
+	#artificial rate limiting (the server is already single-threaded)
+        time.sleep(1)
         cgi.maxlen = 10000
 
         expect = self.headers.getheader('expect', "")
@@ -83,18 +82,15 @@ class CAHandler(BaseHTTPRequestHandler):
                 t = tempfile.NamedTemporaryFile()
                 t.write(req_item.file.read())
                 t.flush()
-                p = subprocess.Popen(["bash", "/home/ca/CA/sign_req.sh", sign_type, t.name],
-                                     stdout=subprocess.PIPE,
-                                     stderr=subprocess.PIPE)
+                p = subprocess.Popen(["bash", "/home/ca/CA/sign_req.sh", sign_type, t.name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 (out, err) = p.communicate()
                 t.close()
                 p.wait()
                 if p.returncode == 0:
-                    crtname = os.path.splitext(req_item.filename)[0].replace("_csr_", "_crt_")
                     self.send_response(200, 'OK')
                     self.send_header('Content-Type', 'application/octet-stream')
                     self.send_header('Content-Disposition',
-                                     'attachment; filename="{}.pem"'.format(crtname))
+                                     'attachment; filename="{}-crt.pem"'.format(sign_type))
                     self.send_header('Content-Length', len(out))
                     self.end_headers()
                     self.wfile.write(out)
@@ -113,6 +109,8 @@ class CAHandler(BaseHTTPRequestHandler):
         return
 
 if __name__ == '__main__':
-    server = HTTPServer(('localhost', 9998), CAHandler)
+    from BaseHTTPServer import HTTPServer
+    server = HTTPServer(('0.0.0.0', 9998), CAHandler)
     print 'Starting server...'
     server.serve_forever()
+
