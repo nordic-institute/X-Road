ea38def43a89970e1c6f05fda2da713df4ff896f
Olli Ruotsalainen <ollijr@gmail.com>
2020-05-28 23:50:06 -0700

    XRDDEV-1030 wsdl operation e2e tests XRDDEV-1030 XRDDEV-1031 XRDDEV-1032

--- src/proxy-ui-api/frontend/tests/e2e/specs/ss-clients-wsdlservices.js
+++ src/proxy-ui-api/frontend/tests/e2e/specs/ss-clients-wsdlservices.js
@@ -7,7 +7,6 @@ module.exports = {
     const clientsTab = mainPage.section.clientsTab;
     const clientInfo = mainPage.section.clientInfo;
     const clientServices = clientInfo.section.services;
-    const servicesPopup = mainPage.section.servicesWarningPopup
     const serviceDetails = mainPage.section.serviceDetails
 
     // Open SUT and check that page is loaded
@@ -53,6 +52,255 @@ module.exports = {
     clientServices.expandServiceDetails();
     browser.waitForElementVisible('//td[contains(@data-test, "service-link") and contains(text(),"testOp1")]');
 
+    browser.end();
+  },
+  'Security server client edit wsdl operation': browser => {
+    const frontPage = browser.page.ssFrontPage();
+    const mainPage = browser.page.ssMainPage();
+    const clientsTab = mainPage.section.clientsTab;
+    const clientInfo = mainPage.section.clientInfo;
+    const clientServices = clientInfo.section.services;
+    const operationDetails = mainPage.section.wsdlOperationDetails;
+
+
+    // Open SUT and check that page is loaded
+    frontPage.navigate();
+    browser.waitForElementVisible('//*[@id="app"]');
+
+    // Enter valid credentials
+    frontPage.signinDefaultUser();
+
+    // Navigate
+    mainPage.openClientsTab();
+    browser.waitForElementVisible(clientsTab);
+    clientsTab.openTestService();
+    browser.waitForElementVisible(clientInfo);
+    clientInfo.openServicesTab();
+    browser.waitForElementVisible(clientServices);
+	
+    clientServices.expandServiceDetails();
+    clientServices.openOperation('testOp1');
+    operationDetails.close();
+    clientServices.openOperation('testOp1');
+    browser.waitForElementVisible(operationDetails);
+
+    // Verify tooltips
+    browser.moveToElement(operationDetails.elements.urlHelp,0,0);
+    browser.expect.element(operationDetails.elements.activeTooltip).to.be.visible.and.text.to.equal("The URL where requests targeted at the service are directed");
+
+    browser.moveToElement(operationDetails.elements.timeoutHelp,0,0);
+    browser.expect.element(operationDetails.elements.activeTooltip).to.be.visible.and.text.to.equal("The maximum duration of a request to the service, in seconds");
+
+    browser.moveToElement(operationDetails.elements.verifyCertHelp,0,0);
+    browser.expect.element(operationDetails.elements.activeTooltip).to.be.visible.and.text.to.equal("Verify TLS certificate when a secure connection is established");
+
+    // Verify cancel
+    operationDetails.enterUrl('https://www.niis.org/nosuch2/');
+    operationDetails.enterTimeout('40');
+    operationDetails.toggleCertVerification();
+    operationDetails.toggleCertVerification();
+    operationDetails.toggleUrlApplyToAll();
+    operationDetails.toggleTimeoutApplyToAll();
+    operationDetails.toggleVerifyCertApplyToAll();
+
+    operationDetails.close();
+
+    // Verify that options were not changed
+    browser.assert.containsText(clientServices.elements.operationUrl, 'https://www.niis.org/nosuch1/');
+    browser.waitForElementVisible('//tr[.//td[@data-test="service-link" and contains(text(), "testOp1")]]//*[contains(@class, "mdi-lock-open-outline")]');
+    browser.waitForElementVisible('//tr[.//td[@data-test="service-link" and contains(text(), "testOpA")]]//*[contains(@class, "mdi-lock-open-outline")]');
+    clientServices.openOperation('testOp1');
+    browser.waitForElementVisible(operationDetails);
+    browser.assert.valueContains(operationDetails.elements.serviceURL, 'https://www.niis.org/nosuch1/');
+    browser.assert.valueContains(operationDetails.elements.timeout, '60');
+    browser.expect.element(operationDetails.elements.sslAuth).to.be.not.selected;
+
+
+    // Verify change single operation
+    operationDetails.enterUrl('https://www.niis.org/nosuch2/');
+    operationDetails.enterTimeout('40');
+    operationDetails.saveParameters();
+    browser.waitForElementVisible('//*[contains(@class, "v-snack") and .//*[contains(text(), "Service saved")]]');
+    mainPage.closeSnackbar();
+    operationDetails.close();
+
+    browser.waitForElementVisible('//tr[.//td[@data-test="service-link" and contains(text(), "testOp1")]]//*[contains(text(), "https://www.niis.org/nosuch2/")]');
+    browser.waitForElementVisible('//tr[.//td[@data-test="service-link" and contains(text(), "testOpA")]]//*[contains(text(), "https://www.niis.org/nosuch1/")]');
+    browser.waitForElementVisible('//tr[.//td[@data-test="service-link" and contains(text(), "testOp1")]]//*[contains(@class, "mdi-lock") and contains(@style, "'+browser.globals.service_ssl_auth_on_style+'")]');
+   browser.waitForElementVisible('//tr[.//td[@data-test="service-link" and contains(text(), "testOpA")]]//*[contains(@class, "mdi-lock-open-outline")]');
+
+    clientServices.openOperation('testOp1');
+    browser.waitForElementVisible(operationDetails);
+    browser.assert.valueContains(operationDetails.elements.serviceURL, 'https://www.niis.org/nosuch2/');
+    browser.assert.valueContains(operationDetails.elements.timeout, '40');
+    browser.expect.element(operationDetails.elements.sslAuth).to.be.selected;
+    operationDetails.close();
+
+    clientServices.openOperation('testOpA');
+    browser.waitForElementVisible(operationDetails);
+    browser.assert.valueContains(operationDetails.elements.serviceURL, 'https://www.niis.org/nosuch1/');
+    browser.assert.valueContains(operationDetails.elements.timeout, '60');
+    browser.expect.element(operationDetails.elements.sslAuth).to.be.not.selected;
+    operationDetails.close();
+
+    // Verify change all operations
+    clientServices.openOperation('testOpA');
+    browser.waitForElementVisible(operationDetails); 
+    operationDetails.toggleUrlApplyToAll();
+    operationDetails.toggleTimeoutApplyToAll();
+    operationDetails.toggleVerifyCertApplyToAll();
+    operationDetails.enterUrl('https://www.niis.org/nosuch3/');
+    operationDetails.enterTimeout('30');
+    operationDetails.toggleCertVerification();
+    operationDetails.saveParameters();
+    browser.waitForElementVisible('//*[contains(@class, "v-snack") and .//*[contains(text(), "Service saved")]]');
+    mainPage.closeSnackbar();
+    operationDetails.close();
+
+    browser.waitForElementVisible('//tr[.//td[@data-test="service-link" and contains(text(), "testOp1")]]//*[contains(text(), "https://www.niis.org/nosuch3/")]');
+    browser.waitForElementVisible('//tr[.//td[@data-test="service-link" and contains(text(), "testOpA")]]//*[contains(text(), "https://www.niis.org/nosuch3/")]');
+    browser.waitForElementVisible('//tr[.//td[@data-test="service-link" and contains(text(), "testOp1")]]//*[contains(@class, "mdi-lock") and contains(@style, "'+browser.globals.service_ssl_auth_off_style+'")]');
+    browser.waitForElementVisible('//tr[.//td[@data-test="service-link" and contains(text(), "testOpA")]]//*[contains(@class, "mdi-lock") and contains(@style, "'+browser.globals.service_ssl_auth_off_style+'")]');
+
+
+    clientServices.openOperation('testOp1');
+    browser.waitForElementVisible(operationDetails);
+    browser.assert.valueContains(operationDetails.elements.serviceURL, 'https://www.niis.org/nosuch3/');
+    browser.assert.valueContains(operationDetails.elements.timeout, '30');
+    browser.expect.element(operationDetails.elements.sslAuth).to.be.not.selected;
+    operationDetails.close();
+
+    clientServices.openOperation('testOpA');
+    browser.waitForElementVisible(operationDetails);
+    browser.assert.valueContains(operationDetails.elements.serviceURL, 'https://www.niis.org/nosuch3/');
+    browser.assert.valueContains(operationDetails.elements.timeout, '30');
+    browser.expect.element(operationDetails.elements.sslAuth).to.be.not.selected;
+    operationDetails.close();
+
+    browser.end();
+
+  },
+  'Security server client add wsdl operation access rights': browser => {
+    const frontPage = browser.page.ssFrontPage();
+    const mainPage = browser.page.ssMainPage();
+    const clientsTab = mainPage.section.clientsTab;
+    const clientInfo = mainPage.section.clientInfo;
+    const clientServices = clientInfo.section.services;
+    const operationDetails = mainPage.section.wsdlOperationDetails;
+    const addSubjectsPopup = mainPage.section.wsdlAddSubjectsPopup;
+
+    // Open SUT and check that page is loaded
+    frontPage.navigate();
+    browser.waitForElementVisible('//*[@id="app"]');
+
+    // Enter valid credentials
+    frontPage.signinDefaultUser();
+
+    // Navigate
+    mainPage.openClientsTab();
+    browser.waitForElementVisible(clientsTab);
+    clientsTab.openTestService();
+    browser.waitForElementVisible(clientInfo);
+    clientInfo.openServicesTab();
+    browser.waitForElementVisible(clientServices);
+	
+    clientServices.expandServiceDetails();
+    clientServices.openOperation('testOp1');
+    browser.waitForElementVisible(operationDetails);
+
+    operationDetails.openAddAccessRights();
+    browser.waitForElementVisible(addSubjectsPopup);
+
+    // Verify cancel
+    addSubjectsPopup.startSearch();
+    addSubjectsPopup.selectSubject('TestCom');
+    addSubjectsPopup.cancel();
+    browser.waitForElementNotPresent('//table[contains(@class, "group-members-table")]//td[contains(text(), "TestCom")]');
+
+    // Verify add
+    operationDetails.openAddAccessRights();
+    browser.waitForElementVisible(addSubjectsPopup);
+    addSubjectsPopup.startSearch();
+    addSubjectsPopup.selectSubject('TestOrg');
+    addSubjectsPopup.selectSubject('Security server owners');
+    addSubjectsPopup.selectSubject('Group1');
+    addSubjectsPopup.addSelected();
+    browser.assert.containsText(mainPage.elements.snackBarMessage, 'Access rights added successfully');
+    mainPage.closeSnackbar();
+
+    browser.waitForElementVisible('//table[contains(@class, "group-members-table")]//td[contains(text(), "TestOrg")]');
+    browser.waitForElementVisible('//table[contains(@class, "group-members-table")]//td[contains(text(), "Security server owners")]');    
+    browser.waitForElementVisible('//table[contains(@class, "group-members-table")]//td[contains(text(), "Group1")]');
+    browser.waitForElementNotPresent('//table[contains(@class, "group-members-table")]//td[contains(text(), "TestCom")]');
+
+    browser.end();
+  },
+  'Security server client remove wsdl operation access rights': browser => {
+    const frontPage = browser.page.ssFrontPage();
+    const mainPage = browser.page.ssMainPage();
+    const clientsTab = mainPage.section.clientsTab;
+    const clientInfo = mainPage.section.clientInfo;
+    const clientServices = clientInfo.section.services;
+    const operationDetails = mainPage.section.wsdlOperationDetails;
+    const removeMemberPopup = mainPage.section.removeMemberPopup;
+    const removeAllMembersPopup = mainPage.section.removeAllMembersPopup;
+
+
+    // Open SUT and check that page is loaded
+    frontPage.navigate();
+    browser.waitForElementVisible('//*[@id="app"]');
+
+    // Enter valid credentials
+    frontPage.signinDefaultUser();
+
+    // Navigate
+    mainPage.openClientsTab();
+    browser.waitForElementVisible(clientsTab);
+    clientsTab.openTestService();
+    browser.waitForElementVisible(clientInfo);
+    clientInfo.openServicesTab();
+    browser.waitForElementVisible(clientServices);
+	
+    clientServices.expandServiceDetails();
+    clientServices.openOperation('testOp1');
+    browser.waitForElementVisible(operationDetails);
+
+    // Verify cancel remove
+    operationDetails.removeAccessRight('TestOrg');
+    browser.waitForElementVisible(removeMemberPopup);
+    removeMemberPopup.cancel();
+    browser.waitForElementVisible('//table[contains(@class, "group-members-table")]//td[contains(text(), "TestOrg")]');
+
+    // Verify remove	
+    operationDetails.removeAccessRight('TestOrg');
+    browser.waitForElementVisible(removeMemberPopup);
+    removeMemberPopup.confirm();
+    browser.assert.containsText(mainPage.elements.snackBarMessage, 'Access rights removed successfully');
+    mainPage.closeSnackbar();
+    browser.waitForElementNotPresent(mainPage.elements.snackBarMessage);
+    browser.waitForElementNotPresent('//table[contains(@class, "group-members-table")]//td[contains(text(), "TestOrg")]');
+    browser.waitForElementVisible('//table[contains(@class, "group-members-table")]//td[contains(text(), "Security server owners")]');    
+    browser.waitForElementVisible('//table[contains(@class, "group-members-table")]//td[contains(text(), "Group1")]');
+
+    // Verify cancel remove all
+    operationDetails.removeAllAccessRights();
+    browser.waitForElementVisible(removeAllMembersPopup);
+    removeAllMembersPopup.cancel();
+    browser.waitForElementVisible('//table[contains(@class, "group-members-table")]//td[contains(text(), "Security server owners")]');    
+    browser.waitForElementVisible('//table[contains(@class, "group-members-table")]//td[contains(text(), "Group1")]');
+
+    // Verify remove all
+    operationDetails.removeAllAccessRights();
+    browser.waitForElementVisible(removeAllMembersPopup);
+    removeAllMembersPopup.confirm();
+
+    browser.assert.containsText(mainPage.elements.snackBarMessage, 'Access rights removed successfully');
+    mainPage.closeSnackbar();
+    browser.waitForElementNotPresent('//table[contains(@class, "group-members-table")]//td[contains(text(), "Security server owners")]');    
+    browser.waitForElementNotPresent('//table[contains(@class, "group-members-table")]//td[contains(text(), "Group1")]');
+
+
+    browser.pause(2000);
     browser.end();
   },
   'Security server client edit wsdl service': browser => {
