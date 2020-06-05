f7bdb488dc7f22396d747988c7492e83c7c97084
Janne Mattila <janne.mattila@gofore.com>
2020-06-05 12:52:39 +0300

    XRDDEV-404 fix bad Lazy definitions based on PR comments

--- src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/exceptions/ApplicationExceptionHandler.java
+++ src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/exceptions/ApplicationExceptionHandler.java
@@ -29,7 +29,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.niis.xroad.restapi.config.audit.AuditEventLoggingFacade;
 import org.niis.xroad.restapi.openapi.model.ErrorInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -53,7 +52,6 @@ public class ApplicationExceptionHandler {
     private ExceptionTranslator exceptionTranslator;
 
     @Autowired
-    @Lazy
     private AuditEventLoggingFacade auditEventLoggingFacade;
 
     /**
