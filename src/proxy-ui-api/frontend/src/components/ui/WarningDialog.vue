eff3ee4553387ee10a4d918807f10e0b3afd2762
Mikko Riippi <mikko.riippi@gofore.com>
2020-06-11 12:00:21 +0300

    XRDDEV-1108 Update the look of the warning dialog

--- src/proxy-ui-api/frontend/src/components/ui/WarningDialog.vue
+++ src/proxy-ui-api/frontend/src/components/ui/WarningDialog.vue
@@ -1,45 +1,36 @@
 <template>
-  <v-dialog :value="dialog" persistent :max-width="maxWidth">
-    <v-card>
-      <v-card-title class="headline">{{ $t('warning') }}</v-card-title>
-      <v-card-text>
-        <div v-for="warning in warnings" :key="warning.code">
-          <!-- create the localisation key from warning code -->
-          <div class="dlg-warning-header">
-            {{ $t(localizationParent + '.' + warning.code) }}
-          </div>
-          <div v-for="meta in warning.metadata" :key="meta">{{ meta }}</div>
+  <simpleDialog
+    :dialog="dialog"
+    title="warning"
+    @save="accept"
+    @cancel="cancel"
+    :cancelButtonText="cancelButtonText"
+    :saveButtonText="acceptButtonText"
+    :showClose="false"
+    :loading="loading"
+  >
+    <div slot="content" data-test="dialog-content-text">
+      <div v-for="warning in warnings" :key="warning.code">
+        <!-- Create the localisation key from warning code -->
+        <div class="dlg-warning-header">
+          {{ $t(localizationParent + '.' + warning.code) }}
         </div>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn
-          color="primary"
-          data-test="warning-cancel-button"
-          text
-          outlined
-          @click="cancel()"
-          >{{ $t(cancelButtonText) }}</v-btn
-        >
-        <v-btn
-          color="primary"
-          data-test="warning-accept-button"
-          text
-          outlined
-          @click="accept()"
-          >{{ $t(acceptButtonText) }}</v-btn
-        >
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+        <div v-for="meta in warning.metadata" :key="meta">{{ meta }}</div>
+      </div>
+    </div>
+  </simpleDialog>
 </template>
 
 <script lang="ts">
 // A dialog for backend warnings
 import Vue from 'vue';
 import { Prop } from 'vue/types/options';
+import SimpleDialog from '@/components/ui/SimpleDialog.vue';
 
 export default Vue.extend({
+  components: {
+    SimpleDialog,
+  },
   props: {
     dialog: {
       type: Boolean,
@@ -64,6 +55,10 @@ export default Vue.extend({
     maxWidth: {
       type: String,
       default: '850',
+    },
+    loading: {
+      type: Boolean,
+      default: false,
     },
   },
 
