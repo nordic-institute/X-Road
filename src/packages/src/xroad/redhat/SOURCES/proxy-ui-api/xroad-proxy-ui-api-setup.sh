54bf0fee8782b494162bf1993f7993f8bf0b29c3
Janne Mattila <janne.mattila@gofore.com>
2020-07-13 15:13:44 +0300

    XRDDEV-525 fix missing proxy_ui_api.p12

--- src/packages/src/xroad/redhat/SOURCES/proxy-ui-api/xroad-proxy-ui-api-setup.sh
+++ src/packages/src/xroad/redhat/SOURCES/proxy-ui-api/xroad-proxy-ui-api-setup.sh
@@ -13,9 +13,9 @@ done
 
 ALT="${LIST}DNS:$(hostname),DNS:$HOSTNAME"
 
-if [[ ! -r /etc/xroad/ssl/proxy-ui-api.crt || ! -r /etc/xroad/ssl/proxy-ui-api.key ]]
+if [[ ! -r /etc/xroad/ssl/proxy-ui-api.crt || ! -r /etc/xroad/ssl/proxy-ui-api.key  || ! -r /etc/xroad/ssl/proxy-ui-api.p12 ]]
 then
-    echo "Generating new proxy-ui-api.[crt|key] files "
-    rm -f /etc/xroad/ssl/proxy-ui-api.crt /etc/xroad/ssl/proxy-ui-api.key
-    /bin/bash /usr/share/xroad/scripts/generate_certificate.sh  -n proxy-ui-api  -s "/CN=$HOST" -a "$ALT" 2>/tmp/cert.err
+    echo "Generating new proxy-ui-api.[crt|key|p12] files "
+    rm -f /etc/xroad/ssl/proxy-ui-api.crt /etc/xroad/ssl/proxy-ui-api.key /etc/xroad/ssl/proxy-ui-api.p12
+    /usr/share/xroad/scripts/generate_certificate.sh  -n proxy-ui-api -s "/CN=$HOST" -a "$ALT" -p 2> /tmp/cert.err || handle_error
 fi
