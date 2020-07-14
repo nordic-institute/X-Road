46f4d796cf4351684d247571ab866f1507abf167
Mikko Riippi <mikko.riippi@gofore.com>
2020-07-14 13:57:42 +0300

    XRDDEV-1115 Add new component for certificate row

---
+++ src/proxy-ui-api/frontend/src/views/KeysAndCertificates/SignAndAuthKeys/CertificateRow.vue
@@ -0,0 +1,101 @@
+<template>
+  <tr>
+    <td class="td-name">
+      <div class="name-wrap">
+        <i
+          class="icon-xrd_certificate icon clickable"
+          @click="certificateClick(cert, key)"
+        ></i>
+        <div class="clickable-link" @click="certificateClick()">
+          {{ cert.certificate_details.issuer_common_name }}
+          {{ cert.certificate_details.serial }}
+        </div>
+      </div>
+    </td>
+    <td>{{ cert.owner_id }}</td>
+    <td>{{ cert.ocsp_status | ocspStatus }}</td>
+    <td>{{ cert.certificate_details.not_after | formatDate }}</td>
+    <td class="status-cell">
+      <certificate-status :certificate="cert" />
+    </td>
+    <td class="td-align-right">
+      <slot name="certificateAction"></slot>
+    </td>
+  </tr>
+</template>
+
+<script lang="ts">
+/**
+ * Table component for an array of keys
+ */
+import Vue from 'vue';
+import { Prop } from 'vue/types/options';
+import CertificateStatus from './CertificateStatus.vue';
+import { TokenCertificate } from '@/openapi-types';
+
+export default Vue.extend({
+  components: {
+    CertificateStatus,
+  },
+  props: {
+    cert: {
+      type: Object as Prop<TokenCertificate>,
+      required: true,
+    },
+  },
+
+  methods: {
+    certificateClick(): void {
+      this.$emit('certificateClick');
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '../../../assets/tables';
+.icon {
+  margin-left: 18px;
+  margin-right: 20px;
+}
+
+.clickable {
+  cursor: pointer;
+}
+
+.no-border {
+  border-bottom-width: 0 !important;
+}
+
+.table-button-fix {
+  margin-left: auto;
+  margin-right: 0;
+}
+
+.td-align-right {
+  text-align: right;
+}
+
+.clickable-link {
+  text-decoration: underline;
+  cursor: pointer;
+  height: 100%;
+}
+
+.name-wrap {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  i.v-icon.mdi-file-document-outline {
+    margin-left: 42px;
+  }
+}
+
+.name-wrap-top {
+  @extend .name-wrap;
+  margin-top: 18px;
+  margin-bottom: 5px;
+  min-width: 100%;
+}
+</style>
