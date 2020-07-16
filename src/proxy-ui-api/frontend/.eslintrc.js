5edf7727efef0195a9a3af35f9205f2b1ac40ebf
Raido Kaju <raido.kaju@gmail.com>
2020-07-16 14:51:32 +0300

    XRDDEV-1124: linter format fixes + any rule skip

--- src/proxy-ui-api/frontend/.eslintrc.js
+++ src/proxy-ui-api/frontend/.eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     '@typescript-eslint/camelcase': 'off',
     'vue/no-unused-vars': 'warn',
-    '@typescript-eslint/no-explicit-any': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    '@typescript-eslint/no-explicit-any':
+      process.env.NODE_ENV === 'production' ? 'error' : 'warn',
   },
 };
