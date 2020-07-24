00497ca973eb0e21a159bb330b9b86203292c965
Janne Mattila <janne.mattila@gofore.com>
2020-07-24 16:43:54 +0300

    XRDDEV-559 remove proxy-ui sources and update packaging accordingly

--- src/packages/src/xroad/redhat/SOURCES/proxy/xroad-proxy-setup.sh
+++ src/packages/src/xroad/redhat/SOURCES/proxy/xroad-proxy-setup.sh
@@ -2,7 +2,7 @@
 # X-Road proxy post-install configuration
 
 #
-# Create default internal certificates for nginx
+# Create default internal certificate
 #
 HOST=$(hostname -f)
 LIST=
@@ -12,13 +12,6 @@ do
 done
 
 ALT="${LIST}DNS:$(hostname),DNS:$HOSTNAME"
-
-if [[ ! -r /etc/xroad/ssl/nginx.crt || ! -r /etc/xroad/ssl/nginx.key ]]
-then
-    echo "Generating new nginx.[crt|key] files "
-    rm -f /etc/xroad/ssl/nginx.crt /etc/xroad/ssl/nginx.key
-    /bin/bash /usr/share/xroad/scripts/generate_certificate.sh  -n nginx  -s "/CN=$HOST" -a "$ALT" 2>/tmp/cert.err
-fi
 
 if [[ ! -r /etc/xroad/ssl/internal.crt || ! -r /etc/xroad/ssl/internal.key  || ! -r /etc/xroad/ssl/internal.p12 ]]
 then
@@ -41,14 +34,6 @@ if [[ $(getenforce) != "Disabled" ]]; then
 
     # allow httpd to connecto to non-standard port 4000
     semanage port -a -t http_port_t  -p tcp 4000 || true
-
-    # allow httpd to connect to non-standard port 5000 (keep this as long as we have the dual old & new UI)
-    if ! semanage port -m -t http_port_t  -p tcp 5000
-    then
-        echo "could not modify SELinux port 5000, will add instead"
-        semanage port -a -t http_port_t  -p tcp 5000 || true
-    fi
-
 fi
 
 /usr/share/xroad/scripts/setup_serverconf_db.sh
--- src/proxy-ui/public/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-function clientName(name) {
-    return util.escape(name) ||
-        "&lt;" + _("clients.client_acl_subjects_tab.client_not_found") + "&gt;";
-}
-
-function groupDesc(desc) {
-    return util.escape(desc) ||
-        "&lt;" + _("clients.client_acl_subjects_tab.group_not_found") + "&gt;";
-}
