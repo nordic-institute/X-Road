140c1655fb4d1418f3284cec49f09fe9e317d79e
Ilkka Seppälä <ilkka.seppala@gofore.com>
2020-09-15 08:49:36 +0300

    eslint off

--- src/proxy-ui-api/frontend/.eslintrc.js
+++ src/proxy-ui-api/frontend/.eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     '@typescript-eslint/camelcase': 'off',
     'vue/no-unused-vars': 'warn',
