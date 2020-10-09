9cd62110d30b15e7bff9b5968446fe949b6b267a
bertofl90 <alberto.fernandez@gofore.com>
2020-10-09 10:57:00 +0200

    XRDDEV-1164
     generate ssl key and certificate for admin ui using  proxy-ui-api as argument

--- sidecar/files/ss-entrypoint.sh
+++ sidecar/files/ss-entrypoint.sh
@@ -74,7 +74,7 @@ fi
 if [ ! -f /etc/xroad/ssl/nginx.crt ];
 then
     echo "Generating new SSL key and certificate for the admin UI"
-    ARGS="-n nginx -f -S -p"
+    ARGS="-n proxy-ui-api -f -S -p"
     $XROAD_SCRIPT_LOCATION/generate_certificate.sh $ARGS
 fi
 
