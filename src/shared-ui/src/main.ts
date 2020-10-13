364e10480223cf9b2cb0c3e78f28bd0b9eae5ba3
Mikko Riippi <mikko.riippi@gofore.com>
2020-10-13 16:14:28 +0300

    XRDDEV-1396 Add vuetify

--- src/shared-ui/src/main.ts
+++ src/shared-ui/src/main.ts
@@ -2,11 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@mdi/font/css/materialdesignicons.css";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
