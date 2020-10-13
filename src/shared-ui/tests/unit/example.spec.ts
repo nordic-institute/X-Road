691e613216ef44d79b43c4f619dc01a4ec9b27da
Mikko Riippi <mikko.riippi@gofore.com>
2020-10-13 16:50:49 +0300

    XRDDEV-1396 Try to fix tests

--- src/shared-ui/tests/unit/example.spec.ts
+++ src/shared-ui/tests/unit/example.spec.ts
@@ -3,7 +3,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "new message";
+    const msg = "Welcome to Vuetify";
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     });
