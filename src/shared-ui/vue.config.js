b9cb9a7ce62d9e11c6cab264971d819bdcdcc495
Mikko Riippi <mikko.riippi@gofore.com>
2020-10-15 19:01:03 +0300

    XRDDEV-1396 Fix the library build

--- src/shared-ui/vue.config.js
+++ src/shared-ui/vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  configureWebpack: {
+    output: {
+      libraryExport: 'default' // Needed when using default export
+      // https://cli.vuejs.org/guide/build-targets.html#vue-vs-js-ts-entry-files
+    },
+  },
+  css: { extract: false } // Keeps the vue sfc style parts in the build
 };
