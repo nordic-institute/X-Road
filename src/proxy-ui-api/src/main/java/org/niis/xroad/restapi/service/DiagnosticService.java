a86490ecf606a3928b13800903131c3b2213a9f8
Petteri Kivimäki <petteri.kivimaki@niis.org>
2020-10-18 13:17:23 +0300

    XRDDEV-1394 API keys have only observer role on a secondary node
    - Remove from API keys all non-observer roles on a secondary node.
    - Load node.ini in the xroad-proxy-ui-api process.

--- src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/service/DiagnosticService.java
+++ src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/service/DiagnosticService.java
@@ -141,7 +141,7 @@ public class DiagnosticService {
 
         try (CloseableHttpClient httpClient = HttpClientBuilder.create().setDefaultRequestConfig(config).build();
              CloseableHttpResponse response = httpClient.execute(request)) {
-            HttpEntity resEntity = response.getEntity();
+               HttpEntity resEntity = response.getEntity();
             if (response.getStatusLine().getStatusCode() == HttpStatus.INTERNAL_SERVER_ERROR.value()
                     || resEntity == null) {
                 log.error("unable to get a response");
