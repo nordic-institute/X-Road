46cb77b640640a6ac2fe18a606787e1abbe6d07f
Mikko Riippi <mikko.riippi@gofore.com>
2020-10-20 11:13:03 +0300

    XRDDEV-1396 Add library entry point

---
+++ src/shared-ui/src/lib.ts
@@ -0,0 +1,21 @@
+// This file is the entry point for the library build
+import { VueConstructor } from "vue";
+import VuetifyButton from "./components/VuetifyButton.vue";
+// Import vee-validate so it's configured on the library build
+import "./plugins/vee-validate";
+
+const MyComponents = {
+  install(Vue: VueConstructor, options?: any) {
+    /* 
+    // In theory a shared vuex store could be used like this:
+    if (!options || !options.store) {
+      throw new Error('Please initialise plugin with a Vuex store.')
+    }
+    options.store.registerModule('dummylib', store) 
+    */
+
+    Vue.component("vuetify-button", VuetifyButton);
+  }
+};
+
+export default MyComponents;
