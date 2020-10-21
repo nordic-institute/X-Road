6f1acc13b25057fb1076f1421163b17f2a39a42e
Mikko Riippi <mikko.riippi@gofore.com>
2020-10-21 22:55:46 +0300

    XRDDEV-1396 Library updates

--- src/shared-ui/.storybook/main.js
+++ src/shared-ui/.storybook/main.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const rootPath = path.resolve(__dirname, '../')
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -6,5 +9,23 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // the @ alias points to the <code>src/</code> directory, a common alias
+    // used in the Vue community
+    config.resolve.alias['@'] = path.resolve(__dirname, '..', 'src');
+
+    config.module.rules.push({
+     test: /\.s(c|a)ss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    // Return the altered config
+    return config;
+  },
 }
--- src/shared-ui/.storybook/preview.js
+++ src/shared-ui/.storybook/preview.js
@@ -1,4 +1,25 @@
+import vuetifyConfig from '../src/plugins/vuetify';
+import '../src/plugins/vee-validate';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import "@mdi/font/css/materialdesignicons.css";
+import i18n from '../src/i18n';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+const appDecorator = () => {
+  return {
+    vuetify: vuetifyConfig,
+    i18n,
+    template: `
+      <v-app>
+          <v-main>
+            <story/>
+          </v-main>
+      </v-app>
+    `
+  };
+};
+
+export const decorators = [appDecorator];
