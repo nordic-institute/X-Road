e685b824a81f1af48f402ddf54191b3fd04cc0f4
Raul Martinez <raul.martinez@leadin.co.uk>
2020-11-12 17:39:46 +0100

    XRDDEV-1291 Replace sidecar image tag 'latest' and '6.23.0' by tag '6.24.0'

--- setup_security_server_sidecar.sh
+++ setup_security_server_sidecar.sh
@@ -48,7 +48,7 @@ docker network inspect xroad-network >/dev/null 2>&1 || docker network create -d
 echo "=====> Build sidecar image"
 docker build -f sidecar/Dockerfile -t xroad-sidecar-security-server-image sidecar/
 echo "=====> Run container"
-docker run  --detach -p $2:4000 -p $httpport:80 -p 5588:5588 --network xroad-network -e XROAD_TOKEN_PIN=$3 -e XROAD_ADMIN_USER=$4 -e XROAD_ADMIN_PASSWORD=$5 -e XROAD_DB_HOST=$postgresqlhost -e XROAD_DB_PORT=$postgresqlport -e XROAD_DB_PWD=$XROAD_DB_PASSWORD  -e XROAD_LOG_LEVEL=$XROAD_LOG_LEVEL -e XROAD_DATABASE_NAME=$8 --name $1 xroad-sidecar-security-server-image:latest
+docker run  --detach -p $2:4000 -p $httpport:80 -p 5588:5588 --network xroad-network -e XROAD_TOKEN_PIN=$3 -e XROAD_ADMIN_USER=$4 -e XROAD_ADMIN_PASSWORD=$5 -e XROAD_DB_HOST=$postgresqlhost -e XROAD_DB_PORT=$postgresqlport -e XROAD_DB_PWD=$XROAD_DB_PASSWORD  -e XROAD_LOG_LEVEL=$XROAD_LOG_LEVEL -e XROAD_DATABASE_NAME=$8 --name $1 xroad-sidecar-security-server-image:6.24.0
 
 printf "\n
 Sidecar security server software token PIN is set to $3
