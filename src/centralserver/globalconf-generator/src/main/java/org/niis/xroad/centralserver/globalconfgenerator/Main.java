b933e5e1b8205fd74df5b95d9badc2631505d761
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2021-02-20 11:24:56 +0200

    XRDDEV-1576: Fix shared-ui gradle build and sharing

--- src/centralserver/globalconf-generator/src/main/java/org/niis/xroad/centralserver/globalconfgenerator/Main.java
+++ src/centralserver/globalconf-generator/src/main/java/org/niis/xroad/centralserver/globalconfgenerator/Main.java
@@ -33,6 +33,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        var ctx = SpringApplication.run(Main.class, args);
     }
 }
