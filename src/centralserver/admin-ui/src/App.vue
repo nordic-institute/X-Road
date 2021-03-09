25eb1c163ed4cff0f04f7003471d1b3db9714192
Mikko Riippi <mikko.riippi@gofore.com>
2021-03-09 14:37:12 +0200

    XRDDEV-1606 Add linter fixes and license headers

--- src/centralserver/admin-ui/src/App.vue
+++ src/centralserver/admin-ui/src/App.vue
@@ -1,19 +1,64 @@
+<!--
+   The MIT License
+   Copyright (c) 2019- Nordic Institute for Interoperability Solutions (NIIS)
+   Copyright (c) 2018 Estonian Information System Authority (RIA),
+   Nordic Institute for Interoperability Solutions (NIIS), Population Register Centre (VRK)
+   Copyright (c) 2015-2017 Estonian Information System Authority (RIA), Population Register Centre (VRK)
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+ -->
 <template>
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    Bloop: {{ USERNAME }}
     <router-view />
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue';
+import { StoreTypes } from '@/global';
+import { mapGetters } from 'vuex';
+
+export default Vue.extend({
+  name: 'App',
+  computed: {
+    ...mapGetters([StoreTypes.getters.USERNAME]),
+  },
+
+  created() {
+    // Example of store usage
+    this.$store.commit(StoreTypes.mutations.SET_USERNAME, 'User one');
+    this.$store.getters[StoreTypes.getters.USERNAME];
+  },
+});
+</script>
+
 <style lang="scss">
-@import "./assets/global-style";
+@import './assets/global-style';
 </style>
 
 <style lang="scss" scoped>
-@import "./assets/colors";
+@import './assets/colors';
 
 .fade-enter-active,
 .fade-leave-active {
--- src/centralserver/admin-ui/tests/e2e/custom-commands/customExecute.js
+++ src/centralserver/admin-ui/tests/e2e/custom-commands/customExecute.js
@@ -1,3 +1,28 @@
+/*
+ * The MIT License
+ * Copyright (c) 2019- Nordic Institute for Interoperability Solutions (NIIS)
+ * Copyright (c) 2018 Estonian Information System Authority (RIA),
+ * Nordic Institute for Interoperability Solutions (NIIS), Population Register Centre (VRK)
+ * Copyright (c) 2015-2017 Estonian Information System Authority (RIA), Population Register Centre (VRK)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 /**
  * A very basic Nightwatch custom command. The command name is the filename and the
  *  exported "command" function is the command.
@@ -22,17 +47,18 @@ exports.command = function command(data) {
   //
   this.execute(
     // The function argument is converted to a string and sent to the browser
-    function(argData) {
+    function (argData) {
       return argData;
     },
 
     // The arguments for the function to be sent to the browser are specified in this array
     [data],
 
-    function(result) {
+    function (result) {
       // The "result" object contains the result of what we have sent back from the browser window
-      console.log("custom execute result:", result.value);
-    }
+      // eslint-disable-next-line no-console
+      console.log('custom execute result:', result.value);
+    },
   );
 
   return this;
