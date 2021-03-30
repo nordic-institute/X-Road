0842578666ad3f10a735ab751a783cf3d34cefaf
Ilkka Seppälä <ilkka.seppala@gofore.com>
2021-03-30 13:20:00 +0300

    XRDDEV-1581 remove preauth

--- src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/openapi/OpenapiApiController.java
+++ src/proxy-ui-api/src/main/java/org/niis/xroad/restapi/openapi/OpenapiApiController.java
@@ -47,7 +47,6 @@ import static org.niis.xroad.restapi.exceptions.DeviationCodes.ERROR_OPENAPI_FIL
 @Controller
 @RequestMapping(ApiUtil.API_V1_PREFIX)
 @Slf4j
-@PreAuthorize("denyAll")
 @RequiredArgsConstructor
 public class OpenapiApiController implements OpenapiApi {
 
