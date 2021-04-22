38a5ed8a689dad858ff16394cd45226ad75cb0a0
Raul Martinez <raul.martinez@leadin.co.uk>
2021-04-22 09:05:57 +0200

    XRDDEV-1599 Minor updates on the script granting IAM access and jenkins pipelines.

--- sidecar/kubernetes/grant-user-access-to-cluster.sh
+++ sidecar/kubernetes/grant-user-access-to-cluster.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-CLUSTER_NAME=$1
-REGION_CODE=$2
-USER_NAME=$3
-USER_ARN=$4
-
-ROLE="    - userarn: $USER_ARN\n      username: $USER_NAME\n      groups:\n        - system:masters\n        - system:nodes"
+USER_NAME=$1
+USER_ARN=$2
 
 kubectl get -n kube-system configmap/aws-auth -o yaml > /tmp/aws-auth-patch.yml
 
-isInFile=$(cat  /tmp/aws-auth-patch.yml | grep -c "mapUsers")
+isInFile=$(cat /tmp/aws-auth-patch.yml | grep -c "mapUsers")
 
+# Add the IAM user to the aws-auth ConfigMap to operate the EKS cluster nodes
+ROLE="    - userarn: $USER_ARN\n      username: $USER_NAME\n      groups:\n        - system:masters\n        - system:nodes"
 if [ $isInFile -eq 0 ]; then
-kubectl get -n kube-system configmap/aws-auth -o yaml | awk "/^data:/{print;print \"  mapUsers: \| \n$ROLE\";next}1" > /tmp/aws-auth-patch.yml
+    kubectl get -n kube-system configmap/aws-auth -o yaml | awk "/^data:/{print;print \"  mapUsers: \| \n$ROLE\";next}1" > /tmp/aws-auth-patch.yml
 else
-kubectl get -n kube-system configmap/aws-auth -o yaml | awk "/mapUsers: \|/{print;print \"$ROLE\";next}1" > /tmp/aws-auth-patch.yml
+    kubectl get -n kube-system configmap/aws-auth -o yaml | awk "/mapUsers: \|/{print;print \"$ROLE\";next}1" > /tmp/aws-auth-patch.yml
 fi
 
 kubectl patch configmap/aws-auth -n kube-system --patch "$(cat /tmp/aws-auth-patch.yml)"
