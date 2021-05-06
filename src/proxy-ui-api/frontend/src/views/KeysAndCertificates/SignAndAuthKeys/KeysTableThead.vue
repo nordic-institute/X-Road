4eaa5d100c799e5955c33a82f996453f8ce6a4cf
Mikko Riippi <mikko.riippi@gofore.com>
2021-05-06 18:47:46 +0300

    XRDDEV-1639 Add new files

---
+++ src/proxy-ui-api/frontend/src/views/KeysAndCertificates/SignAndAuthKeys/KeysTableThead.vue
@@ -0,0 +1,189 @@
+<!--
+   The MIT License
+   Copyright (c) 2019- Nordic Institute for Interoperability Solutions (NIIS)
+   Copyright (c) 2018 Estonian Information System Authority (RIA),
+   Nordic Institute for Interoperability Solutions (NIIS), Population Register Centre (VRK)
+   Copyright (c) 2015-2017 Estonian Information System Authority (RIA), Population Register Centre (VRK)
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+ -->
+<template>
+  <thead>
+    <tr>
+      <th
+        class="title-col"
+        @click="setSort(sortColumn.NAME)"
+        @mouseover="hoverName = true"
+        @mouseleave="hoverName = false"
+      >
+        <div class="header-title">
+          <div>{{ $t(title) }}</div>
+          <sort-button
+            :arrowState="sortDirection"
+            :active="hoverName"
+            :selected="selectedSort === sortColumn.NAME"
+          />
+        </div>
+      </th>
+      <th
+        class="id-col"
+        @click="setSort(sortColumn.ID)"
+        @mouseover="hoverId = true"
+        @mouseleave="hoverId = false"
+      >
+        <div class="header-title">
+          {{ $t('keys.id') }}
+          <sort-button
+            :arrowState="sortDirection"
+            :active="hoverId"
+            :selected="selectedSort === sortColumn.ID"
+          />
+        </div>
+      </th>
+      <th
+        class="ocsp-col"
+        @click="setSort(sortColumn.OCSP)"
+        @mouseover="hoverOcsp = true"
+        @mouseleave="hoverOcsp = false"
+      >
+        <div class="header-title">
+          {{ $t('keys.ocsp') }}
+          <sort-button
+            :arrowState="sortDirection"
+            :active="hoverOcsp"
+            :selected="selectedSort === sortColumn.OCSP"
+          />
+        </div>
+      </th>
+      <th
+        class="expiration-col"
+        @click="setSort(sortColumn.EXPIRATION)"
+        @mouseover="hoverExp = true"
+        @mouseleave="hoverExp = false"
+      >
+        <div class="header-title">
+          {{ $t('keys.expires') }}
+          <sort-button
+            :arrowState="sortDirection"
+            :active="hoverExp"
+            :selected="selectedSort === sortColumn.EXPIRATION"
+          />
+        </div>
+      </th>
+      <th
+        class="status-col"
+        @click="setSort(sortColumn.STATUS)"
+        @mouseover="hoverStatus = true"
+        @mouseleave="hoverStatus = false"
+      >
+        <div class="header-title">
+          {{ $t('keys.status') }}
+          <sort-button
+            :arrowState="sortDirection"
+            :active="hoverStatus"
+            :selected="selectedSort === sortColumn.STATUS"
+          />
+        </div>
+      </th>
+      <th class="action-col"></th>
+    </tr>
+  </thead>
+</template>
+
+<script lang="ts">
+/**
+ * Table component for an array of keys
+ */
+import Vue from 'vue';
+import SortButton from './SortButton.vue';
+import { Colors } from '@/global';
+import { KeysSortColumn } from '@/global';
+
+export default Vue.extend({
+  components: {
+    SortButton,
+  },
+  props: {
+    sortDirection: {
+      type: Boolean,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    selectedSort: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      colors: Colors,
+      titleSort: false,
+      sortColumn: KeysSortColumn,
+      hoverName: false,
+      hoverId: false,
+      hoverOcsp: false,
+      hoverExp: false,
+      hoverStatus: false,
+    };
+  },
+  methods: {
+    setSort(sort: KeysSortColumn): void {
+      this.$emit('set-sort', sort);
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '~styles/tables';
+
+.title-col {
+  width: 30%;
+  cursor: pointer;
+  user-select: none;
+}
+
+.id-col {
+  cursor: pointer;
+  user-select: none;
+}
+
+.ocsp-col,
+.expiration-col,
+.status-col,
+.action-col {
+  width: 10%;
+  font-weight: 700;
+  color: $XRoad-Black100;
+  cursor: pointer;
+  user-select: none;
+}
+
+.header-title {
+  font-size: 12px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-weight: 700;
+  color: $XRoad-Black100;
+}
+</style>
---
+++ src/proxy-ui-api/frontend/src/views/KeysAndCertificates/SignAndAuthKeys/KeysTableTitle.vue
@@ -0,0 +1,151 @@
+<!--
+   The MIT License
+   Copyright (c) 2019- Nordic Institute for Interoperability Solutions (NIIS)
+   Copyright (c) 2018 Estonian Information System Authority (RIA),
+   Nordic Institute for Interoperability Solutions (NIIS), Population Register Centre (VRK)
+   Copyright (c) 2015-2017 Estonian Information System Authority (RIA), Population Register Centre (VRK)
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+ -->
+<template>
+  <div class="table-title" :class="{ 'table-closed': !arrowState }">
+    <div class="title-text">
+      <v-btn icon :color="colors.WarmGrey100" @click="arrowClick">
+        <v-icon v-if="arrowState">icon-Sorting-arrow</v-icon>
+        <v-icon v-else class="arrow-degree">icon-Sorting-arrow</v-icon>
+      </v-btn>
+
+      {{ $t(title) }}
+    </div>
+    <div class="status-wrap">
+      <div v-if="errors > 0" class="errors">
+        <v-icon color="red">icon-Error</v-icon> {{ errorCount }}
+        {{ $t('keys.globalErrors') }}
+      </div>
+      <div v-else-if="registered === certificateCount" class="registered">
+        <v-icon color="green">icon-Checked</v-icon> {{ $t('keys.noIssues') }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+// View for a token
+import Vue from 'vue';
+import { Colors } from '@/global';
+import { TokenCertificate, CertificateStatus } from '@/openapi-types';
+
+export default Vue.extend({
+  props: {
+    keys: {
+      type: Array,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    arrowState: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  computed: {
+    errorCount(): number {
+      let errors = 0;
+
+      this.keys.forEach((key: any) => {
+        key.certificates.forEach((cert: TokenCertificate) => {
+          if (cert.status === CertificateStatus.GLOBAL_ERROR) {
+            errors++;
+          }
+        });
+      });
+
+      return errors;
+    },
+  },
+  data() {
+    return {
+      errors: 0,
+      registered: 0,
+      certificateCount: 0,
+      colors: Colors,
+    };
+  },
+  methods: {
+    countStates(): void {
+      this.keys.forEach((key: any) => {
+        key.certificates.forEach((cert: TokenCertificate) => {
+          if (cert.status === CertificateStatus.GLOBAL_ERROR) {
+            this.errors++;
+          } else if (cert.status === CertificateStatus.REGISTERED) {
+            this.registered++;
+          }
+          this.certificateCount++;
+        });
+      });
+    },
+    arrowClick(): void {
+      this.$emit('click');
+    },
+  },
+  created() {
+    this.countStates();
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '~styles/tables';
+@import '~styles/colors';
+
+.status-wrap {
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 700;
+  color: $XRoad-WarmGrey100;
+}
+
+.errors {
+  color: $XRoad-Error;
+}
+
+.table-title {
+  margin-top: -1px; // avoid double 2px border with multiple components
+  width: 100%;
+  border-top: 1px solid $XRoad-WarmGrey30;
+  padding: 10px;
+  padding-right: 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 100%;
+  align-items: center;
+}
+
+.table-closed {
+  // Show bottom border only when table is closed
+  border-bottom: 1px solid $XRoad-WarmGrey30;
+}
+
+.arrow-degree {
+  transform: translate(-4px) rotate(-90deg);
+}
+</style>
