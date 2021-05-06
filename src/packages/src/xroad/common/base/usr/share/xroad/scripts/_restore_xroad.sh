c827c9da6339c0efd199698ee9b099ac28142347
Andres Allkivi <andres.allkivi@gofore.com>
2021-05-06 18:48:21 +0300

    fixed bug in restore - nginx *.conf files were not restored

--- src/packages/src/xroad/common/base/usr/share/xroad/scripts/_restore_xroad.sh
+++ src/packages/src/xroad/common/base/usr/share/xroad/scripts/_restore_xroad.sh
@@ -184,7 +184,7 @@ setup_tmp_restore_dir() {
 extract_to_tmp_restore_dir () {
   # Restore to temporary directory and fix permissions before copying
   # etc/xroad is always included in the backup, etc/nginx only when backup is for CS
-  tar xfv ${BACKUP_FILENAME} -C ${RESTORE_DIR} --exclude="*.conf" --exclude="gpghome/*" etc/xroad || die "Extracting etc/xroad failed"
+  tar xfv ${BACKUP_FILENAME} -C ${RESTORE_DIR} --exclude="services/*.conf" --exclude="gpghome/*" etc/xroad || die "Extracting etc/xroad failed"
   if tar -tf ${BACKUP_FILENAME} etc/nginx >/dev/null 2>&1; then
     echo "Extracting tar archive to etc/nginx"
     tar xfv ${BACKUP_FILENAME} -C ${RESTORE_DIR} etc/nginx || die "Extracting etc/nginx failed"
