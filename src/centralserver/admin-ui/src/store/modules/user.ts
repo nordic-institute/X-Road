b96c7de1df11dbfdcb79e07e3f6255d4397f7b3e
Caro Hautamäki <caro.hautamaki@gofore.com>
2021-06-28 14:48:49 +0300

    XRDDEV-1715 Fix the Version model path

--- src/centralserver/admin-ui/src/store/modules/user.ts
+++ src/centralserver/admin-ui/src/store/modules/user.ts
@@ -27,7 +27,7 @@ import axiosAuth from '../../axios-auth';
 import axios from 'axios';
 import { ActionTree, GetterTree, Module, MutationTree } from 'vuex';
 import { RootState, StoreTypes } from '@/global';
-import { Version } from '@/openapi-types';
+import { Version } from '@/openapi-types/cs-types';
 
 export interface State {
   authenticated: boolean;
