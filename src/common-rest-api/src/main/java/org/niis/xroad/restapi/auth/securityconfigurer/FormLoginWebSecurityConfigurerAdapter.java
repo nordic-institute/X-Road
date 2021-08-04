2e6ea4ca332f8ed2ceab9f9318532d380cb4d88f
Teemu Kurki <teemu.kurki@gofore.com>
2021-08-04 13:39:37 +0300

    XRDDEV-1702 CSP configs added for SS port 4000 paths

--- src/common-rest-api/src/main/java/org/niis/xroad/restapi/auth/securityconfigurer/FormLoginWebSecurityConfigurerAdapter.java
+++ src/common-rest-api/src/main/java/org/niis/xroad/restapi/auth/securityconfigurer/FormLoginWebSecurityConfigurerAdapter.java
@@ -1,27 +1,27 @@
-/**
- * The MIT License
- * Copyright (c) 2019- Nordic Institute for Interoperability Solutions (NIIS)
- * Copyright (c) 2018 Estonian Information System Authority (RIA),
- * Nordic Institute for Interoperability Solutions (NIIS), Population Register Centre (VRK)
- * Copyright (c) 2015-2017 Estonian Information System Authority (RIA), Population Register Centre (VRK)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+/*
+  The MIT License
+  Copyright (c) 2019- Nordic Institute for Interoperability Solutions (NIIS)
+  Copyright (c) 2018 Estonian Information System Authority (RIA),
+  Nordic Institute for Interoperability Solutions (NIIS), Population Register Centre (VRK)
+  Copyright (c) 2015-2017 Estonian Information System Authority (RIA), Population Register Centre (VRK)
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
  */
 package org.niis.xroad.restapi.auth.securityconfigurer;
 
@@ -90,6 +90,10 @@ public class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurer
                 .ignoringAntMatchers(LOGIN_URL)
                 .csrfTokenRepository(new CookieAndSessionCsrfTokenRepository())
                 .and()
+            .headers()
+                    .contentSecurityPolicy("default-src 'self' 'unsafe-inline'")
+                    .and()
+                .and()
             .formLogin()
                 .loginPage(LOGIN_URL)
                 .successHandler(formLoginStatusCodeSuccessHandler())
@@ -121,13 +125,14 @@ public class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurer
 
     /**
      * authentication failure handler which does not redirect but just returns a http status code
-     * @return
+     * @return the constructed AuthenticationFailureHandler handler
      */
     private static AuthenticationFailureHandler statusCode401AuthenticationFailureHandler() {
         return new SimpleUrlAuthenticationFailureHandler() {
+            @Override
             public void onAuthenticationFailure(HttpServletRequest request,
                     HttpServletResponse response, AuthenticationException exception)
-                    throws IOException, ServletException {
+                    throws IOException {
                 response.setContentType("application/json;charset=UTF-8");
                 response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authentication Failed");
             }
@@ -136,13 +141,14 @@ public class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurer
 
     /**
      * form login success handler which does not redirect but just returns a http status code
-     * @return
+     * @return the constructed AuthenticationSuccessHandler handler
      */
     private static AuthenticationSuccessHandler formLoginStatusCodeSuccessHandler() {
         return new SimpleUrlAuthenticationSuccessHandler() {
+            @Override
             public void onAuthenticationSuccess(HttpServletRequest request,
                     HttpServletResponse response, Authentication authentication)
-                    throws IOException, ServletException {
+                    throws IOException {
                 response.setContentType("application/json;charset=UTF-8");
                 response.getWriter().println("OK");
             }
