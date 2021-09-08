afd0ec6d124e9c3e1e245d96428e4984aa658438
Teemu Kurki <teemu.kurki@gofore.com>
2021-09-08 16:18:28 +0300

    XRDDEV-1711 less magic strings, static final strings instead

--- src/centralserver/admin-service/src/main/java/org/niis/xroad/centralserver/restapi/config/CurrentHAConfigStatus.java
+++ src/centralserver/admin-service/src/main/java/org/niis/xroad/centralserver/restapi/config/CurrentHAConfigStatus.java
@@ -1,21 +1,21 @@
 /**
  * The MIT License
- *
+ * <p>
  * Copyright (c) 2019- Nordic Institute for Interoperability Solutions (NIIS)
  * Copyright (c) 2018 Estonian Information System Authority (RIA),
  * Nordic Institute for Interoperability Solutions (NIIS), Population Register Centre (VRK)
  * Copyright (c) 2015-2017 Estonian Information System Authority (RIA), Population Register Centre (VRK)
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -32,6 +32,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CurrentHAConfigStatus {
 
+    private static final String XROAD_HA_NODE_NAME_PROPERTY = "xroad.center.ha-node-name";
+    private static final String XROAD_HA_NODE_NAME_DEFAULT = "node_0";
+
     @Bean
     HAConfigStatus currentHaConfigStatus() {
         boolean isHaConfigured = true;
@@ -45,7 +48,7 @@ public class CurrentHAConfigStatus {
         haNodeName = getHaNodeNameProperty();
         if (haNodeName.isEmpty()) {
             isHaConfigured = false;
-            haNodeName = "node_0";
+            haNodeName = XROAD_HA_NODE_NAME_DEFAULT;
 
         }
         return new HAConfigStatus(haNodeName, isHaConfigured);
@@ -57,7 +60,7 @@ public class CurrentHAConfigStatus {
     }
 
     private String getHaNodeNameProperty() {
-        return System.getProperty("xroad.center.ha-node-name", "");
+        return System.getProperty(XROAD_HA_NODE_NAME_PROPERTY, "");
     }
 
 
