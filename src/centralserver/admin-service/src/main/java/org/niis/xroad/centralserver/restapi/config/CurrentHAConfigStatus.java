bcc988b9734ccf4a0b287308c20b17c6fb6b5b8a
Teemu Kurki <teemu.kurki@gofore.com>
2021-09-15 09:28:35 +0300

    XRDDEV-1711 HA config detected only from config property

--- src/centralserver/admin-service/src/main/java/org/niis/xroad/centralserver/restapi/config/CurrentHAConfigStatus.java
+++ src/centralserver/admin-service/src/main/java/org/niis/xroad/centralserver/restapi/config/CurrentHAConfigStatus.java
@@ -40,23 +40,12 @@ public class CurrentHAConfigStatus {
         boolean isHaConfigured = true;
         String haNodeName;
 
-
-        if (!isPostgresDatabaseInUse()) {
-            isHaConfigured = false;
-        }
-
         haNodeName = getHaNodeNameProperty();
         if (haNodeName.isEmpty()) {
             isHaConfigured = false;
             haNodeName = XROAD_HA_NODE_NAME_DEFAULT;
-
         }
         return new HAConfigStatus(haNodeName, isHaConfigured);
-    }
-
-    private boolean isPostgresDatabaseInUse() {
-        String dataSourceUrl = System.getProperty("spring.datasource.url", "");
-        return dataSourceUrl.toLowerCase().contains("postgres");
     }
 
     private String getHaNodeNameProperty() {
