f8d431bffa24d642877467be6309cddedb698cdd
Jarkko Hyöty <jarkko.hyoty@gofore.com>
2021-10-21 16:45:21 +0300

    XRDDEV-1817: Partially fix configuration migration
    Configuration migration to a new version should work provided that
    * same contaner type (e.g. from slim to slim) is used
    * /etc/xroad is a persistent volume
    * remote database is used (or database data is on a volume)
    * /etc/xroad.properties with database admin passwords is provided (e.g.
      as a mounted file)

--- sidecar/files/_entrypoint_common.sh
+++ sidecar/files/_entrypoint_common.sh
@@ -1,117 +1,162 @@
 #!/bin/bash
+log() { echo "$(date --utc -Iseconds) INFO [entrypoint] $1"; }
+warn() { echo "$(date --utc -Iseconds) WARN [entrypoint] $1" >&2; }
+
 XROAD_SCRIPT_LOCATION=/usr/share/xroad/scripts
 DB_PROPERTIES=/etc/xroad/db.properties
 ROOT_PROPERTIES=/etc/xroad.properties
-GROUPNAMES="xroad-security-officer xroad-registration-officer xroad-service-administrator xroad-system-administrator xroad-securityserver-observer"
-
+GROUPNAMES=(xroad-security-officer xroad-registration-officer xroad-service-administrator xroad-system-administrator xroad-securityserver-observer)
 INSTALLED_VERSION=$(dpkg-query --showformat='${Version}' --show xroad-proxy)
-PACKAGED_VERSION="$(cat /root/VERSION)"
+PACKAGED_CONFIG=/usr/share/xroad/config
+PACKAGED_VERSION="$(cat /${PACKAGED_CONFIG}/VERSION)"
 
-if [ "$INSTALLED_VERSION" == "$PACKAGED_VERSION" ]; then
-    if [ -f /etc/xroad/VERSION ]; then
-        CONFIG_VERSION="$(cat /etc/xroad/VERSION)"
-    else
-        echo "WARN: Current configuration version not known" >&2
-        CONFIG_VERSION=
-    fi
-    if dpkg --compare-versions "$PACKAGED_VERSION" gt "$CONFIG_VERSION"; then
-        echo "Updating configuration from $CONFIG_VERSION to $PACKAGED_VERSION"
-        cp -a /root/etc/xroad/* /etc/xroad/
-        cp -a -n /root/backup/local.ini /etc/xroad/conf.d/
-        cp -a -n /root/backup/local.conf /etc/xroad/services/
-        cp -a -n /root/backup/devices.ini /etc/xroad/
-        pg_ctlcluster 12 main start
-        pg_isready -t 12
-        dpkg-reconfigure xroad-proxy
-        pg_ctlcluster 12 main stop
-        sleep 1
-        echo "$PACKAGED_VERSION" >/etc/xroad/VERSION
-    fi
-else
-    echo "WARN: Installed version ($INSTALLED_VERSION) does not match packaged version ($PACKAGED_VERSION)" >&2
+RECONFIG=(xroad-signer xroad-proxy)
+if [ -f /usr/share/xroad/jlib/addon/proxy/messagelog.conf ]; then
+  RECONFIG+=(xroad-addon-messagelog)
 fi
+if [ -f /usr/share/xroad/jlib/addon/proxy/opmonitoring.conf ]; then
+  RECONFIG+=(xroad-opmonitor)
+fi
+
+LOCAL_DB=
+
+if [ -f /.xroad-reconfigured ]; then
+  RECONFIG_REQUIRED=${RECONFIG_REQUIRED:-false}
+else
+  RECONFIG_REQUIRED=${RECONFIG_REQUIRED:-true}
+fi
+
+log "Starting X-Road Security Server version $PACKAGED_VERSION"
 
 # Configure admin user with user-supplied username and password
 if ! getent passwd "$XROAD_ADMIN_USER" &>/dev/null; then
-    echo "Creating admin user with user-supplied credentials"
-    useradd -m "${XROAD_ADMIN_USER}" -s /usr/sbin/nologin
-    echo "${XROAD_ADMIN_USER}:${XROAD_ADMIN_PASSWORD}" | chpasswd
-    echo "xroad-proxy xroad-common/username string ${XROAD_ADMIN_USER}" | debconf-set-selections
+  log "Creating admin user with user-supplied credentials"
+  useradd -m "${XROAD_ADMIN_USER}" -s /usr/sbin/nologin
+  echo "${XROAD_ADMIN_USER}:${XROAD_ADMIN_PASSWORD}" | chpasswd
+  echo "xroad-proxy xroad-common/username string ${XROAD_ADMIN_USER}" | debconf-set-selections
 
-    echo "Configuring groups"
-    usergroups=" $(id -Gn "${XROAD_ADMIN_USER}") "
+  usergroups=" $(id -Gn "${XROAD_ADMIN_USER}") "
+  for groupname in "${GROUPNAMES[@]}"; do
+    if [[ $usergroups != *" $groupname "* ]]; then
+      usermod -a -G "$groupname" "${XROAD_ADMIN_USER}" || true
+    fi
+  done
+fi
 
-    for groupname in ${GROUPNAMES}; do
-        if [[ $usergroups != *" $groupname "* ]]; then
-            echo "$groupname"
-            usermod -a -G "$groupname" "${XROAD_ADMIN_USER}" || true
-        fi
-    done
+if [ "$INSTALLED_VERSION" == "$PACKAGED_VERSION" ]; then
+  if [ -f /etc/xroad/VERSION ]; then
+    CONFIG_VERSION="$(cat /etc/xroad/VERSION)"
+  else
+    warn "Current configuration version not known"
+    CONFIG_VERSION=
+  fi
+  if dpkg --compare-versions "$PACKAGED_VERSION" gt "$CONFIG_VERSION"; then
+    # handles also the case where configuration is missing (e.g. config volume not automatically populated)
+    log "Migrating configuration from ${CONFIG_VERSION:-none} to $PACKAGED_VERSION"
+    cp -a "$PACKAGED_CONFIG/etc/xroad/"* /etc/xroad/
+    # copy if not exists
+    cp -a -n "$PACKAGED_CONFIG"/backup/local.ini /etc/xroad/conf.d/
+    cp -a -n "$PACKAGED_CONFIG"/backup/devices.ini /etc/xroad/
+
+    RECONFIG_REQUIRED=true
+  fi
+else
+    warn "Installed version ($INSTALLED_VERSION) does not match packaged version ($PACKAGED_VERSION)"
 fi
 
 # Generate internal and admin UI TLS keys and certificates on the first run
 if [ ! -f /etc/xroad/ssl/internal.crt ]; then
-    echo "Generating new internal TLS key and certificate"
-    ARGS="-n internal -f -S -p"
-    $XROAD_SCRIPT_LOCATION/generate_certificate.sh $ARGS
+  log "Generating new internal TLS key and certificate"
+  "$XROAD_SCRIPT_LOCATION/generate_certificate.sh" -n internal -f -S -p 2>&1 >/dev/null | sed 's/^/    /'
 fi
 
 if [ ! -f /etc/xroad/ssl/proxy-ui-api.crt ]; then
-    echo "Generating new SSL key and certificate for the admin UI"
-    ARGS="-n proxy-ui-api -f -S -p"
-    $XROAD_SCRIPT_LOCATION/generate_certificate.sh $ARGS
+  log "Generating new SSL key and certificate for the admin UI"
+  "$XROAD_SCRIPT_LOCATION/generate_certificate.sh" -n proxy-ui-api -f -S -p 2>&1 >/dev/null | sed 's/^/   /'
 fi
 
-# Recreate serverconf database and properties file with serverconf username and random password on the first run
+# Recreate serverconf database and properties file with on the first run
 if [ ! -f ${DB_PROPERTIES} ]; then
-    echo "Creating serverconf database and properties file"
+  XROAD_DB_PORT="${XROAD_DB_PORT:-5432}"
+  XROAD_DB_HOST="${XROAD_DB_HOST:-127.0.0.1}"
+  log "Creating serverconf database and properties file"
+  RECONFIG_REQUIRED=true
+  if [[ "${XROAD_DB_HOST}" != "127.0.0.1" ]]; then
+    LOCAL_DB=false
+    log "Using remote database $XROAD_DB_HOST:$XROAD_DB_PORT"
     if [ -f /usr/share/xroad/jlib/addon/proxy/messagelog.conf ]; then
       messagelog=true
     fi
     if [ -f /usr/share/xroad/jlib/addon/proxy/opmonitoring.conf ]; then
       opmonitor=true
     fi
-    if [[ -n "${XROAD_DB_PWD}" && "${XROAD_DB_HOST}" != "127.0.0.1" ]]; then
-        echo "xroad-proxy xroad-common/database-host string ${XROAD_DB_HOST}:${XROAD_DB_PORT}" | debconf-set-selections
-        touch /etc/xroad.properties
-        chown root:root /etc/xroad.properties
-        chmod 600 /etc/xroad.properties
-        echo "postgres.connection.password = ${XROAD_DB_PWD}" >> ${ROOT_PROPERTIES}
-        if [ -n "${XROAD_DATABASE_NAME}" ]; then
-          touch /etc/xroad/db.properties
-          chown xroad:xroad /etc/xroad/db.properties
-          chmod 640 /etc/xroad/db.properties
-          echo "serverconf.database.admin_user = ${XROAD_DATABASE_NAME}_admin" >> ${ROOT_PROPERTIES}
-          echo "serverconf.hibernate.connection.username= ${XROAD_DATABASE_NAME}_serverconf" >> ${DB_PROPERTIES}
-          echo "serverconf.hibernate.connection.url = jdbc:postgresql://${XROAD_DB_HOST}:${XROAD_DB_PORT}/${XROAD_DATABASE_NAME}_serverconf" >> ${DB_PROPERTIES}
-          if [ -n "$opmonitor" ]; then
-            echo "op-monitor.database.admin_user = ${XROAD_DATABASE_NAME}_op_monitor_admin" >> ${ROOT_PROPERTIES}
-            echo "op-monitor.hibernate.connection.username= ${XROAD_DATABASE_NAME}_op-monitor" >> ${DB_PROPERTIES}
-            echo "op-monitor.hibernate.connection.url = jdbc:postgresql://${XROAD_DB_HOST}:${XROAD_DB_PORT}/${XROAD_DATABASE_NAME}_op-monitor" >> ${DB_PROPERTIES}
-          fi
-          if [ -n "$messagelog" ]; then
-            echo "messagelog.database.admin_user = ${XROAD_DATABASE_NAME}_messagelog_admin" >> ${ROOT_PROPERTIES}
-            echo "messagelog.hibernate.connection.username= ${XROAD_DATABASE_NAME}_messagelog" >> ${DB_PROPERTIES}
-            echo "messagelog.hibernate.connection.url = jdbc:postgresql://${XROAD_DB_HOST}:${XROAD_DB_PORT}/${XROAD_DATABASE_NAME}_messagelog" >> ${DB_PROPERTIES}
-          fi
-        fi
-        crudini --del /etc/supervisor/conf.d/xroad.conf program:postgres
-        dpkg-reconfigure -fnoninteractive xroad-proxy
-        [ -n "$messagelog" ] && dpkg-reconfigure -fnoninteractive xroad-addon-messagelog
-        [ -n "$opmonitor" ] && dpkg-reconfigure -fnoninteractive xroad-opmonitor
-    else
-        pg_ctlcluster 12 main start
-        dpkg-reconfigure -fnoninteractive xroad-proxy
-        [ -n "$messagelog" ] && dpkg-reconfigure -fnoninteractive xroad-addon-messagelog
-        [ -n "$opmonitor" ] && dpkg-reconfigure -fnoninteractive xroad-opmonitor
-        pg_ctlcluster 12 main stop
+    echo "xroad-proxy xroad-common/database-host string ${XROAD_DB_HOST}:${XROAD_DB_PORT}" | debconf-set-selections
+    touch /etc/xroad.properties
+    chown root:root /etc/xroad.properties
+    chmod 600 /etc/xroad.properties
+    if [ -n "$XROAD_DB_PWD" ]; then
+      crudini --set --inplace "$ROOT_PROPERTIES" "" "postgres.connection.password" "${XROAD_DB_PWD}"
     fi
+    if [ -n "${XROAD_DATABASE_NAME}" ]; then
+      touch /etc/xroad/db.properties
+      chown xroad:xroad /etc/xroad/db.properties
+      chmod 640 /etc/xroad/db.properties
+      set_db_props() {
+        crudini --set --inplace "$ROOT_PROPERTIES" "" "$1.database.admin_user" "${XROAD_DATABASE_NAME}_$1_admin"
+        echo "$1.hibernate.connection.username= ${XROAD_DATABASE_NAME}_$1" >> "${DB_PROPERTIES}"
+        echo "$1.hibernate.connection.url = jdbc:postgresql://${XROAD_DB_HOST}:${XROAD_DB_PORT}/${XROAD_DATABASE_NAME}_$1" >> "${DB_PROPERTIES}"
+      }
+      set_db_props serverconf
+      if [ -n "$opmonitor" ]; then
+        set_db_props "op-monitor"
+      fi
+      if [ -n "$messagelog" ]; then
+        set_db_props messagelog
+      fi
+    fi
+  else
+    LOCAL_DB=true
+  fi
+fi
+
+if [[ "$RECONFIG_REQUIRED" == "true" ]]; then
+
+  if [ -z "$LOCAL_DB" ]; then
+    db_url=$(crudini --get '/etc/xroad/db.properties' "" 'serverconf.hibernate.connection.url' 2>/dev/null)
+    pat='^jdbc:postgresql://([^/]*).*'
+    db_host=
+    if [[ "$db_url" =~ $pat ]]; then
+      db_host="${BASH_REMATCH[1]:-127.0.0.1:5432}"
+    fi
+    if [[ -n "$db_host" && "$db_host" != "127.0.0.1:5432" ]]; then
+      LOCAL_DB=false
+    else
+      LOCAL_DB=true
+    fi
+  fi
+
+  if [[ "$LOCAL_DB" == "true" ]]; then
+    pg_ctlcluster 12 main start
+  fi
+  log "Reconfiguring packages"
+  if dpkg-reconfigure -fnoninteractive "${RECONFIG[@]}" 2>&1 | sed 's/^/    /'; then
+    echo "$PACKAGED_VERSION" >/etc/xroad/VERSION
+    touch /.xroad-reconfigured
+  fi
+  if [[ "$LOCAL_DB" == "true" ]]; then
+    pg_ctlcluster 12 main stop
+    sleep 1
+    crudini --set /etc/supervisor/conf.d/xroad.conf program:postgres autostart true
+  else
+    crudini --set /etc/supervisor/conf.d/xroad.conf program:postgres autostart false
+  fi
 fi
 
 if [ -n "${XROAD_LOG_LEVEL}" ]; then
-    sed -i -e "s/XROAD_LOG_LEVEL=.*/XROAD_LOG_LEVEL=${XROAD_LOG_LEVEL}/" /etc/xroad/conf.d/variables-logback.properties
+  sed -i -e "s/XROAD_LOG_LEVEL=.*/XROAD_LOG_LEVEL=${XROAD_LOG_LEVEL}/" /etc/xroad/conf.d/variables-logback.properties
 fi
 
 if [ -n "${XROAD_ROOT_LOG_LEVEL}" ]; then
-    sed -i -e "s/XROAD_ROOT_LOG_LEVEL=.*/XROAD_ROOT_LOG_LEVEL=${XROAD_ROOT_LOG_LEVEL}/" /etc/xroad/conf.d/variables-logback.properties
+  sed -i -e "s/XROAD_ROOT_LOG_LEVEL=.*/XROAD_ROOT_LOG_LEVEL=${XROAD_ROOT_LOG_LEVEL}/" /etc/xroad/conf.d/variables-logback.properties
 fi
+
