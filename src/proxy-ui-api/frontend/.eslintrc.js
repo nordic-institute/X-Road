858c79dcc49ddec6513cf77504ce4ce3dac032de
Mikko Riippi <mikko.riippi@gofore.com>
2021-10-29 11:23:04 +0300

    XRDDEV-1824 Add vue-scoped-css eslint plugin

--- src/proxy-ui-api/frontend/.eslintrc.js
+++ src/proxy-ui-api/frontend/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     '@vue/typescript/recommended',
     '@vue/prettier',
     '@vue/prettier/@typescript-eslint',
+    'plugin:vue-scoped-css/recommended',
   ],
   ignorePatterns: ['node_modules/'],
   parserOptions: {
