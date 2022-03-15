86ed9bfd0312273fd13b63eb4f9f41efd71abd2b
Teemu Kurki <teemu.kurki@gofore.com>
2022-03-15 13:38:02 +0200

    XRDDEV-1914 use JPA repositorys findAll in SecurityServerRepository with Specifications.
    Repository integration test added SecurityServerRepositoryIntegrationTest.java

--- src/centralserver/admin-service/src/main/java/org/niis/xroad/centralserver/restapi/repository/SecurityServerRepository.java
+++ src/centralserver/admin-service/src/main/java/org/niis/xroad/centralserver/restapi/repository/SecurityServerRepository.java
@@ -1,21 +1,21 @@
 /**
  * The MIT License
- *
+ * <p>
  * Copyright (c) 2019- Nordic Institute for Interoperability Solutions (NIIS)
  * Copyright (c) 2018 Estonian Information System Authority (RIA),
  * Nordic Institute for Interoperability Solutions (NIIS), Population Register Centre (VRK)
  * Copyright (c) 2015-2017 Estonian Information System Authority (RIA), Population Register Centre (VRK)
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,8 +28,6 @@ package org.niis.xroad.centralserver.restapi.repository;
 
 import org.niis.xroad.centralserver.restapi.entity.SecurityServer;
 import org.niis.xroad.centralserver.restapi.entity.XRoadMember;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -38,23 +36,27 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+
 import java.util.Optional;
 
 import static org.niis.xroad.centralserver.restapi.entity.SecurityServer_.OWNER;
 import static org.niis.xroad.centralserver.restapi.entity.SecurityServer_.SERVER_CODE;
-import static org.niis.xroad.centralserver.restapi.entity.XRoadMember_.*;
+import static org.niis.xroad.centralserver.restapi.entity.XRoadMember_.MEMBER_CLASS;
+import static org.niis.xroad.centralserver.restapi.entity.XRoadMember_.MEMBER_CODE;
+import static org.niis.xroad.centralserver.restapi.entity.XRoadMember_.NAME;
 
 
 @Repository
-public interface SecurityServerRepository extends PagingAndSortingRepository<SecurityServer, Integer>, JpaSpecificationExecutor<SecurityServer> {
+public interface SecurityServerRepository extends
+        PagingAndSortingRepository<SecurityServer, Integer>,
+        JpaSpecificationExecutor<SecurityServer> {
     Optional<SecurityServer> findByOwnerAndServerCode(XRoadMember owner, String serverCode);
-
-    Page<SecurityServer> findAllBy(Specification<SecurityServer> spec, Pageable pageable);
 
     static Specification<SecurityServer> multifieldSearch(String q) {
         return (root, query, builder) ->
-               builder.like(root.get(SERVER_CODE) , "%"+q+"%");
+                multifieldTextSearch(root, builder, q);
     }
+
 
     private static Predicate serverCodePredicate(Root<SecurityServer> root, CriteriaBuilder builder, String s) {
         return builder.like(
@@ -72,7 +74,7 @@ public interface SecurityServerRepository extends PagingAndSortingRepository<Sec
 
     private static Predicate memberClassPredicate(Root<SecurityServer> root, CriteriaBuilder builder, String s) {
         return builder.like(
-                builder.lower(root.join(OWNER).get(MEMBER_CLASS)),
+                builder.lower(root.join(OWNER).get(MEMBER_CLASS).get("code")),
                 builder.lower(builder.literal("%" + s + "%"))
         );
     }
@@ -86,9 +88,9 @@ public interface SecurityServerRepository extends PagingAndSortingRepository<Sec
 
     private static Predicate multifieldTextSearch(Root<SecurityServer> root, CriteriaBuilder builder, String q) {
         return builder.or(
-               /* memberNamePredicate(root, builder, q),
+                memberNamePredicate(root, builder, q),
                 memberClassPredicate(root, builder, q),
-                memberCodePredicate(root, builder, q),*/
+                memberCodePredicate(root, builder, q),
                 serverCodePredicate(root, builder, q)
 
         );
--- src/centralserver/admin-service/src/main/java/org/niis/xroad/centralserver/restapi/service/SecurityServerService.java
+++ src/centralserver/admin-service/src/main/java/org/niis/xroad/centralserver/restapi/service/SecurityServerService.java
@@ -1,3 +1,29 @@
+/**
+ * The MIT License
+ *
+ * Copyright (c) 2019- Nordic Institute for Interoperability Solutions (NIIS)
+ * Copyright (c) 2018 Estonian Information System Authority (RIA),
+ * Nordic Institute for Interoperability Solutions (NIIS), Population Register Centre (VRK)
+ * Copyright (c) 2015-2017 Estonian Information System Authority (RIA), Population Register Centre (VRK)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.niis.xroad.centralserver.restapi.service;
 
 import lombok.RequiredArgsConstructor;
@@ -12,6 +38,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,12 +51,9 @@ public class SecurityServerService {
 
     private final SecurityServerConverter serverConverter = new SecurityServerConverter();
 
-    public FoundSecurityServersWithTotalsDto findSecurityServers(String q, Pageable pageable ) {
+    public FoundSecurityServersWithTotalsDto findSecurityServers(String q, Pageable pageable) {
 
-
-
-
-        Page<SecurityServer> foundServers = securityServerRepository.findAllBy(
+        Page<SecurityServer> foundServers = securityServerRepository.findAll(
                 SecurityServerRepository.multifieldSearch(q), pageable);
 
         PagingMetadata pagingMetadata = new PagingMetadata().totalItems((int) foundServers.getTotalElements());
