e3e6ed29b53f7131aa97e45b60d3c41d4c0ba76e
Teemu Kurki <teemu.kurki@gofore.com>
2022-03-18 15:04:15 +0200

    XRDDEV-1914 checkstyle formatting of ManagementRequestService.java

--- src/centralserver/admin-service/src/main/java/org/niis/xroad/centralserver/restapi/service/managementrequest/ManagementRequestService.java
+++ src/centralserver/admin-service/src/main/java/org/niis/xroad/centralserver/restapi/service/managementrequest/ManagementRequestService.java
@@ -1,21 +1,21 @@
 /**
  * The MIT License
- *
+ * <p>
  * Copyright (c) 2019- Nordic Institute for Interoperability Solutions (NIIS)
  * Copyright (c) 2018 Estonian Information System Authority (RIA),
  * Nordic Institute for Interoperability Solutions (NIIS), Population Register Centre (VRK)
  * Copyright (c) 2015-2017 Estonian Information System Authority (RIA), Population Register Centre (VRK)
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -65,6 +65,7 @@ public class ManagementRequestService {
 
     /**
      * Get a management request
+     *
      * @param id request id
      */
     public ManagementRequestDto getRequest(int id) {
@@ -76,7 +77,8 @@ public class ManagementRequestService {
      * Find management requests matching criteria.
      */
     public Page<ManagementRequestInfoDto> findRequests(Origin origin, ManagementRequestType type,
-            ManagementRequestStatus status, SecurityServerId server, Pageable page) {
+                                                       ManagementRequestStatus status, SecurityServerId server,
+                                                       Pageable page) {
         var spec = RequestRepository.findSpec(origin, type, status, server);
         var result = requests.findAll(spec, page);
         return result.map(ManagementRequests::asInfoDto);
@@ -91,6 +93,7 @@ public class ManagementRequestService {
 
     /**
      * Approve pending management request
+     *
      * @param requestId request id to approve
      */
     public ManagementRequestInfoDto approve(int requestId) {
@@ -100,6 +103,7 @@ public class ManagementRequestService {
 
     /**
      * Revoke (decline) pending management request
+     *
      * @param requestId request id to revoke
      */
     public void revoke(Integer requestId) {
