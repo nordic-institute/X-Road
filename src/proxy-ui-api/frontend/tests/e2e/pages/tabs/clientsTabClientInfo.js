8363276c45369d1de58bee07ecd39830c000533c
Turkka Holmqvist <turkka.holmqvist@q-factory.fi>
2022-04-12 14:56:46 +0300

    XRDDEV-1947 fixing locator to be more specific

--- src/proxy-ui-api/frontend/tests/e2e/pages/tabs/clientsTabClientInfo.js
+++ src/proxy-ui-api/frontend/tests/e2e/pages/tabs/clientsTabClientInfo.js
@@ -160,7 +160,7 @@ var clientServicesCommands = {
     // Verifies that error message element is shown, if argument is given, function polls that text from page.
     this.api.waitForElementVisible('//div[contains(@class, "v-messages__message")]');
     if (expectedMessage) {
-      this.api.waitForElementVisible(`//*[text()[contains(.,"${expectedMessage}")]]`)
+      this.api.waitForElementVisible(`//div[contains(@class, "v-messages__message") and contains(text(), "${expectedMessage}")]`)
     }
     return this;
   },
