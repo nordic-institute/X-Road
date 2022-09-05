de8853ef993de17a777f979c608db92e67cc2851
andresrosenthal <andres.rosenthal@nortal.com>
2022-09-05 09:47:16 +0300

    XRDDEV-1935 Checkstyle

--- src/e2e-tests/src/intTest/java/org/niis/xroad/test/ui/glue/constants/Constants.java
+++ src/e2e-tests/src/intTest/java/org/niis/xroad/test/ui/glue/constants/Constants.java
@@ -4,8 +4,12 @@ import org.openqa.selenium.By;
 
 import static org.openqa.selenium.By.xpath;
 
-public class Constants {
+public final class Constants {
+
     public static final By BTN_DIALOG_SAVE = xpath("//button[@data-test=\"dialog-save-button\"]");
     public static final By SNACKBAR_SUCCESS = xpath("//div[@data-test=\"success-snackbar\"]");
     public static final By BTN_CLOSE_SNACKBAR = By.xpath("//button[@data-test=\"close-snackbar\"]");
+
+    private Constants() {
+    }
 }
