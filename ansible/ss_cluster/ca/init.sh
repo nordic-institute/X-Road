0509f427baca3a0d954f211132773b706f01f7a7
Ričardas Bučiūnas <ricardas.buciunas@nortal.com>
2022-09-20 14:57:43 +0300

    CLustered docker env poc

--- ansible/ss_cluster/ca/init.sh
+++ ansible/ss_cluster/ca/init.sh
@@ -3,7 +3,7 @@ umask 0077
 SUBJ=${1:-/O=cluster/CN=cluster-ca}
 echo $SUBJ
 if [ ! -f ca.key ]; then
-    openssl req -new -x509 -days 7305 -sha256 -out ca.crt -keyout ca.key -subj "$SUBJ"
+    openssl req -new -x509 -days 7305 -sha512 -out ca.crt -keyout ca.key -subj "$SUBJ"
     chmod 400 ca.key ca.crt
 else
     echo "ca.key already exists"
