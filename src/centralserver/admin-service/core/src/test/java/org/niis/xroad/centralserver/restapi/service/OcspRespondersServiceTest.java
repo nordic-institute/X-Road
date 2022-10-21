6b3df2276b606b593fab72e387e6ad17992e2724
Vytautas Paliliunas <vytautas.paliliunas@gmail.com>
2022-10-21 12:17:44 +0300

    XRDDEV-2144 API to get the certificate of an OCSP responder of a certification service

---
+++ src/centralserver/admin-service/core/src/test/java/org/niis/xroad/centralserver/restapi/service/OcspRespondersServiceTest.java
@@ -0,0 +1,102 @@
+/**
+ * The MIT License
+ * Copyright (c) 2019- Nordic Institute for Interoperability Solutions (NIIS)
+ * Copyright (c) 2018 Estonian Information System Authority (RIA),
+ * Nordic Institute for Interoperability Solutions (NIIS), Population Register Centre (VRK)
+ * Copyright (c) 2015-2017 Estonian Information System Authority (RIA), Population Register Centre (VRK)
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.niis.xroad.centralserver.restapi.service;
+
+import ee.ria.xroad.common.TestCertUtil;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.niis.xroad.centralserver.restapi.dto.CertificateDetails;
+import org.niis.xroad.centralserver.restapi.dto.converter.CaInfoConverter;
+import org.niis.xroad.centralserver.restapi.dto.converter.KeyUsageConverter;
+import org.niis.xroad.centralserver.restapi.entity.ApprovedCa;
+import org.niis.xroad.centralserver.restapi.entity.CaInfo;
+import org.niis.xroad.centralserver.restapi.entity.OcspInfo;
+import org.niis.xroad.centralserver.restapi.repository.OcspInfoJpaRepository;
+
+import java.time.Instant;
+import java.util.Optional;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+import static org.niis.xroad.centralserver.restapi.dto.KeyUsageEnum.DIGITAL_SIGNATURE;
+
+@ExtendWith(MockitoExtension.class)
+class OcspRespondersServiceTest {
+    private static final Integer ID = 123;
+    private static final Instant VALID_FROM = Instant.now().minus(1, DAYS);
+    private static final Instant VALID_TO = Instant.now().plus(1, DAYS);
+    private static final String CA_NAME = "X-Road Test CA";
+    private static final String CERT_PROFILE = "ee.ria.xroad.common.certificateprofile.impl.FiVRKCertificateProfileInfoProvider";
+
+    @Mock
+    private OcspInfoJpaRepository ocspInfoRepository;
+    @Spy
+    private CaInfoConverter caInfoConverter = new CaInfoConverter(new KeyUsageConverter());
+
+    @InjectMocks
+    private OcspRespondersService service;
+
+
+    @Test
+    void getCertificateDetails() {
+        when(ocspInfoRepository.findById(ID)).thenReturn(Optional.of(ocspResponder()));
+
+        final CertificateDetails certificateDetails = service.getOcspResponderCertificateDetails(ID);
+
+        assertNotNull(certificateDetails);
+        assertThat(certificateDetails.getKeyUsages()).contains(DIGITAL_SIGNATURE);
+        assertEquals("Subject", certificateDetails.getSubjectCommonName());
+        assertEquals("CN=Subject", certificateDetails.getSubjectDistinguishedName());
+        assertEquals("Cyber", certificateDetails.getIssuerCommonName());
+        assertEquals("1", certificateDetails.getSerial());
+        assertEquals("SHA256withRSA", certificateDetails.getSignatureAlgorithm());
+        assertEquals("EMAILADDRESS=aaa@bbb.ccc, CN=Cyber, OU=ITO, O=Cybernetica, C=EE",
+                certificateDetails.getIssuerDistinguishedName());
+    }
+
+    @SneakyThrows
+    private OcspInfo ocspResponder() {
+        CaInfo caInfo = new CaInfo();
+        caInfo.setValidFrom(VALID_FROM);
+        caInfo.setValidTo(VALID_TO);
+        caInfo.setCert(TestCertUtil.generateAuthCert());
+        ApprovedCa ca = new ApprovedCa();
+        ca.setName(CA_NAME);
+        ca.setAuthenticationOnly(true);
+        ca.setCertProfileInfo(CERT_PROFILE);
+        ca.setCaInfo(caInfo);
+        return new OcspInfo(caInfo, "https://flakyocsp:666", new byte[0]);
+    }
+
+}
