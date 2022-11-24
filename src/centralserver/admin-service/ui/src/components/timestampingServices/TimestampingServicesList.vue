3fe8d66a4528cf4d049abc5cb6dc8a3efa09a0e8
Justas Samuolis <justas.samuolis@nortal.com>
2022-11-24 15:45:38 +0200

    XRDDEV-2179 Showing timestamping service certificate

--- src/centralserver/admin-service/ui/src/components/timestampingServices/TimestampingServicesList.vue
+++ src/centralserver/admin-service/ui/src/components/timestampingServices/TimestampingServicesList.vue
@@ -1,29 +1,29 @@
 <!--
-  - The MIT License
-  -
-  - Copyright (c) 2019- Nordic Institute for Interoperability Solutions (NIIS)
-  - Copyright (c) 2018 Estonian Information System Authority (RIA),
-  - Nordic Institute for Interoperability Solutions (NIIS), Population Register Centre (VRK)
-  - Copyright (c) 2015-2017 Estonian Information System Authority (RIA), Population Register Centre (VRK)
-  - <p>
-  - Permission is hereby granted, free of charge, to any person obtaining a copy
-  - of this software and associated documentation files (the "Software"), to deal
-  - in the Software without restriction, including without limitation the rights
-  - to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  - copies of the Software, and to permit persons to whom the Software is
-  - furnished to do so, subject to the following conditions:
-  - <p>
-  - The above copyright notice and this permission notice shall be included in
-  - all copies or substantial portions of the Software.
-  - <p>
-  - THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  - IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  - FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  - AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  - LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  - OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  - THE SOFTWARE.
-  -->
+   The MIT License
+
+   Copyright (c) 2019- Nordic Institute for Interoperability Solutions (NIIS)
+   Copyright (c) 2018 Estonian Information System Authority (RIA),
+   Nordic Institute for Interoperability Solutions (NIIS), Population Register Centre (VRK)
+   Copyright (c) 2015-2017 Estonian Information System Authority (RIA), Population Register Centre (VRK)
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+ -->
 <template>
   <div data-test="timestamping-services">
     <!-- Title and button -->
@@ -75,7 +75,11 @@
       </template>
       <template #[`item.button`]="{ item }">
         <div class="cs-table-actions-wrap">
-          <xrd-button text :outlined="false" @click="() => {}">
+          <xrd-button
+            text
+            :outlined="false"
+            @click="navigateToCertificateDetails(item)"
+          >
             {{ $t('trustServices.viewCertificate') }}
           </xrd-button>
           <xrd-button
@@ -124,7 +128,7 @@ import { notificationsStore } from '@/store/modules/notifications';
 import { userStore } from '@/store/modules/user';
 import { TimestampingService } from '@/openapi-types';
 import { timestampingServicesStore } from '@/store/modules/trust-services';
-import { Permissions } from '@/global';
+import { Permissions, RouteName } from '@/global';
 
 export default Vue.extend({
   name: 'TimestampingServicesList',
@@ -226,6 +230,14 @@ export default Vue.extend({
     },
     toMinutes(seconds: number): string {
       return '' + parseFloat((seconds / 60).toFixed(2));
+    },
+    navigateToCertificateDetails(item: TimestampingService) {
+      this.$router.push({
+        name: RouteName.TimestampingServiceCertificateDetails,
+        params: {
+          timestampingServiceId: String(item.id),
+        },
+      });
     },
   },
 });
