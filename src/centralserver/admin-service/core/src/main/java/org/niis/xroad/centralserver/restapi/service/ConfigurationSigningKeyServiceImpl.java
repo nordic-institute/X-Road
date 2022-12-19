ca3de8bc945df2709b970d59c537a642ebd5a184
Vytautas Paliliunas <vytautas.paliliunas@gmail.com>
2022-12-19 12:57:47 +0200

    XRDDEV-2196 API to add signing keys

---
+++ src/centralserver/admin-service/core/src/main/java/org/niis/xroad/centralserver/restapi/service/ConfigurationSigningKeyServiceImpl.java
@@ -0,0 +1,61 @@
+/**
+ * The MIT License
+ * <p>
+ * Copyright (c) 2019- Nordic Institute for Interoperability Solutions (NIIS)
+ * Copyright (c) 2018 Estonian Information System Authority (RIA),
+ * Nordic Institute for Interoperability Solutions (NIIS), Population Register Centre (VRK)
+ * Copyright (c) 2015-2017 Estonian Information System Authority (RIA), Population Register Centre (VRK)
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.niis.xroad.centralserver.restapi.service;
+
+import lombok.RequiredArgsConstructor;
+import org.niis.xroad.centralserver.restapi.service.exception.NotFoundException;
+import org.niis.xroad.cs.admin.api.dto.ConfigurationSigningKey;
+import org.niis.xroad.cs.admin.api.dto.ConfigurationSigningKeyAddRequest;
+import org.niis.xroad.cs.admin.api.service.ConfigurationSigningKeyService;
+import org.niis.xroad.cs.admin.core.entity.ConfigurationSourceEntity;
+import org.niis.xroad.cs.admin.core.repository.ConfigurationSourceRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+import static org.niis.xroad.centralserver.restapi.service.exception.ErrorMessage.CONFIGURATION_NOT_FOUND;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ConfigurationSigningKeyServiceImpl implements ConfigurationSigningKeyService {
+
+    private final ConfigurationSourceRepository configurationSourceRepository;
+
+    @Override
+    public ConfigurationSigningKey addKey(ConfigurationSigningKeyAddRequest configurationSigningKeyAddRequest) {
+        ConfigurationSourceEntity configurationSourceEntity =
+                findConfigurationSourceBySourceType(configurationSigningKeyAddRequest.getSourceType());
+        
+        return null;
+    }
+
+    private ConfigurationSourceEntity findConfigurationSourceBySourceType(String sourceType) {
+        return configurationSourceRepository.findBySourceType(sourceType)
+                .orElseThrow(() -> new NotFoundException(CONFIGURATION_NOT_FOUND));
+    }
+}
