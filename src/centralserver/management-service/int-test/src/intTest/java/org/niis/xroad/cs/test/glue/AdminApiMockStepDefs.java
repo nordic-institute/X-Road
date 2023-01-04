fdbe9ec166b588e556d6026c71689d57c4245f7c
Ričardas Bučiūnas <ricardas.buciunas@nortal.com>
2023-01-04 19:28:20 +0200

    XRDDEV-2189 added expected response to test automation report.

--- src/centralserver/management-service/int-test/src/intTest/java/org/niis/xroad/cs/test/glue/AdminApiMockStepDefs.java
+++ src/centralserver/management-service/int-test/src/intTest/java/org/niis/xroad/cs/test/glue/AdminApiMockStepDefs.java
@@ -1,21 +1,21 @@
 /**
  * The MIT License
- *
+ * <p>
  * Copyright (c) 2019- Nordic Institute for Interoperability Solutions (NIIS)
  * Copyright (c) 2018 Estonian Information System Authority (RIA),
  * Nordic Institute for Interoperability Solutions (NIIS), Population Register Centre (VRK)
  * Copyright (c) 2015-2017 Estonian Information System Authority (RIA), Population Register Centre (VRK)
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -38,6 +38,7 @@ import org.niis.xroad.centralserver.openapi.model.ManagementRequestDto;
 import org.niis.xroad.centralserver.openapi.model.ManagementRequestTypeDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 import static org.eclipse.jetty.http.HttpMethod.POST;
 import static org.mockserver.model.HttpRequest.request;
@@ -74,6 +75,9 @@ public class AdminApiMockStepDefs extends BaseStepDefs {
 
     @Step("Admin api has received following request")
     public void adminApiValidateMock(String expectedRequest) throws JsonProcessingException {
+        cucumberScenarioProvider.getCucumberScenario()
+                .attach(expectedRequest, MediaType.APPLICATION_JSON_VALUE, "Expected json");
+
         HttpRequest[] httpRequests = mockServerClient.retrieveRecordedRequests(getManagementRequestsDefinition());
 
         var actualBody = httpRequests[0].getBody().getValue();
