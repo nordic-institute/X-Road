ac0d303e78e89e138b45bc88f70c98b9b3c90183
Ovidijus Narkevicius <ovidijus.narkevicius@nortal.com>
2023-01-10 14:40:44 +0200

    XRDDEV-2214 XRDDEV-2215 XRDDEV-2213 checkstyle

--- src/centralserver/admin-service/core/src/main/java/org/niis/xroad/cs/admin/core/entity/ConfigurationSigningKeyEntity.java
+++ src/centralserver/admin-service/core/src/main/java/org/niis/xroad/cs/admin/core/entity/ConfigurationSigningKeyEntity.java
@@ -30,7 +30,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
