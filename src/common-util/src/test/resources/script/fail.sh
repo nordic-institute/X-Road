e4cc1b1b83005e1f41d6384674998a4e1885d470
Ričardas Bučiūnas <ricardas.buciunas@nortal.com>
2023-01-27 11:30:39 +0200

    chore: extract ExternalProcessRunner code to commons

---
+++ src/common-util/src/test/resources/script/fail.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "FAIL" >&2
+exit 1
--- src/proxy-ui-api/src/main/java/org/niis/xroad/securityserver/restapi/service/ConfigurationVerifier.java
+++ src/proxy-ui-api/src/main/java/org/niis/xroad/securityserver/restapi/service/ConfigurationVerifier.java
@@ -25,6 +25,10 @@
  */
 package org.niis.xroad.securityserver.restapi.service;
 
+import ee.ria.xroad.common.util.process.ExternalProcessRunner;
+import ee.ria.xroad.common.util.process.ProcessFailedException;
+import ee.ria.xroad.common.util.process.ProcessNotExecutableException;
+
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.niis.xroad.restapi.exceptions.ErrorDeviation;
