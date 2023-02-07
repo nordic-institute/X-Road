2852c9b2a6fd11aa154d865b815404dc6755667d
Justas Samuolis <justas.samuolis@nortal.com>
2023-02-07 11:38:10 +0200

    feat: XRDDEV-2233 removing validation-program property

--- src/common-ui/src/test/java/ee/ria/xroad/commonui/OptionalPartsConfBehavior.java
+++ src/common-ui/src/test/java/ee/ria/xroad/commonui/OptionalPartsConfBehavior.java
@@ -4,17 +4,17 @@
  * Copyright (c) 2018 Estonian Information System Authority (RIA),
  * Nordic Institute for Interoperability Solutions (NIIS), Population Register Centre (VRK)
  * Copyright (c) 2015-2017 Estonian Information System Authority (RIA), Population Register Centre (VRK)
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -54,26 +54,6 @@ public class OptionalPartsConfBehavior {
 
     @Rule
     public ExpectedCodedException thrown = ExpectedCodedException.none();
-
-    /**
-     * Test to ensure validation program can be found from test configuration part.
-     * @throws IOException in case optional parts directory cannot be read
-     */
-    @Test
-    public void shouldGetValidationProgram() throws IOException {
-        // Given
-        String confDir = "src/test/resources/configuration-parts";
-        OptionalPartsConf conf = new OptionalPartsConf(confDir);
-        String partFile = "test-configuration-part.xml";
-
-        // When
-        String actualValidationProgram = conf.getValidationProgram(partFile);
-
-        // Then
-        String expectedValidationProgram = "/usr/share/xroad/scripts/validate-test-configuration-part.sh";
-
-        assertEquals(expectedValidationProgram, actualValidationProgram);
-    }
 
     /**
      * Test to ensure test configuration part content identifier can be read.
