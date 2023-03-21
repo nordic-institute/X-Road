83d8b2c4b7b306ceca5b2fb4cc4c7b65f8a1df8b
Justas Samuolis <justas.samuolis@nortal.com>
2023-03-21 13:58:43 +0200

    chore: ConfigurationVerifier moved to commons-rest-api
    Refs: XRDDEV-2295

--- src/common-rest-api/src/main/java/org/niis/xroad/restapi/service/ConfigurationVerifier.java
+++ src/common-rest-api/src/main/java/org/niis/xroad/restapi/service/ConfigurationVerifier.java
@@ -1,20 +1,21 @@
-/**
+/*
  * The MIT License
+ * <p>
  * Copyright (c) 2019- Nordic Institute for Interoperability Solutions (NIIS)
  * Copyright (c) 2018 Estonian Information System Authority (RIA),
  * Nordic Institute for Interoperability Solutions (NIIS), Population Register Centre (VRK)
  * Copyright (c) 2015-2017 Estonian Information System Authority (RIA), Population Register Centre (VRK)
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,17 +24,14 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.niis.xroad.securityserver.restapi.service;
+package org.niis.xroad.restapi.service;
 
 import ee.ria.xroad.common.util.process.ExternalProcessRunner;
 import ee.ria.xroad.common.util.process.ProcessFailedException;
 import ee.ria.xroad.common.util.process.ProcessNotExecutableException;
 
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.niis.xroad.restapi.exceptions.ErrorDeviation;
-import org.niis.xroad.restapi.service.ServiceException;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import static org.niis.xroad.restapi.exceptions.DeviationCodes.ERROR_ANCHOR_NOT_FOR_EXTERNAL_SOURCE;
@@ -56,26 +54,24 @@ public class ConfigurationVerifier {
     public static final int EXIT_STATUS_INVALID_SIGNATURE = 124;
     public static final int EXIT_STATUS_OTHER = 125;
 
-    @Setter
-    @Value("${script.internal-configuration-verifier.path}")
-    private String internalConfVerificationScriptPath;
-
     private final ExternalProcessRunner externalProcessRunner;
 
     /**
-     * Verify internal configuration anchor.
-     * @param anchorPath path to the configuration anchor to be verified
+     * Verify configuration file.
+     *
+     * @param verificationScriptPath verification script path
+     * @param fileToValidatePath     path to the configuration file
      * @throws ProcessNotExecutableException
      * @throws ProcessFailedException
-     * @throws InterruptedException if the thread running the verifier is interrupted. <b>The interrupted thread has
-     * already been handled with so you can choose to ignore this exception if you so please.</b>
+     * @throws InterruptedException               if the thread running the verifier is interrupted. <b>The interrupted thread has
+     *                                            already been handled with so you can choose to ignore this exception if you so please.</b>
      * @throws ConfigurationVerificationException when a known exception happens during verification. An error code
-     * is attached to this exception according to the correct exit code
+     *                                            is attached to this exception according to the correct exit code
      */
-    public void verifyInternalConfiguration(String anchorPath) throws ProcessNotExecutableException,
+    public void verifyConfiguration(String verificationScriptPath, String fileToValidatePath) throws ProcessNotExecutableException,
             ProcessFailedException, InterruptedException, ConfigurationVerificationException {
         ExternalProcessRunner.ProcessResult processResult =
-                externalProcessRunner.execute(internalConfVerificationScriptPath, anchorPath);
+                externalProcessRunner.execute(verificationScriptPath, fileToValidatePath);
         int exitCode = processResult.getExitCode();
         switch (exitCode) {
             case 0:
