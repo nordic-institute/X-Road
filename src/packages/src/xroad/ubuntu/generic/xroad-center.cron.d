efd3c6c82d3a48eec487b8234328f3cc6fc3ec2e
Eneli Reimets <eneli.reimets@nortal.com>
2023-05-24 08:35:19 +0300

    fix: minor ui fixes and file type fix in cs old backups delete cron
    Refs: XRDDEV-2397

--- src/packages/src/xroad/ubuntu/generic/xroad-center.cron.d
+++ src/packages/src/xroad/ubuntu/generic/xroad-center.cron.d
@@ -7,4 +7,4 @@
 15 3 * * * xroad /usr/share/xroad/scripts/autobackup_xroad_center_configuration.sh
 
 # backup retention policy, delete backups older that 30 days
-10 * * * * xroad find /var/lib/xroad/backup -type f -name "cs-automatic-backup*.tar" -mtime 30 -delete
+10 * * * * xroad find /var/lib/xroad/backup -type f -name "cs-automatic-backup*.gpg" -mtime 30 -delete
