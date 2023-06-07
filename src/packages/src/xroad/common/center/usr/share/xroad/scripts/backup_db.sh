49a5cfef45c395c8a4f6aa7bd4f0e92d7c345f6f
Ovidijus Narkevicius <ovidijus.narkevicius@nortal.com>
2023-06-07 09:09:05 +0300

    feat(cs): (WIP) support Spring datasource config in backup_script
    Closes XRDDEV-2422

--- src/packages/src/xroad/common/center/usr/share/xroad/scripts/backup_db.sh
+++ src/packages/src/xroad/common/center/usr/share/xroad/scripts/backup_db.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+db_properties=/etc/xroad/db.properties
+root_properties=/etc/xroad.properties
+
 TMP=$(mktemp)
 DUMP_FILE=$1
 HOST=$(crudini --get /etc/xroad/db.properties '' host)
@@ -16,7 +19,17 @@ if [[ -n "${ADMIN_USER}" && -n "${ADMIN_PASSWORD}" ]]; then
   PASSWORD=${ADMIN_PASSWORD}
 fi
 
-echo "$PASSWORD"
+db_host="127.0.0.1:5432"
+db_conn_user="$(get_prop ${db_properties} 'serverconf.hibernate.connection.username' 'serverconf')"
+db_user="${db_conn_user%%@*}"
+db_schema=$(get_prop ${db_properties} 'serverconf.hibernate.hikari.dataSource.currentSchema' "${db_user},public")
+db_schema=${db_schema%%,*}
+db_password="$(get_prop ${db_properties} 'serverconf.hibernate.connection.password' "serverconf")"
+db_url="$(get_prop ${db_properties} 'serverconf.hibernate.connection.url' "jdbc:postgresql://$db_host/serverconf")"
+db_database=serverconf
+pg_options="-c client-min-messages=warning -c search_path=$db_schema,public"
+db_admin_user=$(get_prop ${root_properties} 'serverconf.database.admin_user' "$db_conn_user")
+db_admin_password=$(get_prop ${root_properties} 'serverconf.database.admin_password' "$db_password")
 
 PGOPTIONS='-c client-min-messages=warning' PGPASSWORD="$PASSWORD" \
     pg_dump -n "${SCHEMA:-$USER}" -x -O -F p -h "${HOST:-127.0.0.1}" -p "${PORT:-5432}" -U "${USER:-centerui}" -f "${DUMP_FILE}" \
