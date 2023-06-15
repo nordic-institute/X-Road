42554fcf9cd0f185a5fe40a85dfdf28353930b7e
Ovidijus Narkevicius <ovidijus.narkevicius@nortal.com>
2023-06-15 16:47:04 +0300

    refactor(deps): WIP create new module for Vue3
    create new empty module with Vue3/Vuetify3/Storybook7 for porting over shared-ui
    
    Closes XRDDEV-2263

---
+++ src/xrd-shared-ui/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript', 'plugin:storybook/recommended'],
+  rules: {
+    'vue/multi-word-component-names': 'off'
+  }
+};
