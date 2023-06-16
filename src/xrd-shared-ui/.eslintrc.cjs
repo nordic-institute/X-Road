b0ba0ac11f2c2216aaabfa3a26a3c4d4b0fb4dae
Ovidijus Narkevicius <ovidijus.narkevicius@nortal.com>
2023-06-16 12:06:49 +0300

    refactor(deps): WIP working POV module with storybook
    Closes XRDDEV-2263

--- src/xrd-shared-ui/.eslintrc.cjs
+++ src/xrd-shared-ui/.eslintrc.cjs
@@ -1,14 +1,9 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
+require('@rushstack/eslint-patch/modern-module-resolution');
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript'
-  ],
+  'extends': ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript', 'plugin:storybook/recommended'],
   parserOptions: {
     ecmaVersion: 'latest'
   }
-}
+};
