89a7bb19bd939264457877525f0d506a71d881b0
Ovidijus Narkevicius <ovidijus.narkevicius@nortal.com>
2023-06-19 09:18:29 +0300

    Revert "refactor(deps): WIP working POV module with storybook"
    This reverts commit 49fee14a00063f5a2f534e97e97b3d4ff9a065c6.

--- src/xrd-shared-ui/.eslintrc.cjs
+++ src/xrd-shared-ui/.eslintrc.cjs
@@ -1,9 +1,14 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution');
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 module.exports = {
   root: true,
-  'extends': ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript', 'plugin:storybook/recommended'],
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript'
+  ],
   parserOptions: {
     ecmaVersion: 'latest'
   }
-};
+}
