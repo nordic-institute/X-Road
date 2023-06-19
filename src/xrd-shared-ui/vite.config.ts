1aa85c724fe5c8b5c6069859b9581460a29001ca
Ovidijus Narkevicius <ovidijus.narkevicius@nortal.com>
2023-06-19 14:39:47 +0300

    refactor(deps): WIP moving in component from OG shared-ui
    Closes XRDDEV-2263

--- src/xrd-shared-ui/vite.config.ts
+++ src/xrd-shared-ui/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import vuetify from 'vite-plugin-vuetify'
 import dts from "vite-plugin-dts";
 import { resolve } from "node:path";
 
@@ -10,6 +11,7 @@ export default defineConfig({
     vue(),
     dts(),
     vueJsx(),
+    vuetify({ autoImport: true })
   ],
   build: {
     lib: {
@@ -18,12 +20,12 @@ export default defineConfig({
       fileName: "xrd-shared-ui",
     },
     rollupOptions: {
-      external: ["vue"],
+      external: ["vue", "vuetify"],
       output: {
         globals: {
           vue: "Vue",
         },
       },
-    },
+    }
   },
 })
