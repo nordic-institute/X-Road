b37f9503aa0a992d336553fd4666915b416d5876
Ovidijus Narkevicius <ovidijus.narkevicius@nortal.com>
2023-06-19 14:39:47 +0300

    refactor(deps): WIP moving in component from OG shared-ui
    Closes XRDDEV-2263

---
+++ src/xrd-shared-ui/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-main>
+      <xrd-button text="road-x">Nested</xrd-button>
+      <br>
+      <xrd-close-button @click="console.log('close it')" />
+      <br>
+      <v-btn text="road-y" @click="console.log('click it')"/>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+
+import XrdButton from "./components/XrdButton.vue";
+import XrdCloseButton from "./components/XrdCloseButton.vue";
+</script>
--- src/xrd-shared-ui/src/main.ts
+++ src/xrd-shared-ui/src/main.ts
@@ -1,1 +1,18 @@
+/**
+ * main.ts
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
 
+// Components
+import App from './App.vue'
+
+// Composables
+import { createApp } from 'vue'
+
+// Plugins
+import vuetify from './plugins/vuetify'
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
