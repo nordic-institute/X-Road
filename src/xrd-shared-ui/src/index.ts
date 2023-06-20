299aebc6d275e5b6ec711d4acebe0fb0a40e7912
Ovidijus Narkevicius <ovidijus.narkevicius@nortal.com>
2023-06-20 15:23:58 +0300

    refactor(deps): WIP moving in component from OG shared-ui
    empty placeholders and i18n support
    
    Closes XRDDEV-2263

--- src/xrd-shared-ui/src/index.ts
+++ src/xrd-shared-ui/src/index.ts
@@ -1,21 +1,52 @@
 import './assets/icons.css';
 import type { App } from 'vue';
-import vuetify from "./plugins/vuetify";
 
+//plugins
+import vuetify from "./plugins/vuetify";
+import { createI18n } from "vue-i18n";
+import messages from '@intlify/unplugin-vue-i18n/messages'
+
+//components
 import XrdComponent from "./components/XrdComponent.vue";
 import XrdButton from "./components/XrdButton.vue";
 import XrdCloseButton from "./components/XrdCloseButton.vue";
 import XrdAlert from "./components/XrdAlert.vue";
+import XrdEmptyPlaceholder from "./components/XrdEmptyPlaceholder.vue";
+import XrdEmptyPlaceholderRow from "./components/XrdEmptyPlaceholderRow.vue";
+import merge from "deepmerge";
 
 
-function install(app: App) {
-  app.use(vuetify)
-    .component('XrdComponent', XrdComponent)
-    .component('XrdButton', XrdButton)
-    .component('XrdCloseButton', XrdCloseButton)
-    .component('XrdAlert', XrdAlert)
-  ;
+function createSharedUi(i18nMessages = {}) {
+  const i18n = createI18n({
+    locale: 'en',
+    fallbackLocale: 'en',
+    silentFallbackWarn: true,
+    allowComposition: true,
+    messages: merge(messages, i18nMessages)
+  })
+
+  return {
+    install(app: App) {
+      console.log('Installing', messages, i18nMessages, )
+      app.use(vuetify)
+        .use(i18n)
+        .component('XrdComponent', XrdComponent)
+        .component('XrdButton', XrdButton)
+        .component('XrdCloseButton', XrdCloseButton)
+        .component('XrdAlert', XrdAlert)
+        .component('XrdEmptyPlaceholder', XrdEmptyPlaceholder)
+        .component('XrdEmptyPlaceholderRow', XrdEmptyPlaceholderRow)
+      ;
+    }
+  }
 }
 
-export default { install };
-export { XrdComponent, XrdButton, XrdCloseButton, XrdAlert };
+export {
+  createSharedUi,
+  XrdComponent,
+  XrdButton,
+  XrdCloseButton,
+  XrdAlert,
+  XrdEmptyPlaceholder,
+  XrdEmptyPlaceholderRow
+};
