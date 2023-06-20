bf31b0243f094e6fd7db7bb9d8ffc2664c79cc5b
Ovidijus Narkevicius <ovidijus.narkevicius@nortal.com>
2023-06-20 15:23:58 +0300

    refactor(deps): WIP moving in component from OG shared-ui
    empty placeholders and i18n support
    
    Closes XRDDEV-2263

--- src/xrd-shared-ui/src/App.vue
+++ src/xrd-shared-ui/src/App.vue
@@ -1,29 +1,74 @@
 <template>
   <v-app>
     <v-main>
-      <br>
-      <xrd-button text="road-x">
-        Nested
+      <hr>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>City</th>
+          </tr>
+        </thead>
+        <tbody>
+          <xrd-empty-placeholder-row
+            colspan="2"
+            no-items-text="No rows"
+            :loading="loading"
+          />
+        </tbody>
+      </table>
+      <hr>
+      <xrd-button
+        text="road-x"
+        @click="loading=!loading"
+      >
+        {{ loading?"Loading":"Loaded" }}
       </xrd-button>
-      <br>
+      <hr>
       <xrd-close-button @click="console.log('close it')" />
-      <br>
+      <hr>
       <xrd-alert
         :show="true"
         message="something went wrong"
       />
-      <br>
-      <v-btn
-        variant="text"
-        @click="console.log('click it')"
+      <hr>
+      <xrd-empty-placeholder
+        no-items-text="No data here"
+        :loading="loading"
+        skeleton-type="table-heading"
+      />
+      <xrd-empty-placeholder
+        no-items-text="No data here"
+        :loading="loading"
+      />
+      <xrd-empty-placeholder
+        no-items-text="No data here"
+        :loading="loading"
+        :filtered="true"
+      />
+      <xrd-empty-placeholder
+        no-items-text="No data here"
+        :loading="loading"
+        no-matches-text="No matches here"
+        :filtered="true"
+      />
+      <xrd-empty-placeholder
+        no-items-text="No data here"
+        :loading="loading"
+        :data="[]"
       />
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
-
 import XrdAlert from "./components/XrdAlert.vue";
 import XrdButton from "./components/XrdButton.vue";
 import XrdCloseButton from "./components/XrdCloseButton.vue";
+import XrdEmptyPlaceholder from "./components/XrdEmptyPlaceholder.vue";
+import XrdEmptyPlaceholderRow from "./components/XrdEmptyPlaceholderRow.vue";
+import { ref } from 'vue'
+
+let loading = ref(false);
+
 </script>
