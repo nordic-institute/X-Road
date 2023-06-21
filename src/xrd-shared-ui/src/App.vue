5f9831d2dfb898ecd5c18e41840e66358d806934
Ovidijus Narkevicius <ovidijus.narkevicius@nortal.com>
2023-06-21 13:42:08 +0300

    refactor(deps): WIP moving in component from OG shared-ui
    form-label & help-icon, make expandable self contained component
    
    Closes XRDDEV-2263

--- src/xrd-shared-ui/src/App.vue
+++ src/xrd-shared-ui/src/App.vue
@@ -1,87 +1,118 @@
 <template>
   <v-app>
     <v-main>
-      <hr>
-      <xrd-file-upload
-        v-slot="{ upload }"
-        accepts="*"
-      >
-        <v-btn @click="upload">
-          Upload
-        </v-btn>
-      </xrd-file-upload>
-      <hr>
-      <xrd-expandable
-        :is-open="opened"
-        @close="opened=false"
-        @open="opened=true"
-      >
+      <xrd-expandable>
         <template #link>
-          Link to somewhere
+          xrd-form-label & xrd-help-icon
         </template>
         <template #action>
           Call for Action
         </template>
         <template #content>
-          some content text
+          <xrd-form-label label-text="Label text" help-text="helpful tip"/>
+          <xrd-help-icon text="help me"/>
         </template>
       </xrd-expandable>
-      <hr>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>City</th>
-          </tr>
-        </thead>
-        <tbody>
-          <xrd-empty-placeholder-row
-            colspan="2"
-            no-items-text="No rows"
+      <xrd-expandable>
+        <template #link>
+          xrd-file-upload
+        </template>
+        <template #content>
+          <xrd-file-upload
+            v-slot="{ upload }"
+            accepts="*"
+          >
+            <v-btn @click="upload">
+              Upload
+            </v-btn>
+          </xrd-file-upload>
+        </template>
+      </xrd-expandable>
+      <xrd-expandable>
+        <template #link>
+          xrd-button & xrd-close-button
+        </template>
+        <template #content>
+          <xrd-button
+            text="road-x"
+            @click="loading=!loading"
+          >
+            {{ loading ? "Loading" : "Loaded" }}
+          </xrd-button>
+          <xrd-close-button @click="console.log('close it')"/>
+        </template>
+      </xrd-expandable>
+      <xrd-expandable>
+        <template #link>
+          xrd-alert
+        </template>
+        <template #content>
+          <xrd-alert
+            :show="true"
+            message="something went wrong"
+          />
+        </template>
+      </xrd-expandable>
+      <xrd-expandable>
+        <template #link>
+          xrd-empty-placeholder
+        </template>
+        <template #action>
+          <xrd-button @click="loading=!loading" :text="loading?'Loading':'Loaded'"/>
+        </template>
+        <template #content>
+          <xrd-empty-placeholder
+            no-items-text="No data here"
             :loading="loading"
           />
-        </tbody>
-      </table>
-      <hr>
-      <xrd-button
-        text="road-x"
-        @click="loading=!loading"
-      >
-        {{ loading?"Loading":"Loaded" }}
-      </xrd-button>
-      <hr>
-      <xrd-close-button @click="console.log('close it')" />
-      <hr>
-      <xrd-alert
-        :show="true"
-        message="something went wrong"
-      />
-      <hr>
-      <xrd-empty-placeholder
-        no-items-text="No data here"
-        :loading="loading"
-        skeleton-type="table-heading"
-      />
-      <xrd-empty-placeholder
-        no-items-text="No data here"
-        :loading="loading"
-      />
-      <xrd-empty-placeholder
-        no-items-text="No data here"
-        :loading="loading"
-        :filtered="true"
-      />
-      <xrd-empty-placeholder
-        no-items-text="No data here"
-        :loading="loading"
-        no-matches-text="No matches here"
-        :filtered="true"
-      />
-      <xrd-empty-placeholder
-        no-items-text="No data here"
-        :loading="loading"
-        :data="[]"
-      />
+          <xrd-empty-placeholder
+            no-items-text="No data here"
+            :loading="loading"
+            :filtered="true"
+          />
+          <xrd-empty-placeholder
+            no-items-text="No data here"
+            :loading="loading"
+            no-matches-text="No matches here"
+            :filtered="true"
+          />
+          <xrd-empty-placeholder
+            no-items-text="No data here"
+            :loading="loading"
+            :data="[]"
+          />
+          <xrd-empty-placeholder
+            no-items-text="No data here"
+            :loading="loading"
+            skeleton-type="table-heading"
+          />
+        </template>
+      </xrd-expandable>
+      <xrd-expandable>
+        <template #link>
+          xrd-empty-placeholder-row
+        </template>
+        <template #action>
+          <xrd-button @click="loading=!loading" :text="loading?'Loading':'Loaded'"/>
+        </template>
+        <template #content>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>City</th>
+              </tr>
+            </thead>
+            <tbody>
+              <xrd-empty-placeholder-row
+                colspan="2"
+                no-items-text="No rows"
+                :loading="loading"
+              />
+            </tbody>
+          </table>
+        </template>
+      </xrd-expandable>
     </v-main>
   </v-app>
 </template>
@@ -95,8 +126,11 @@ import XrdEmptyPlaceholderRow from "./components/XrdEmptyPlaceholderRow.vue";
 import { ref } from 'vue'
 import XrdExpandable from "./components/XrdExpandable.vue";
 import XrdFileUpload from "./components/XrdFileUpload.vue";
+import XrdHelpIcon from "./components/XrdHelpIcon.vue";
+import XrdFormLabel from "./components/XrdFormLabel.vue";
 
 let loading = ref(false);
 let opened = ref(false);
+let isOpen = ref(true);
 
 </script>
