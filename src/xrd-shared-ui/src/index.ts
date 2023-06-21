00379229f29182cf565d3a1843a3c5fca9e781c6
Ovidijus Narkevicius <ovidijus.narkevicius@nortal.com>
2023-06-21 15:44:43 +0300

    refactor(deps): WIP moving in component from OG shared-ui
    xrd-simple-dialog, xrd-confirm-dialog & xrd-help-dialog
    
    Closes XRDDEV-2263

--- src/xrd-shared-ui/src/index.ts
+++ src/xrd-shared-ui/src/index.ts
@@ -22,6 +22,9 @@ import XrdExpandable from "./components/XrdExpandable.vue";
 import XrdFileUpload from "./components/XrdFileUpload.vue";
 import XrdFormLabel from "./components/XrdFormLabel.vue";
 import XrdHelpIcon from "./components/XrdHelpIcon.vue";
+import XrdSimpleDialog from "./components/XrdSimpleDialog.vue";
+import XrdConfirmDialog from "./components/XrdConfirmDialog.vue";
+import XrdHelpDialog from "./components/XrdHelpDialog.vue";
 
 
 function createSharedUi(i18nMessages = {}) {
@@ -47,6 +50,9 @@ function createSharedUi(i18nMessages = {}) {
         .component('XrdFileUpload', XrdFileUpload)
         .component('XrdFormLabel', XrdFormLabel)
         .component('XrdHelpIcon', XrdHelpIcon)
+        .component('XrdSimpleDialog', XrdSimpleDialog)
+        .component('XrdConfirmDialog', XrdConfirmDialog)
+        .component('XrdHelpDialog', XrdHelpDialog)
       ;
     }
   }
@@ -59,5 +65,12 @@ export {
   XrdCloseButton,
   XrdAlert,
   XrdEmptyPlaceholder,
-  XrdEmptyPlaceholderRow
+  XrdEmptyPlaceholderRow,
+  XrdExpandable,
+  XrdFileUpload,
+  XrdFormLabel,
+  XrdHelpIcon,
+  XrdSimpleDialog,
+  XrdConfirmDialog,
+  XrdHelpDialog
 };
