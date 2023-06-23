b876a2f2d2f470d95242505fda22a086da96ee12
Ovidijus Narkevicius <ovidijus.narkevicius@nortal.com>
2023-06-23 14:13:00 +0300

    refactor(deps): WIP moving in component from OG shared-ui
    backu/restore components
    
    Closes XRDDEV-2263

--- src/xrd-shared-ui/src/types.ts
+++ src/xrd-shared-ui/src/types.ts
@@ -2,3 +2,26 @@ export type FileUploadResult = {
   buffer: ArrayBuffer;
   file: File;
 };
+
+export type BackupItem = {
+  filename: string;
+  local_conf_present?: boolean;
+}
+
+export interface BackupHandler {
+  showError(error: unknown): void;
+
+  showWarning(textKey: string, data?: Record<string, unknown>): void;
+
+  showSuccess(textKey: string, data?: Record<string, unknown>): void;
+
+  upload(backupFile: File, ignoreWarnings?: boolean): Promise<BackupItem>;
+
+  create(): Promise<BackupItem>;
+
+  delete(filename: string): Promise<unknown>;
+
+  download(filename: string): Promise<unknown>;
+
+  restore(filename: string): Promise<unknown>;
+}
