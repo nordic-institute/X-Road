dda71dc8eb2aab01b99624aefad30b082aadd663
Raido Kaju <raido.kaju@gmail.com>
2023-08-15 13:47:44 +0300

    feat: add possibility to run system tests under GitHub actions

--- src/central-server/admin-service/ui-system-test/src/intTest/java/org/niis/xroad/cs/test/ui/container/ContainerSetup.java
+++ src/central-server/admin-service/ui-system-test/src/intTest/java/org/niis/xroad/cs/test/ui/container/ContainerSetup.java
@@ -51,13 +51,14 @@ public class ContainerSetup {
 
     @Bean
     public TestContainerConfigurator testContainerConfigurator(
+            @Value("${test-automation.custom.docker-root}") String dockerRoot,
             @Value("${test-automation.custom.package-repo}") String packageRepo,
             @Value("${test-automation.custom.package-repo-key}") String packageRepoKey) {
         return new TestContainerConfigurator() {
             @NotNull
             @Override
             public LazyFuture<String> imageDefinition() {
-                Path csDockerRoot = Paths.get("../../../../Docker/centralserver/");
+                Path csDockerRoot = Paths.get(dockerRoot);
                 Path dockerfilePath = csDockerRoot.resolve("Dockerfile");
 
                 return new ImageFromDockerfile("cs-system-test", true)
