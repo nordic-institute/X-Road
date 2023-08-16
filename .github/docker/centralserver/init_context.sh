bd94d52456c92be0135b5d2826bba17b5aa3d9df
Raido Kaju <raido.kaju@gmail.com>
2023-08-16 10:52:36 +0300

    chore: fix init setup and add some output to verify files

--- .github/docker/centralserver/init_context.sh
+++ .github/docker/centralserver/init_context.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-rm -rf ./build
-mkdir -p ./build
-
 cp -r ansible/roles/xroad-ca/files/etc ./build/
 cp -r ansible/roles/xroad-ca/files/home ./build/
 cp -r Docker/centralserver/files ./build/
