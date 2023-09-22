0b9e36d34000700e446ee631abde49e1b1d19192
Ovidijus Narkevicius <ovidijus.narkevicius@nortal.com>
2023-09-22 12:07:22 +0300

    test(ss): make tests a bit more precise

--- src/security-server/system-test/src/intTest/java/org/niis/xroad/ss/test/addons/glue/BaseStepDefs.java
+++ src/security-server/system-test/src/intTest/java/org/niis/xroad/ss/test/addons/glue/BaseStepDefs.java
@@ -93,7 +93,7 @@ public abstract class BaseStepDefs {
     }
 
     @SneakyThrows
-    private String evalXpath(String body, String xpath) {
+    protected String evalXpath(String body, String xpath) {
         InputStream is = new ByteArrayInputStream(body.getBytes());
 
         XPath xpathEvaluator = XPathFactory.newInstance().newXPath();
--- src/security-server/system-test/src/intTest/java/org/niis/xroad/ss/test/addons/jmx/JmxClientImpl.java
+++ src/security-server/system-test/src/intTest/java/org/niis/xroad/ss/test/addons/jmx/JmxClientImpl.java
@@ -3,7 +3,7 @@ package org.niis.xroad.ss.test.addons.jmx;
 import javax.management.AttributeNotFoundException;
 import javax.management.InstanceNotFoundException;
 import javax.management.MBeanException;
-import javax.management.MalformedObjectNameException;
+import javax.management.MBeanServerConnection;
 import javax.management.ObjectName;
 import javax.management.ReflectionException;
 import javax.management.remote.JMXConnector;
@@ -21,13 +21,25 @@ public class JmxClientImpl implements JmxClient {
         this.jmxServiceUrl = jmxServiceUrl;
     }
 
-    public Object getValue(final String objectName, final String attribute) {
+    public Object getValue(final String objectNameFragment, final String attribute) {
         try (final JMXConnector connector = JMXConnectorFactory.connect(new JMXServiceURL(jmxServiceUrl.get()))) {
             connector.connect();
             final var connection = connector.getMBeanServerConnection();
-            return connection.getAttribute(ObjectName.getInstance(objectName), attribute);
-        } catch (MBeanException | AttributeNotFoundException | InstanceNotFoundException | ReflectionException | IOException |
-                 MalformedObjectNameException e) {
+            return connection.queryNames(null, null).stream()
+                    .filter(name -> name.getCanonicalName().contains(objectNameFragment))
+                    .findFirst()
+                    .map(name -> readAttribute(connection, name, attribute))
+                    .orElse(null);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to get JMX Object", e);
+        }
+    }
+
+
+    private Object readAttribute(MBeanServerConnection connection, ObjectName objectName, String attribute) {
+        try {
+            return connection.getAttribute(objectName, attribute);
+        } catch (InstanceNotFoundException | ReflectionException | IOException | MBeanException | AttributeNotFoundException e) {
             throw new RuntimeException("Failed to read JMX Object attribute", e);
         }
     }
