485f86e0f5d309671023a27626b1a81636c01a5d
Ričardas Bučiūnas <ricardas.buciunas@nortal.com>
2024-02-01 15:23:16 +0200

    chore: add debug ports for edc

--- src/security-server/edc/run-provider.sh
+++ src/security-server/edc/run-provider.sh
@@ -2,19 +2,30 @@
 
 resourcesDir="src/main/resources"
 jarFile="connector.jar"
+debugPort=""
 
-# Check for --in-memory flag
+# Check for --in-memory and --debug flags
 for arg in "$@"
 do
     if [ "$arg" == "--in-memory" ]; then
         jarFile="connector-inmemory.jar"
-        break
+    elif [ "$arg" == "--debug" ]; then
+        debugPort="5005"
     fi
 done
 
-exec java -Dedc.keystore="$resourcesDir/certs/cert.pfx" \
+# Construct the Java command
+javaCmd="java -Dedc.keystore="$resourcesDir/certs/cert.pfx" \
 -Dedc.keystore.password=123456 \
 -Dedc.vault="$resourcesDir/configuration/provider-vault.properties" \
 -Dedc.fs.config="$resourcesDir/configuration/provider-configuration.properties" \
--Xmx128m \
--jar "build/libs/$jarFile"
+-Xmx128m"
+
+# Add debug options if debug port is set
+if [ ! -z "$debugPort" ]; then
+    javaCmd="$javaCmd -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:$debugPort"
+fi
+
+# Append -jar option and execute the command
+javaCmd="$javaCmd -jar "build/libs/$jarFile""
+exec $javaCmd
