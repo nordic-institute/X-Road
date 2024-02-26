7d5074753f602312ac2d38b04a1b164662011171
Raido Kaju <raido.kaju@gmail.com>
2024-02-26 14:56:44 +0200

    feat: refactor workflow names/files, add local build script and Hurl support

--- .github/dev-containers/build-local.sh
+++ .github/dev-containers/build-local.sh
@@ -4,7 +4,7 @@ set -e # Exit immediately if a command exits with a non-zero status.
 
 # Build X-Road
 cd ../../src
-./compile_code.sh
+./gradlew clean build
 
 # Package X-Road
 cd packages
@@ -15,18 +15,18 @@ cd ../../.github/dev-containers/centralserver
 rm -rf build
 ./init_context.sh
 mkdir -p build/packages
-cp ../../src/packages/* build/packages/
+cp ../../../src/packages/build/ubuntu22.04/* build/packages/
 docker build -t centralserver .
 
-# Set up Central Server context and build container
-cd ../../.github/dev-containers/centralserver
+# Set up Security Server context and build container
+cd ../securityserver
 rm -rf build
 ./init_context.sh
 mkdir -p build/packages
-cp ../../src/packages/* build/packages/
+cp ../../../src/packages/build/ubuntu22.04/* build/packages/
 docker build -t centralserver .
 
 # Set up TestCA context and build container
-cd ../../.github/dev-containers/testca
+cd ../testca
 ./init_context.sh
 docker build -t testca .
