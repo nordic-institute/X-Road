fb47c1007bba06af4417ea4cb757e2cc9574e22f
andresrosenthal <andres.rosenthal@nortal.com>
2024-03-13 15:07:14 +0200

    feat: Configure proper Identity And Trust Protocol for EDC intercommunication

---
+++ src/security-server/edc/extensions/xroad-identity-and-trust/src/main/java/org/niis/xroad/edc/extension/iam/DefaultScopeExtractor.java
@@ -0,0 +1,17 @@
+package org.niis.xroad.edc.extension.iam;
+
+import org.eclipse.edc.policy.engine.spi.PolicyContext;
+import org.eclipse.edc.policy.model.Policy;
+import org.eclipse.edc.spi.iam.RequestScope;
+
+import java.util.Set;
+import java.util.function.BiFunction;
+
+public record DefaultScopeExtractor(Set<String> defaultScopes) implements BiFunction<Policy, PolicyContext, Boolean> {
+    @Override
+    public Boolean apply(Policy policy, PolicyContext policyContext) {
+        var scopes = policyContext.getContextData(RequestScope.Builder.class);
+        scopes.scope(defaultScopes.iterator().next());
+        return true;
+    }
+}
---
+++ src/security-server/edc/extensions/xroad-identity-and-trust/src/main/java/org/niis/xroad/edc/extension/iam/XrdIatpScopeExtension.java
@@ -0,0 +1,43 @@
+package org.niis.xroad.edc.extension.iam;
+
+import org.eclipse.edc.policy.engine.spi.PolicyEngine;
+import org.eclipse.edc.runtime.metamodel.annotation.Extension;
+import org.eclipse.edc.runtime.metamodel.annotation.Inject;
+import org.eclipse.edc.spi.system.ServiceExtension;
+import org.eclipse.edc.spi.system.ServiceExtensionContext;
+
+import java.util.Set;
+
+import static org.niis.xroad.edc.extension.iam.XrdIatpScopeExtension.NAME;
+
+@Extension(NAME)
+public class XrdIatpScopeExtension implements ServiceExtension {
+
+    static final String NAME = "X-Road IATP scope extension";
+
+    public static final String CATALOG_REQUEST_SCOPE = "request.catalog";
+    public static final String NEGOTIATION_REQUEST_SCOPE = "request.contract.negotiation";
+    public static final String TRANSFER_PROCESS_REQUEST_SCOPE = "request.transfer.process";
+
+    public static final String SCOPE_FORMAT = "%s:%s:read";
+    public static final String CREDENTIAL_TYPE_NAMESPACE = "org.eclipse.edc.vc.type";
+    public static final String CREDENTIAL_FORMAT = "XRoadCredential";
+
+    @Inject
+    private PolicyEngine policyEngine;
+
+    @Override
+    public String name() {
+        return NAME;
+    }
+
+    @Override
+    public void initialize(ServiceExtensionContext context) {
+        var contextMappingFunction = new DefaultScopeExtractor(
+                Set.of(SCOPE_FORMAT.formatted(CREDENTIAL_TYPE_NAMESPACE, CREDENTIAL_FORMAT)));
+        policyEngine.registerPostValidator(CATALOG_REQUEST_SCOPE, contextMappingFunction);
+        policyEngine.registerPostValidator(NEGOTIATION_REQUEST_SCOPE, contextMappingFunction);
+        policyEngine.registerPostValidator(TRANSFER_PROCESS_REQUEST_SCOPE, contextMappingFunction);
+    }
+
+}
