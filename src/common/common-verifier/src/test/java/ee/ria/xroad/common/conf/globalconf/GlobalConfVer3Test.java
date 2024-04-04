ff9cd073ea0119d9a5c595086a3e817a2b97d82f
Justas Samuolis <justas.samuolis@nortal.com>
2024-04-04 16:05:03 +0300

    chore: test fixes

--- src/common/common-verifier/src/test/java/ee/ria/xroad/common/conf/globalconf/GlobalConfVer3Test.java
+++ src/common/common-verifier/src/test/java/ee/ria/xroad/common/conf/globalconf/GlobalConfVer3Test.java
@@ -43,7 +43,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
