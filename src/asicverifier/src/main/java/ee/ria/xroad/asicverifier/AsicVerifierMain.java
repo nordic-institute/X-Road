dee31b8f35e6f28570c39b282f45a6d389de9a7d
Ovidijus Narkevicius <ovidijus.narkevicius@nortal.com>
2024-04-10 15:08:31 +0300

    chore: fix zip slip vulnerability
    Refs: //github.com/nordic-institute/X-Road/security/code-scanning/36
    
    Closes: XRDDEV-2612

--- src/asicverifier/src/main/java/ee/ria/xroad/asicverifier/AsicVerifierMain.java
+++ src/asicverifier/src/main/java/ee/ria/xroad/asicverifier/AsicVerifierMain.java
@@ -36,7 +36,6 @@ import ee.ria.xroad.common.conf.globalconf.GlobalConf;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
