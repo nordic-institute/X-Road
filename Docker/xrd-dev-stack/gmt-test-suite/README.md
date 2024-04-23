a559fb9f8371132191078a87949bea8b4f51f3f1
Raido Kaju <raido.kaju@niis.org>
2024-04-23 15:58:51 +0300

    chore: fix run script for Docker environment

--- Docker/xrd-dev-stack/gmt-test-suite/README.md
+++ Docker/xrd-dev-stack/gmt-test-suite/README.md
@@ -6,8 +6,14 @@ and `sed` installed.
 
 The intended use-case is running it inside a Docker container with the service client on `ss1`.
 
+Arguments are as follows:
+
+* 1 - Path to requests, defaults to .
+* 2 - Number of sequential tests, defaults to 500
+* 3 - Number of parallel tests, defaults to 500
+
 To override the default numbers (500 for both), run the script with the following arguments:
 
 ```bash
-./run-tests.sh [sequential_test_count] [parallel_test_count]
+./run-tests.sh [path_to_requests] [sequential_test_count] [parallel_test_count]
 ```
--- Docker/xrd-dev-stack/gmt-test-suite/run-tests.sh
+++ Docker/xrd-dev-stack/gmt-test-suite/run-tests.sh
@@ -1,21 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 
-sequential=${1:-500}
-parallel=${2:-500}
+data_path=${1:-./}
+sequential=${2:-500}
+parallel=${3:-500}
 
 # Test tasks
 getRandom() {
   # Change the request id in the request so we don't alawys send the same input
-  sed "s/_REQUEST_ID_/get_random_$1/" getRandom.xml | curl -s -d @- --header "Content-Type: text/xml" -X POST http://ss1:8080 > /dev/null
+  sed "s/_REQUEST_ID_/get_random_$1/" "$data_path/getRandom.xml" | curl -s -d @- --header "Content-Type: text/xml" -X POST http://ss1:8080 > /dev/null
 }
 
 helloService() {
   # Change the request id and name in the request so we don't alawys send the same input
-  sed "s/_REQUEST_ID_/get_hello_$1/" helloService.xml | sed "s/_NAME_/Test$1/" | curl -s -d @- --header "Content-Type: text/xml" -X POST http://ss1:8080 > /dev/null
+  sed "s/_REQUEST_ID_/get_hello_$1/" "$data_path/helloService.xml" | sed "s/_NAME_/Test$1/" | curl -s -d @- --header "Content-Type: text/xml" -X POST http://ss1:8080 > /dev/null
 }
 
 rest_1() {
-  curl -s -d @rest-test-1.json --header "Content-Type: application/json" --header "X-Road-Client: DEV/COM/4321/TestClient" -X POST http://ss1:8080/r1/DEV/COM/1234/TestService/mock1 > /dev/null
+  curl -s -d "@$data_path/rest-test-1.json" --header "Content-Type: application/json" --header "X-Road-Client: DEV/COM/4321/TestClient" -X POST http://ss1:8080/r1/DEV/COM/1234/TestService/mock1 > /dev/null
 }
 
 # Sequential tests
