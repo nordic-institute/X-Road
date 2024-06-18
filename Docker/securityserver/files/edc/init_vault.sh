ea063648c3bcf4dfe7ed8f5f3568130d965b6266
andresrosenthal <andres.rosenthal@nortal.com>
2024-06-18 12:34:22 +0300

    feat: Use DIDs from Identity Hub

--- Docker/securityserver/files/edc/init_vault.sh
+++ Docker/securityserver/files/edc/init_vault.sh
@@ -14,12 +14,13 @@ vault server -dev &> /var/log/xroad/vault.log &
 wait_for_vault
 
 #store initial values
+#NB! Keys are urlencoded as values are fetched by EDC vault client through HTTP GET
 
 #public keys
 vault kv put --mount secret public-key content=@/etc/xroad-edc/certs/public-key
-vault kv put --mount secret did%3Aweb%3Axroad-8-ss0.s3.eu-west-1.amazonaws.com content=@/etc/xroad-edc/certs/ss0.crt
-vault kv put --mount secret did%3Aweb%3Axroad-8-ss1.s3.eu-west-1.amazonaws.com content=@/etc/xroad-edc/certs/ss1.crt
+vault kv put --mount secret did%3Aweb%3Ass0%253A9396 content=@/etc/xroad-edc/certs/ss0.crt
+vault kv put --mount secret did%3Aweb%3Ass1%253A9396 content=@/etc/xroad-edc/certs/ss1.crt
 
 #private keys
-vault kv put --mount secret did%3Aweb%3Axroad-8-ss0.s3.eu-west-1.amazonaws.com-private-key content=@/etc/xroad-edc/certs/ss0.pkcs8
-vault kv put --mount secret did%3Aweb%3Axroad-8-ss1.s3.eu-west-1.amazonaws.com-private-key content=@/etc/xroad-edc/certs/ss1.pksc8
+vault kv put --mount secret did%3Aweb%3Ass0%253A9396-private-key content=@/etc/xroad-edc/certs/ss0.pkcs8
+vault kv put --mount secret did%3Aweb%3Ass1%253A9396-private-key content=@/etc/xroad-edc/certs/ss1.pksc8
