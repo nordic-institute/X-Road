fcf4b9f6778ba29f6db7fe2f20783d4e7b422199
Ovidijus Narkevicius <Ovidijus.Narkevicius@nortal.com>
2024-07-15 20:52:27 +0300

    fix: avoid adding unnecessary spaces
    Refs: XRDSD-434

--- src/packages/src/xroad/common/centralserver/usr/share/xroad/scripts/configure_management_api_key.sh
+++ src/packages/src/xroad/common/centralserver/usr/share/xroad/scripts/configure_management_api_key.sh
@@ -53,7 +53,11 @@ else
     source /etc/xroad/db_libpq.env
   fi
 
-  PGOPTIONS="${PGOPTIONS_EXTRA-}" PGDATABASE="$db_database" PGUSER="$db_user" PGPASSWORD="$db_password" psql -h "$db_host" -p "$db_port" -qtA -c \
+  if [ ! -z $PGOPTIONS_EXTRA ]; then
+    PGOPTIONS_EXTRA=" ${PGOPTIONS_EXTRA} "
+  fi
+
+  PGOPTIONS="${PGOPTIONS_EXTRA-}"PGDATABASE="$db_database" PGUSER="$db_user" PGPASSWORD="$db_password" psql -h "$db_host" -p "$db_port" -qtA -c \
   "INSERT INTO apikey(id, encodedkey) VALUES ((SELECT NEXTVAL('hibernate_sequence')), '$encoded_token');
   INSERT INTO apikey_roles(apikey_id,role) VALUES ((SELECT id FROM apikey WHERE encodedkey = '$encoded_token'), 'XROAD_MANAGEMENT_SERVICE');"
   if [ $? -ne 0 ] ; then
