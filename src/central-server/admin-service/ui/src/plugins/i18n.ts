9949a15fe67e6b01911035d5b931ce3a6f402896
bibigozel <bibi45-prog>
2025-02-06 12:06:38 +0200

    feat(lang): add Turkmen languages

--- src/central-server/admin-service/ui/src/plugins/i18n.ts
+++ src/central-server/admin-service/ui/src/plugins/i18n.ts
@@ -27,7 +27,7 @@
 import { prepareI18n } from '@niis/shared-ui';
 import enMessages from '@/locales/en.json';
 
-export const availableLanguages = ['en', 'es', 'ru'];
+export const availableLanguages = ['en', 'es', 'ru', 'tk'];
 
 export const { i18n, languageHelper } = prepareI18n(enMessages, loadMessages);
 
