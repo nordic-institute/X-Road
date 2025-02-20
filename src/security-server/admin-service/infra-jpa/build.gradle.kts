5e842c0e3aad9088969a05a23c03ca18f0cb5b12
Ričardas Bučiūnas <ricardas.buciunas@nortal.com>
2025-02-20 18:00:34 +0200

    chore: add serverconf init image creation
    Refs: XRDDEV-2837

--- src/security-server/admin-service/infra-jpa/build.gradle.kts
+++ src/security-server/admin-service/infra-jpa/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   id("xroad.java-conventions")
-  id("java-library")
+  id("xroad.jib-conventions")
 }
 
 dependencies {
@@ -17,4 +17,38 @@ tasks.register<Jar>("changelogJar") {
 
 artifacts {
   add("changelogJar", tasks.named("changelogJar"))
+}
+
+val libs = project.extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+jib {
+  from {
+    image = "liquibase:${libs.findVersion("liquibase").get()}"
+  }
+  to {
+    image = "${project.property("xroadImageRegistry")}/ss-db-serverconf-init"
+    tags = setOf("latest")
+  }
+  container {
+    entrypoint = listOf("/liquibase/docker-entrypoint.sh")
+    workingDirectory = "/liquibase"
+    user = "liquibase"
+    args = listOf(
+      "--log-level=debug",
+      "update"
+    )
+    environment = mapOf(
+      "LIQUIBASE_COMMAND_CHANGELOG_FILE" to "changelog/serverconf-changelog.xml",
+      "LIQUIBASE_COMMAND_DRIVER" to "org.postgresql.Driver",
+    )
+
+  }
+  extraDirectories {
+    paths {
+      path {
+        setFrom(project.file("src/main/resources/liquibase/").toPath())
+        into = "/liquibase/changelog"
+      }
+    }
+  }
 }
