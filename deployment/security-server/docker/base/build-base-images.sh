648218e79eef254749b6160d1e73b2efb0d6487e
Justas Samuolis <justas.samuolis@nortal.com>
2025-03-06 16:43:38 +0200

    chore: github ci - build base images
    Refs: XRDDEV-2837

--- deployment/security-server/docker/base/build-base-images.sh
+++ deployment/security-server/docker/base/build-base-images.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+# Set registry URL, default to localhost:5555 if not provided
+REGISTRY_URL=${1:-localhost:5555}
+
 echo "Building baseline"
 docker buildx create --name multiarch-builder --driver docker-container --driver-opt network=host --use
 
 docker buildx build \
   --platform linux/amd64,linux/arm64 \
-  --tag localhost:5555/ss-baseline-runtime \
+  --tag "$REGISTRY_URL"/ss-baseline-runtime \
   --file Dockerfile-baseline \
   --push \
   .
 
 docker buildx build \
   --platform linux/amd64,linux/arm64 \
-  --tag localhost:5555/ss-baseline-ui-runtime \
+  --tag "$REGISTRY_URL"/ss-baseline-ui-runtime \
   --file Dockerfile-ui-baseline \
   --push \
   .
