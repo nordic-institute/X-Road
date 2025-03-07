af1f5381d08759af3d1057b36f29b86ce8a46642
Madis Loitmaa <madis.loitmaa@nortal.com>
2025-03-07 12:32:14 +0200

    test: added tests for auth key change
    Refs: XRDDEV-2760

--- src/service/proxy/proxy-core/src/test/java/org/niis/xroad/proxy/core/AbstractProxyIntegrationTest.java
+++ src/service/proxy/proxy-core/src/test/java/org/niis/xroad/proxy/core/AbstractProxyIntegrationTest.java
@@ -26,16 +26,23 @@
 package org.niis.xroad.proxy.core;
 
 import ee.ria.xroad.common.SystemProperties;
+import ee.ria.xroad.common.TestPortUtils;
+import ee.ria.xroad.common.identifier.ServiceId;
 import ee.ria.xroad.common.util.JobManager;
 import ee.ria.xroad.common.util.TimeUtils;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.rules.ExternalResource;
-import org.junit.runner.Description;
-import org.junit.runners.model.Statement;
+import lombok.Getter;
+import lombok.SneakyThrows;
+import org.apache.http.protocol.HttpContext;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.niis.xroad.globalconf.impl.cert.CertChainFactory;
 import org.niis.xroad.globalconf.impl.cert.CertHelper;
 import org.niis.xroad.keyconf.KeyConfProvider;
@@ -48,17 +55,24 @@ import org.niis.xroad.proxy.core.serverproxy.ServerProxy;
 import org.niis.xroad.proxy.core.test.TestService;
 import org.niis.xroad.proxy.core.test.TestSigningCtxProvider;
 import org.niis.xroad.proxy.core.util.CommonBeanProxy;
+import org.niis.xroad.test.globalconf.EmptyGlobalConf;
 import org.niis.xroad.test.globalconf.TestGlobalConf;
 import org.niis.xroad.test.globalconf.TestGlobalConfWrapper;
 import org.niis.xroad.test.keyconf.TestKeyConf;
+import org.niis.xroad.test.serverconf.EmptyServerConf;
 import org.niis.xroad.test.serverconf.TestServerConf;
 import org.niis.xroad.test.serverconf.TestServerConfWrapper;
 
-import java.net.ServerSocket;
+import javax.net.ssl.SSLSession;
+
+import java.net.URI;
+import java.security.cert.X509Certificate;
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneOffset;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static ee.ria.xroad.common.SystemProperties.PROXY_SERVER_LISTEN_ADDRESS;
@@ -72,40 +86,21 @@ public abstract class AbstractProxyIntegrationTest {
     private static final Set<Integer> RESERVED_PORTS = new HashSet<>();
     private static final Instant CLOCK_FIXED_INSTANT = Instant.parse("2020-01-01T00:00:00Z");
 
-    private static ClientProxy clientProxy;
-    private static ServerProxy serverProxy;
+    protected static ClientProxy clientProxy;
+    protected static ServerProxy serverProxy;
 
     protected static int proxyClientPort = getFreePort();
-    protected static int servicePort = getFreePort();
-    protected static TestService service;
 
-    protected static final TestServerConfWrapper TEST_SERVER_CONF = new TestServerConfWrapper(new TestServerConf(servicePort));
+    protected static final TestServerConfWrapper TEST_SERVER_CONF = new TestServerConfWrapper(new TestServiceServerConf());
     protected static final TestGlobalConfWrapper TEST_GLOBAL_CONF = new TestGlobalConfWrapper(new TestGlobalConf());
+    protected static TestKeyConf clientKeyConf;
+    protected static TestKeyConf serverKeyConf;
+    protected static LoggingAuthTrustVerifier clientAuthTrustVerifier;
 
-    @Rule
-    public final ExternalResource serviceResource = new ExternalResource() {
-        @Override
-        public Statement apply(Statement base, Description description) {
-            return new Statement() {
-                @Override
-                public void evaluate() throws Throwable {
-                    try {
-                        before();
-                        service.before();
-                        try {
-                            base.evaluate();
-                        } finally {
-                            service.assertOk();
-                        }
-                    } finally {
-                        after();
-                    }
-                }
-            };
-        }
-    };
+    @RegisterExtension
+    protected static TestServiceExtension testServiceExtension = new TestServiceExtension();
 
-    @BeforeClass
+    @BeforeAll
     public static void beforeAll() throws Exception {
         TimeUtils.setClock(Clock.fixed(CLOCK_FIXED_INSTANT, ZoneOffset.UTC));
 
@@ -133,57 +128,125 @@ public abstract class AbstractProxyIntegrationTest {
 
         org.apache.xml.security.Init.init();
 
+        OpMonitoring.init(new EmptyServerConf());
+        MessageLog.init(mock(JobManager.class), new EmptyGlobalConf(), new EmptyServerConf());
+
         startServices();
     }
 
     static void startServices() throws Exception {
-        service = new TestService(servicePort);
-        service.start();
+        startClientProxy();
+        startServerProxy();
+    }
 
-        KeyConfProvider keyConfProvider = new TestKeyConf(TEST_GLOBAL_CONF);
+    private static void startClientProxy() throws Exception {
+        clientKeyConf = new TestKeyConf(TEST_GLOBAL_CONF);
         CertHelper certHelper = new CertHelper(TEST_GLOBAL_CONF);
         CertChainFactory certChainFactory = new CertChainFactory(TEST_GLOBAL_CONF);
-        AuthTrustVerifier authTrustVerifier = new AuthTrustVerifier(keyConfProvider, certHelper, certChainFactory);
-        SigningCtxProvider signingCtxProvider = new TestSigningCtxProvider(TEST_GLOBAL_CONF, keyConfProvider);
-
+        clientAuthTrustVerifier = new LoggingAuthTrustVerifier(clientKeyConf, certHelper, certChainFactory);
+        SigningCtxProvider signingCtxProvider = new TestSigningCtxProvider(TEST_GLOBAL_CONF, clientKeyConf);
         CommonBeanProxy commonBeanProxy = new CommonBeanProxy(TEST_GLOBAL_CONF, TEST_SERVER_CONF,
-                keyConfProvider, signingCtxProvider, certChainFactory, certHelper);
+                clientKeyConf, signingCtxProvider, certChainFactory, certHelper);
 
-        clientProxy = new ClientProxy(commonBeanProxy, TEST_GLOBAL_CONF, keyConfProvider, TEST_SERVER_CONF,
-                authTrustVerifier);
+        clientProxy = new ClientProxy(commonBeanProxy, TEST_GLOBAL_CONF, clientKeyConf, TEST_SERVER_CONF,
+                clientAuthTrustVerifier);
         clientProxy.afterPropertiesSet();
+    }
+
+    private static void startServerProxy() throws Exception {
+        serverKeyConf = new TestKeyConf(TEST_GLOBAL_CONF);
+        CertHelper certHelper = new CertHelper(TEST_GLOBAL_CONF);
+        CertChainFactory certChainFactory = new CertChainFactory(TEST_GLOBAL_CONF);
+        SigningCtxProvider signingCtxProvider = new TestSigningCtxProvider(TEST_GLOBAL_CONF, serverKeyConf);
+        CommonBeanProxy commonBeanProxy = new CommonBeanProxy(TEST_GLOBAL_CONF, TEST_SERVER_CONF,
+                serverKeyConf, signingCtxProvider, certChainFactory, certHelper);
 
         serverProxy = new ServerProxy(commonBeanProxy);
         serverProxy.afterPropertiesSet();
-
-        OpMonitoring.init(TEST_SERVER_CONF);
-        MessageLog.init(mock(JobManager.class), TEST_GLOBAL_CONF, TEST_SERVER_CONF);
     }
 
-    @AfterClass
+    @AfterAll
     public static void afterAll() throws Exception {
         RESERVED_PORTS.clear();
-        service.destroy();
         serverProxy.destroy();
         clientProxy.destroy();
     }
 
-    @After
+    @AfterEach
     public void after() {
-        TEST_SERVER_CONF.setServerConfProvider(new TestServerConf(servicePort));
+        TEST_SERVER_CONF.setServerConfProvider(new TestServiceServerConf());
         TEST_GLOBAL_CONF.setGlobalConfProvider(new TestGlobalConf());
+        clientAuthTrustVerifier.clearVerifiedCertificates();
+    }
+
+    protected void setServiceHandler(TestService.Handler handler) {
+        testServiceExtension.getService().setHandler(handler);
     }
 
     static int getFreePort() {
         while (true) {
-            try (ServerSocket ss = new ServerSocket(0)) {
-                final int port = ss.getLocalPort();
-                if (RESERVED_PORTS.add(port)) {
-                    return port;
-                }
-            } catch (Exception e) {
-                throw new IllegalStateException(e);
+            final int port = TestPortUtils.findRandomPort();
+            if (RESERVED_PORTS.add(port)) {
+                return port;
             }
+        }
+    }
+
+    @Getter
+    public static class TestServiceExtension implements BeforeAllCallback, AfterAllCallback, BeforeEachCallback, AfterEachCallback {
+
+        private TestService service;
+
+        @Override
+        public void beforeAll(ExtensionContext context) throws Exception {
+            service = new TestService(0);
+            service.start();
+        }
+
+        @Override
+        public void afterAll(ExtensionContext context) throws Exception {
+            service.destroy();
+        }
+
+        @Override
+        public void beforeEach(ExtensionContext context) {
+            service.before();
+        }
+
+        @Override
+        public void afterEach(ExtensionContext context) throws Exception {
+            service.assertOk();
+        }
+    }
+
+    public static class TestServiceServerConf extends TestServerConf {
+
+        @Override
+        public String getServiceAddress(ServiceId service) {
+            return "http://127.0.0.1:" + testServiceExtension.getService().getPort();
+        }
+    }
+
+    @Getter
+    public static class LoggingAuthTrustVerifier extends AuthTrustVerifier {
+        private final List<X509Certificate> verifiedCertificates = new ArrayList<>();
+
+        public LoggingAuthTrustVerifier(KeyConfProvider keyConfProvider, CertHelper certHelper, CertChainFactory certChainFactory) {
+            super(keyConfProvider, certHelper, certChainFactory);
+        }
+
+        @SneakyThrows
+        @Override
+        protected void verify(HttpContext context, SSLSession sslSession, URI selectedAddress) {
+            X509Certificate[] peerCerts = (X509Certificate[]) sslSession.getPeerCertificates();
+            if (peerCerts.length > 0) {
+                verifiedCertificates.add(peerCerts[0]);
+            }
+            super.verify(context, sslSession, selectedAddress);
+        }
+
+        public void clearVerifiedCertificates() {
+            verifiedCertificates.clear();
         }
     }
 }
