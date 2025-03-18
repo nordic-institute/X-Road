6aee548bfab9001522f18c14549a5d48f1490224
Justas Samuolis <justas.samuolis@nortal.com>
2025-03-18 09:42:08 +0200

    chore: op-monitor-db module
    Refs: XRDDEV-2854

--- src/settings.gradle.kts
+++ src/settings.gradle.kts
@@ -54,6 +54,7 @@ include("service:monitor:monitor-core")
 include("service:op-monitor:op-monitor-application")
 include("service:op-monitor:op-monitor-api")
 include("service:op-monitor:op-monitor-core")
+include("service:op-monitor:op-monitor-db")
 
 include("service:proxy:proxy-application")
 include("service:proxy:proxy-core")
