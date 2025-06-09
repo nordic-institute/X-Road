9ac841f665fb386714f84662118381fcae31e4fc
Ovidijus Narkevicius <Ovidijus.Narkevicius@nortal.com>
2025-06-09 20:33:51 +0300

    refactor(UI): extract common layouts/views
    xpath
    
    Refs: XRDDEV-2914

--- src/central-server/admin-service/ui-system-test/src/intTest/java/org/niis/xroad/cs/test/ui/page/SettingsApiKeysPageObj.java
+++ src/central-server/admin-service/ui-system-test/src/intTest/java/org/niis/xroad/cs/test/ui/page/SettingsApiKeysPageObj.java
@@ -74,7 +74,7 @@ public class SettingsApiKeysPageObj {
         }
 
         public SelenideElement createdApiKeyId() {
-            return $x("//div[@data-test='created-apikey-id']");
+            return $x("//td[@data-test='created-apikey-id']");
         }
 
         public SelenideElement createdApiKey() {
