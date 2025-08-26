d6af89d24f8b856045e56e81c9635c0bd9ced1d0
Ričardas Bučiūnas <ricardas.buciunas@nortal.com>
2025-08-26 13:32:35 +0300

    fix: add missing logback library to wsdl validator

--- src/addons/wsdlvalidator/build.gradle.kts
+++ src/addons/wsdlvalidator/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
   implementation(libs.apache.cxfRtTransportsHttp)
   implementation(libs.jakarta.annotationApi)
   implementation(project(":common:common-core"))
+  implementation(libs.logback.classic)
 }
 
 tasks.shadowJar {
