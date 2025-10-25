a53deaa3f750582b209fcd9fe36eafcf503fa315
Andres Rosenthal <andres.rosenthal@nortal.com>
2025-10-25 22:21:43 +0300

    chore: Serve OpenBao over HTTPS on k8s

--- development/k8s/terraform/modules/openbao/main.tf
+++ development/k8s/terraform/modules/openbao/main.tf
@@ -10,6 +10,51 @@ resource "helm_release" "postgresql_openbao" {
   values = [yamlencode(var.openbao_db_override_values)]
 }
 
+resource "tls_private_key" "openbao_server" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P384"
+}
+
+resource "tls_self_signed_cert" "openbao_server" {
+  private_key_pem = tls_private_key.openbao_server.private_key_pem
+
+  subject {
+    common_name  = "openbao"
+  }
+
+  dns_names = [
+    "localhost",
+    "openbao.ss.svc.cluster.local"
+  ]
+
+  validity_period_hours = 43800 # 5 years
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
+}
+
+resource "null_resource" "openbao_server_tls_secret" {
+  provisioner "local-exec" {
+    command = <<EOT
+      cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: openbao-server-tls
+  namespace: ${var.namespace}
+type: kubernetes.io/tls
+data:
+  tls.crt: ${base64encode(tls_self_signed_cert.openbao_server.cert_pem)}
+  tls.key: ${base64encode(tls_private_key.openbao_server.private_key_pem)}
+EOF
+    EOT
+  }
+  depends_on = [helm_release.postgresql_openbao]
+}
+
 resource "helm_release" "openbao_secret_store" {
   name      = "openbao"
   namespace = var.namespace
@@ -19,7 +64,7 @@ resource "helm_release" "openbao_secret_store" {
   chart      = "openbao"
   version    = "0.19.0"
 
-  depends_on = [helm_release.postgresql_openbao]
+  depends_on = [null_resource.openbao_server_tls_secret]
 
   values = [yamlencode(var.openbao_override_values)]
 }
