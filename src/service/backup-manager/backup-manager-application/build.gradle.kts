8683db74f9db09326fe51f1042515073257cb250
justasnortal <113103740+justasnortal@users.noreply.github.com>
2025-11-03 15:58:35 +0200

    chore: add application name to db connection (#3067)
    * chore: add db connection application name  in conf client, op monitor, db prop source
    
    Refs: XRDDEV-2972
    
    * chore: enabling db prop source for backup-manager, op-monitor, proxy, monitor, signer
    
    Refs: XRDDEV-2972
    
    * chore: enabling db prop source modules in k8s
    
    Refs: XRDDEV-2972
    
    * chore: minor fix
    
    Refs: XRDDEV-2972
    
    * chore: application name for messagelog db
    
    Refs: XRDDEV-2972

--- src/service/backup-manager/backup-manager-application/build.gradle.kts
+++ src/service/backup-manager/backup-manager-application/build.gradle.kts
@@ -8,9 +8,11 @@ dependencies {
   implementation(platform(libs.quarkus.bom))
 
   implementation(project(":lib:bootstrap-quarkus"))
+  implementation(project(":common:common-properties-db-source-quarkus"))
   implementation(project(":common:common-rpc-quarkus"))
   implementation(project(":service:backup-manager:backup-manager-core"))
 
+  implementation(libs.postgresql)
   implementation(libs.bundles.quarkus.containerized)
   implementation(libs.quarkus.extension.systemd.notify)
 
