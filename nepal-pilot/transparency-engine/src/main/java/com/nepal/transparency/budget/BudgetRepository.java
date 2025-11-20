75bdedd0b2b582dc4560176b54d0a5c59be88b5b
DrSkyle <86089508+DrSkyle@users.noreply.github.com>
2025-11-20 19:42:16 +0545

    Revert "Implement Nepal Transparency Engine Pilot"

--- nepal-pilot/transparency-engine/src/main/java/com/nepal/transparency/budget/BudgetRepository.java
+++
@@ -1,6 +0,0 @@
-package com.nepal.transparency.budget;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BudgetRepository extends JpaRepository<Budget, TrackingID> {
-}
