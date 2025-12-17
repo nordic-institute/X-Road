31ad6a156d5ae5b8c2739c93bd8832aabd78f378
Justas Samuolis <justas.samuolis@nortal.com>
2025-12-17 15:50:33 +0200

    chore: deploying prometheus stack locally
    Refs: XRDDEV-3058

--- development/k8s/terraform/environments/dev/main.tf
+++ development/k8s/terraform/environments/dev/main.tf
@@ -5,10 +5,10 @@ provider "helm" {
 }
 
 module "kind_cluster" {
-  source       = "../../modules/kind"
+  source = "../../modules/kind"
 
   kind_cluster_name = "xroad-dev-cluster"
-  kubeconfig_path = var.kubeconfig_path
+  kubeconfig_path   = var.kubeconfig_path
   containerd_config_patches = [
     <<-EOF
     [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:5555"]
@@ -18,29 +18,29 @@ module "kind_cluster" {
 }
 
 module "openbao" {
-  source      = "../../modules/openbao"
+  source = "../../modules/openbao"
 
   depends_on = [
     module.kind_cluster
   ]
 
-  namespace = var.security_server_namespace
-  openbao_init_chart_repo = null
-  openbao_init_chart = "${path.module}/../../../../../deployment/security-server/k8s/charts/openbao-init"
+  namespace                  = var.security_server_namespace
+  openbao_init_chart_repo    = null
+  openbao_init_chart         = "${path.module}/../../../../../deployment/security-server/k8s/charts/openbao-init"
   openbao_init_chart_version = null
 
-  openbao_db_override_values = yamldecode(file("${path.module}/override-values/openbao-db-values.yaml"))
-  openbao_override_values = yamldecode(file("${path.module}/override-values/openbao-values.yaml"))
+  openbao_db_override_values   = yamldecode(file("${path.module}/override-values/openbao-db-values.yaml"))
+  openbao_override_values      = yamldecode(file("${path.module}/override-values/openbao-values.yaml"))
   openbao_init_override_values = yamldecode(file("${path.module}/override-values/openbao-init-values.yaml"))
 }
 
 module "cs_service_bridge" {
-  source        = "../../modules/external-service-bridge"
-  name          = "xrd-cs"
+  source = "../../modules/external-service-bridge"
+  name   = "xrd-cs"
 
-  namespace = var.security_server_namespace
-  external_service_bridge_chart_repo = null
-  external_service_bridge_chart = "${path.module}/../../../../../deployment/security-server/k8s/charts/external-service-bridge"
+  namespace                             = var.security_server_namespace
+  external_service_bridge_chart_repo    = null
+  external_service_bridge_chart         = "${path.module}/../../../../../deployment/security-server/k8s/charts/external-service-bridge"
   external_service_bridge_chart_version = null
 
   external_host = "host.docker.internal"
@@ -73,12 +73,12 @@ module "cs_service_bridge" {
 }
 
 module "ca_service_bridge" {
-  source        = "../../modules/external-service-bridge"
-  name          = "xrd-ca"
+  source = "../../modules/external-service-bridge"
+  name   = "xrd-ca"
 
-  namespace = var.security_server_namespace
-  external_service_bridge_chart_repo = null
-  external_service_bridge_chart = "${path.module}/../../../../../deployment/security-server/k8s/charts/external-service-bridge"
+  namespace                             = var.security_server_namespace
+  external_service_bridge_chart_repo    = null
+  external_service_bridge_chart         = "${path.module}/../../../../../deployment/security-server/k8s/charts/external-service-bridge"
   external_service_bridge_chart_version = null
 
   external_host = "host.docker.internal"
@@ -101,12 +101,12 @@ module "ca_service_bridge" {
 }
 
 module "ss0_service_bridge" {
-  source        = "../../modules/external-service-bridge"
-  name          = "xrd-ss0"
+  source = "../../modules/external-service-bridge"
+  name   = "xrd-ss0"
 
-  namespace = var.security_server_namespace
-  external_service_bridge_chart_repo = null
-  external_service_bridge_chart = "${path.module}/../../../../../deployment/security-server/k8s/charts/external-service-bridge"
+  namespace                             = var.security_server_namespace
+  external_service_bridge_chart_repo    = null
+  external_service_bridge_chart         = "${path.module}/../../../../../deployment/security-server/k8s/charts/external-service-bridge"
   external_service_bridge_chart_version = null
 
   external_host = "host.docker.internal"
@@ -129,7 +129,7 @@ module "ss0_service_bridge" {
 }
 
 module "security-server" {
-  source       = "../../modules/security-server"
+  source = "../../modules/security-server"
 
   depends_on = [
     module.openbao,
@@ -138,13 +138,25 @@ module "security-server" {
     module.ss0_service_bridge
   ]
 
-  namespace = var.security_server_namespace
-  security_server_chart_repo = null
-  security_server_chart = "${path.module}/../../../../../deployment/security-server/k8s/charts/security-server"
+  namespace                     = var.security_server_namespace
+  security_server_chart_repo    = null
+  security_server_chart         = "${path.module}/../../../../../deployment/security-server/k8s/charts/security-server"
   security_server_chart_version = null
 
-  serverconf_db_override_values = yamldecode(file("${path.module}/override-values/serverconf-db-values.yaml"))
-  messagelog_db_override_values = yamldecode(file("${path.module}/override-values/messagelog-db-values.yaml"))
-  opmonitor_db_override_values = yamldecode(file("${path.module}/override-values/opmonitor-db-values.yaml"))
+  serverconf_db_override_values   = yamldecode(file("${path.module}/override-values/serverconf-db-values.yaml"))
+  messagelog_db_override_values   = yamldecode(file("${path.module}/override-values/messagelog-db-values.yaml"))
+  opmonitor_db_override_values    = yamldecode(file("${path.module}/override-values/opmonitor-db-values.yaml"))
   security_server_override_values = yamldecode(file("${path.module}/override-values/security-server-values.yaml"))
+}
+
+module "prometheus" {
+  source = "../../modules/prometheus"
+
+  depends_on = [
+    module.kind_cluster
+  ]
+
+  namespace = var.monitoring_namespace
+
+  prometheus_override_values = yamldecode(file("${path.module}/override-values/prometheus-values.yaml"))
 }
