0aedd3bfa04b6a0eae9cf89ca2f43bc86ed7e203
Eneli Reimets <eneli.reimets@nortal.com>
2025-12-23 15:58:45 +0200

    fix: remove unnecessary import from SignerUtil

--- src/signer/src/main/java/ee/ria/xroad/signer/util/SignerUtil.java
+++ src/signer/src/main/java/ee/ria/xroad/signer/util/SignerUtil.java
@@ -31,7 +31,6 @@ import ee.ria.xroad.signer.tokenmanager.TokenManager;
 
 import jakarta.xml.bind.DatatypeConverter;
 import lombok.SneakyThrows;
-import org.apache.commons.lang3.RandomUtils;
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
