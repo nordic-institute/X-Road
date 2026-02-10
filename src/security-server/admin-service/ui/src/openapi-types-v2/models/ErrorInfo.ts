e6800e326d4c369f72849a753c24fb110ac9dc4f
Egidijus <egidijus.milierius@nortal.com>
2026-02-10 09:37:22 +0200

    fix: actually remove openapi-types-v2 generated files

--- src/security-server/admin-service/ui/src/openapi-types-v2/models/ErrorInfo.ts
+++
@@ -1,8 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-/**
- * Error response object (shared with main API spec)
- */
-export type ErrorInfo = Record<string, any>;
