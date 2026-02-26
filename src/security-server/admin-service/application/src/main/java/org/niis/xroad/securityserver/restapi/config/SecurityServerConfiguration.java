0d32019514fde75332bcc00895dbe7a29a3772d5
Mikk Bachmann <Mikk-Erik.Bachmann@nortal.com>
2026-02-26 15:19:12 +0200

    fix: IpThrottlingFilter ordering wasn't picked up and instead default was used (lowest precedence)
    refs: XRDDEV-3071

--- src/security-server/admin-service/application/src/main/java/org/niis/xroad/securityserver/restapi/config/SecurityServerConfiguration.java
+++ src/security-server/admin-service/application/src/main/java/org/niis/xroad/securityserver/restapi/config/SecurityServerConfiguration.java
@@ -27,7 +27,6 @@ package org.niis.xroad.securityserver.restapi.config;
 
 import ee.ria.xroad.common.util.process.ExternalProcessRunner;
 
-import jakarta.servlet.Filter;
 import org.niis.xroad.common.api.throttle.IpThrottlingFilter;
 import org.niis.xroad.restapi.config.AddCorrelationIdFilter;
 import org.niis.xroad.restapi.config.ApiCachingConfiguration;
@@ -38,6 +37,7 @@ import org.niis.xroad.securityserver.restapi.service.diagnostic.MonitorClient;
 import org.niis.xroad.securityserver.restapi.service.diagnostic.OsVersionCollector;
 import org.niis.xroad.securityserver.restapi.service.diagnostic.XrdPackagesCollector;
 import org.niis.xroad.securityserver.restapi.service.diagnostic.XrdProcessesCollector;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -56,16 +56,20 @@ import static org.niis.xroad.securityserver.restapi.service.CertificateAuthority
 @Configuration
 public class SecurityServerConfiguration {
 
+    private static final int IP_THROTTLING_FILTER_ORDER = AddCorrelationIdFilter.CORRELATION_ID_FILTER_ORDER + 3;
+
     @Bean
     public ExternalProcessRunner externalProcessRunner() {
         return new ExternalProcessRunner();
     }
 
     @Bean
-    @Order(AddCorrelationIdFilter.CORRELATION_ID_FILTER_ORDER + 3)
     @Profile("nontest")
-    public Filter ipThrottlingFilter(AdminServiceProperties properties) {
-        return new IpThrottlingFilter(properties);
+    public FilterRegistrationBean<IpThrottlingFilter> ipThrottlingFilter(AdminServiceProperties properties) {
+        var filter = new IpThrottlingFilter(properties);
+        var bean = new FilterRegistrationBean<>(filter);
+        bean.setOrder(IP_THROTTLING_FILTER_ORDER);
+        return bean;
     }
 
     @Bean
