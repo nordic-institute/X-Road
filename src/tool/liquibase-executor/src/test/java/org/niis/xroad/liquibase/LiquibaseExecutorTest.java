7837504b6c0390709533f460d053c8ec869bc673
Ričardas Bučiūnas <ricardas.buciunas@nortal.com>
2026-03-17 16:05:27 +0200

    test(05.11-01): add failing test shouldIncludeSearchPathInArgs
    - Assert --searchPath=classpath: is the first argument in buildLiquibaseArgs()
    - Assert --changeLogFile uses hyphenated op-monitor name

--- src/tool/liquibase-executor/src/test/java/org/niis/xroad/liquibase/LiquibaseExecutorTest.java
+++ src/tool/liquibase-executor/src/test/java/org/niis/xroad/liquibase/LiquibaseExecutorTest.java
@@ -140,6 +140,17 @@ class LiquibaseExecutorTest {
     }
 
     @Test
+    void shouldIncludeSearchPathInArgs() {
+        var executor = new LiquibaseExecutor();
+        new CommandLine(executor).parseArgs("--changelog=op-monitor", "--url=jdbc:h2:mem:test", "update");
+        List<String> args = Arrays.asList(executor.buildLiquibaseArgs());
+        assertEquals("--searchPath=classpath:", args.get(0),
+                "--searchPath=classpath: must be the first argument, got: " + args);
+        assertTrue(args.contains("--changeLogFile=liquibase/op-monitor-changelog.xml"),
+                "Should have --changeLogFile for op-monitor, got: " + args);
+    }
+
+    @Test
     void shouldTranslateChangelogToChangeLogFile() {
         var executor = new LiquibaseExecutor();
         new CommandLine(executor).parseArgs("--changelog=serverconf", "--url=jdbc:h2:mem:test", "update");
