19d78830cdee0a48f81e0b058ae2b42cd86a2ede
Andres Rosenthal <andres.rosenthal@nortal.com>
2026-03-23 13:56:58 +0200

    feat(01-02): scaffold ds-issuance-service module structure and main class
    - Add parent build.gradle.kts with xroad.submodule-conventions
    - Add application build.gradle.kts with Quarkus conventions and issuerservice BOMs
    - Add IssuanceServiceMain.java with @QuarkusMain entry point

---
+++ src/service/ds-issuance-service/ds-issuance-service-application/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+  id("xroad.java-conventions")
+  id("xroad.java-exec-conventions")
+  id("xroad.quarkus-application-conventions")
+}
+
+dependencies {
+  implementation(platform(libs.quarkus.bom))
+
+  implementation(project(":lib:bootstrap-edc-quarkus"))
+  implementation(project(":lib:properties-quarkus"))
+
+  implementation(libs.bundles.quarkus.containerized)
+
+  runtimeOnly(libs.edc.bom.issuerservice)
+  runtimeOnly(libs.edc.bom.issuerservice.sql)
+
+  runtimeOnly(libs.edc.core.sql.bootstrapper)
+}
