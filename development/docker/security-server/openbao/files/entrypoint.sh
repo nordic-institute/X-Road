733c50ece69f020a18a50cab61574224922aa64f
Andres Rosenthal <andres.rosenthal@nortal.com>
2026-03-24 14:20:06 +0200

    feat(03-03): seed ds-cp-sts-client-secret into OpenBao on startup
    - Added bao kv put to seed ds-cp-sts-client-secret into xrd-secret KV mount
    - Seeding occurs after secret-store-init.sh (which creates the xrd-secret mount)
    - Secret is available before any EDC services attempt STS authentication

--- development/docker/security-server/openbao/files/entrypoint.sh
+++ development/docker/security-server/openbao/files/entrypoint.sh
@@ -6,4 +6,7 @@ bao server -dev \
 
 ./usr/share/xroad/scripts/secret-store-init.sh
 
+# Seed ds-control-plane STS client secret for DCP authentication
+bao kv put xrd-secret/ds-cp-sts-client-secret value="dev-sts-secret"
+
 wait
