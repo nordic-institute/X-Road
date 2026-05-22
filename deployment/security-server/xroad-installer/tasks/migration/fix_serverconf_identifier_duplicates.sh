b253ac2cdc0ef2ad396a8a63315ade76f576c971
Eneli Reimets <eneli.reimets@nortal.com>
2026-05-22 14:48:41 +0300

    fix: minor fixes

--- deployment/security-server/xroad-installer/tasks/migration/fix_serverconf_identifier_duplicates.sh
+++ deployment/security-server/xroad-installer/tasks/migration/fix_serverconf_identifier_duplicates.sh
@@ -111,39 +111,15 @@ BEGIN
     SELECT COUNT(*)
       INTO bad_count
     FROM serverconf.identifier
-    WHERE xroadinstance IS NULL
-       OR memberclass IS NULL
-       OR membercode IS NULL;
-
-    IF bad_count <> 0 THEN
-        RAISE EXCEPTION
-            'serverconf.identifier contains % rows with NULL values in required fields (xroadinstance/memberclass/membercode)',
-            bad_count;
-    END IF;
-
-    SELECT COUNT(*)
-      INTO bad_count
-    FROM serverconf.identifier
     WHERE servicecode IS NOT NULL
        OR serviceversion IS NOT NULL;
 
     IF bad_count <> 0 THEN
         RAISE EXCEPTION
-            'serverconf.identifier contains % rows where servicecode or serviceversion is NOT NULL (these columns should be NULL)',
+            'serverconf.identifier contains % rows where servercode, servicecode or serviceversion is NOT NULL (these columns should be NULL)',
             bad_count;
     END IF;
 
-    SELECT COUNT(*)
-      INTO bad_count
-    FROM serverconf.identifier
-    WHERE servercode IS NOT NULL
-      AND "type" IN ('MEMBER', 'SUBSYSTEM', 'GLOBALGROUP');
-
-    IF bad_count <> 0 THEN
-        RAISE EXCEPTION
-            'serverconf.identifier contains % rows where servercode is NOT NULL (servercode should be NOT NULL for MEMBER, SUBSYSTEM, and GLOBALGROUP)',
-            bad_count;
-    END IF;
 END \$\$;
 
 DROP TABLE IF EXISTS tmp_identifier_groups;
@@ -178,11 +154,10 @@ FROM serverconf.identifier i
 JOIN tmp_identifier_groups g
     ON i."type" = g."type"
    AND i.xroadinstance = g.xroadinstance
-   AND i.memberclass = g.memberclass
-   AND i.membercode = g.membercode
+   AND i.memberclass IS NOT DISTINCT FROM g.memberclass
+   AND i.membercode IS NOT DISTINCT FROM  g.membercode
    AND i.subsystemcode IS NOT DISTINCT FROM g.subsystemcode
    AND i.groupcode IS NOT DISTINCT FROM g.groupcode
-   AND i.servercode IS NOT DISTINCT FROM g.servercode
 WHERE i.id <> g.main_id
   AND i."type" IN ('MEMBER', 'SUBSYSTEM', 'GLOBALGROUP');
 
@@ -228,7 +203,7 @@ BEGIN
 
     IF remaining_count <> 0 THEN
         RAISE EXCEPTION
-            'Duplicate rows still remain in serverconf.identifier for MEMBER, SUBSYSTEM, and GLOBALGROUP: %',
+            'Duplicate rows still remain in serverconf.identifier for MEMBER, SUBSYSTEM and GLOBALGROUP: %. Fix the problematic rows manually and re-run the upgrade wizard.',
             remaining_count;
     END IF;
 END \$\$;
