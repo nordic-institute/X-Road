da195b1ae0ff762230e7b590a33017623eef7b46
Ričardas Bučiūnas <ricardas.buciunas@nortal.com>
2026-06-03 09:32:37 +0300

    chore(dsp): drop unused prop

--- src/service/proxy/proxy-dsp-core/src/main/java/org/niis/xroad/proxy/dataplane/DataPlaneServerProperties.java
+++ src/service/proxy/proxy-dsp-core/src/main/java/org/niis/xroad/proxy/dataplane/DataPlaneServerProperties.java
@@ -55,10 +55,6 @@ public interface DataPlaneServerProperties {
     @WithDefault("60000")
     int threadPoolIdleTimeout();
 
-    @WithName("data-flow-endpoint")
-    @WithDefault("http://${xroad.proxy.dsp.listen-address}:${xroad.proxy.dsp.listen-port}/full/api/v1/dataflows")
-    String dataFlowEndpoint();
-
     @WithName("serverproxy-endpoint")
     @WithDefault("https://localhost:${xroad.proxy.server-port:5500}")
     String serverproxyEndpoint();
