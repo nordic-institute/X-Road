825cfc611c3d47d118adbe961b57cf6853b3b266
Stefan Cvetkovski <stefan.cvetkovski@nortal.com>
2026-06-12 15:59:40 +0200

    feat: switch Central Server to DB-based configuration properties
    Refs: XRDDEV-3189

--- src/central-server/registration-service/build.gradle.kts
+++ src/central-server/registration-service/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
 
   implementation(libs.springBoot.starterJetty)
   implementation(libs.springBoot.starterSecurity)
+  implementation(project(":common:common-db"))
   implementation(project(":lib:properties-spring"))
   implementation(libs.jakarta.validationApi)
   implementation(libs.bucket4j.core)
