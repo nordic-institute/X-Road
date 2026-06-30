ffd6d32cde01b49d2c45e1bf063411479d8ec453
Justas Samuolis <justas.samuolis@nortal.com>
2026-06-30 12:55:44 +0300

    fix(cs-ui-system-test): bump Selenium CDP package v145->v146
    selenium-java 4.44.0 (pulled transitively by selenide 7.16.2) drops the
    versioned DevTools package org.openqa.selenium.devtools.v145; it ships
    v146/v147/v148. Update the imports in CommonUiStepDefs so intTest compiles.

--- src/central-server/admin-service/ui-system-test/src/intTest/java/org/niis/xroad/cs/test/ui/glue/CommonUiStepDefs.java
+++ src/central-server/admin-service/ui-system-test/src/intTest/java/org/niis/xroad/cs/test/ui/glue/CommonUiStepDefs.java
@@ -29,8 +29,8 @@ import io.cucumber.java.After;
 import io.cucumber.java.en.Step;
 import org.niis.xroad.test.framework.core.ui.utils.SeleniumUtils;
 import org.openqa.selenium.devtools.DevTools;
-import org.openqa.selenium.devtools.v145.network.Network;
-import org.openqa.selenium.devtools.v145.network.model.ConnectionType;
+import org.openqa.selenium.devtools.v146.network.Network;
+import org.openqa.selenium.devtools.v146.network.model.ConnectionType;
 
 import java.util.Optional;
 
