594858c9f8044da547271ab0db9cabca7155ddc0
marcdavid <marc.david@nortal.com>
2026-07-20 11:36:00 +0300

    fix: Adding clock reset in unit tests

--- src/configuration-client/core/src/test/java/ee/ria/xroad/common/conf/globalconf/ConfigurationParserTest.java
+++ src/configuration-client/core/src/test/java/ee/ria/xroad/common/conf/globalconf/ConfigurationParserTest.java
@@ -73,6 +73,11 @@ class ConfigurationParserTest {
         TimeUtils.setClock(Clock.fixed(Instant.parse("2024-01-01T00:00:00Z"), ZoneOffset.UTC));
     }
 
+    @AfterEach
+    void resetClock() {
+        TimeUtils.setClock(Clock.systemUTC());
+    }
+
     /**
      * Test to ensure the parser succeeds on a simple configuration.
      *
